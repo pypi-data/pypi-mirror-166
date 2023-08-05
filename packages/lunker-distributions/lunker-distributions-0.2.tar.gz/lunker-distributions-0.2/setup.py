23d5ecdb253d9e6bc8c3ef9b1e2607580bd92d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:45 +0000

    Add package lunker-distributions/0.2/lunker-distributions-0.2.tar.gz

---
+++ packages/lunker-distributions/lunker-distributions-0.2.tar.gz/lunker-distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='lunker-distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['lunker-distributions'],
+      zip_safe=False)
