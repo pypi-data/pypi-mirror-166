13773e524c4a6dc31375e2575765f7641a90f64a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:27:31 +0000

    Add package wenrenren-nester/1.0.0/wenrenren-nester-1.0.0.tar.gz

---
+++ packages/wenrenren-nester/wenrenren-nester-1.0.0.tar.gz/wenrenren-nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'wenrenren-nester',
+    version = '1.0.0',
+    py_modules = ['wenrenren-nester'],
+    author = 'jie',
+    author_email = '632931651@qq.com',
+    url = 'http://www.python.org/sigs/distutils-sig/',
+    description = 'A simple printer of nested lists',
+) # end of setup()
