a7f058fde02edbd30d8737c0e78032e5ed6fd024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:23 +0000

    Add package mingpython/0.0.1/mingpython-0.0.1.zip

---
+++ packages/mingpython/mingpython-0.0.1.zip/mingpython-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name='mingpython',
+    version='0.0.1',
+    packages=find_packages(exclude=["mingpython"]),
+    py_modules=['Tool'],
+    url='https://gitee.com/minglie/mingpython',
+    author='minglie',
+    author_email='934031452@qq.com',
+    description='Automatically set package version from Git.',
+    license='http://opensource.org/licenses/MIT',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ],
+
+    install_requires=[],
+)
