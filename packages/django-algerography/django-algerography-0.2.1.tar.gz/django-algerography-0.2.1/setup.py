ec167a6aaf07d8db45d1896ed10978d04df071c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:19 +0000

    Add package django-algerography/0.2.1/django-algerography-0.2.1.tar.gz

---
+++ packages/django-algerography/django-algerography-0.2.1.tar.gz/django-algerography-0.2.1/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['django_algerography',
+ 'django_algerography.management.commands',
+ 'django_algerography.migrations']
+
+package_data = \
+{'': ['*'], 'django_algerography': ['fixtures/*']}
+
+setup_kwargs = {
+    'name': 'django-algerography',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': None,
+    'author': 'fathibensari',
+    'author_email': 'fethibensari@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
