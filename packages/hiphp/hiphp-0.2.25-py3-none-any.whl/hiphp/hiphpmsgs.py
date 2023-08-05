923195f3c48c49a91f24efe61fa1938091bd9261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:11 +0000

    Add package hiphp/0.2.25/hiphp-0.2.25-py3-none-any.whl

---
+++ packages/hiphp/hiphp-0.2.25-py3-none-any.whl/hiphp/hiphpmsgs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding:utf-8
+#   |                                                          |
+# --+----------------------------------------------------------+--
+#   |   Code by : yasserbdj96                                  |
+#   |   Email   : yasser.bdj96@gmail.com                       |
+#   |   Github  : https://github.com/yasserbdj96               |
+#   |   BTC     : bc1q2dks8w8uurca5xmfwv4jwl7upehyjjakr3xga9   |
+# --+----------------------------------------------------------+--  
+#   |        all posts #yasserbdj96 ,all views my own.         |
+# --+----------------------------------------------------------+--
+#   |                                                          |
+
+#START{
+emsg_1="[✗] We were unable to recognize the hiphp identifier."
+emsg_2="[✗] Command not found!"
+emsg_3="[✗] We could not contact the site"
+emsg_4="[✗] The file you entered does not exist."
+emsg_5="[✗] We could not upload the file"
+
+msg_1="[!] Copy the PHP code below to the website path you entered:"
+msg_2="[!] There is a new update "
+msg_3="[!] Download the new version from 'https://github.com/yasserbdj96/hiphp'"
+msg_4="[!] No updates available"
+
+smsg_1="[✓] File downloaded in: "
+smsg_2="[✓] The path has been compressed into: "
+#}END.
