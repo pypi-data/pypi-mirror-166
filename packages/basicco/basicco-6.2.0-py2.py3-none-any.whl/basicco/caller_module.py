8d249816f0d3d3a2a50e102cd243817da952f20f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:55 +0000

    Add package basicco/6.2.0/basicco-6.2.0-py2.py3-none-any.whl

---
+++ packages/basicco/basicco-6.2.0-py2.py3-none-any.whl/basicco/caller_module.py
@@ -0,0 +1,30 @@
+"""Retrieve the caller's module name."""
+
+from __future__ import absolute_import, division, print_function
+
+import inspect
+
+__all__ = ["caller_module"]
+
+
+def caller_module(frames=0):
+    # type: (int) -> str | None
+    """
+    Get caller module name if possible.
+
+    :param frames: How many frames to go back.
+    :return: Module name or None.
+    """
+    try:
+        frame = inspect.stack()[2 + frames]
+        module = inspect.getmodule(frame[0])
+    except IndexError:
+        return None
+    else:
+        if module is None:
+            try:
+                return frame[0].f_globals["__name__"]
+            except (IndexError, KeyError):
+                return None
+        else:
+            return module.__name__
---
+++ packages/basicco/basicco-6.2.0-py2.py3-none-any.whl/basicco/unique_iterator.py
@@ -0,0 +1,26 @@
+"""Iterator that yields unique values."""
+
+from __future__ import absolute_import, division, print_function
+
+from tippo import Iterator, TypeVar
+
+__all__ = ["unique_iterator"]
+
+
+T = TypeVar("T")
+
+
+def unique_iterator(iterator):
+    # type: (Iterator[T]) -> Iterator[T]
+    """
+    Iterator that yields unique values.
+
+    :param iterator: Iterator.
+    :return: Unique iterator.
+    """
+    seen = set()
+    for value in iterator:
+        if value in seen:
+            continue
+        seen.add(value)
+        yield value
