c455823d2c0002b170944bca7a1c1c9f0b727bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:14 +0000

    Add package osint-tools/0.1.3/osint_tools-0.1.3.tar.gz

---
+++ packages/osint-tools/osint_tools-0.1.3.tar.gz/osint_tools-0.1.3/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['osint_tools',
+ 'osint_tools.api',
+ 'osint_tools.api.four_chan',
+ 'osint_tools.db',
+ 'osint_tools.db.mongo_db',
+ 'osint_tools.schemas']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['feedparser>=6.0.10,<7.0.0',
+ 'motor>=3.0.0,<4.0.0',
+ 'pydantic>=1.10.1,<2.0.0',
+ 'python-jose>=3.3.0,<4.0.0',
+ 'requests>=2.11']
+
+setup_kwargs = {
+    'name': 'osint-tools',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': "# Python library for parsing various API's\n\n\n\n",
+    'author': 'Alexander Slessor',
+    'author_email': 'alexjslessor@gmail.com.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
