6bf587948984970ca01c43eb6f5fcbff36d23801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:53 +0000

    Add package aws-proton-helper/0.7.8/aws-proton-helper-0.7.8.tar.gz

---
+++ packages/aws-proton-helper/aws-proton-helper-0.7.8.tar.gz/aws-proton-helper-0.7.8/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="aws-proton-helper",
+    version="0.7.8",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="aws-proton-helper of aws-proton-helper of aws-proton-helper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
