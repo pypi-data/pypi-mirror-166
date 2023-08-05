e5948f07ba5181f4ae544bb100c324d7c42a28bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:16 +0000

    Add package ml-to-en/0.0.1/ml_to_en-0.0.1.tar.gz

---
+++ packages/ml-to-en/ml_to_en-0.0.1.tar.gz/ml_to_en-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import io
+
+setup(
+	name="ml_to_en",
+	version="0.0.1",
+	description="A python based linguistic tool that transliterates words from Malayalam script to English script",
+	author="Elaine Mary Rose",
+	author_email="ammuelaine@gmail.com",
+	url="https://github.com/erose311/ml_to_en",
+	packages=['ml_to_en'],
+	download_url="https://github.com/erose311/ml_to_en",
+	license="GPLv3",
+	classifiers=[
+		"Intended Audience :: Developers",
+		"Programming Language :: Python :: 3.9",
+	]
+)
