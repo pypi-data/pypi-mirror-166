48e850a019e2de2ff1652915c8a45a2a92771ada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:15 +0000

    Add package foxrandompy/2022.0.3/foxrandompy-2022.0.3.tar.gz

---
+++ packages/foxrandompy/foxrandompy-2022.0.3.tar.gz/foxrandompy-2022.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+long_description = '''Привет! Данная библиотека нужна для рандома, от стандартной библиотеки random эта отличается тем что в ней больше функций! По всем вопросам писать мне на почту, а также ссылка на документацию: https://shiro-2.gitbook.io/foxrandompy/'''
+
+setup(
+	name='foxrandompy',
+	author='Ginji',
+	version='2022.0.3',
+	description='Данная библиотека предназначена для рандома, вся информация в документации!',
+	packages=['foxrandompy'],
+	author_email='bfire1999@gmail.com',
+	long_description=long_description,
+	license='Apache License, Version 2.0, see LICENSE file'
+	)
