8eb2610b251ff700c5e6384f9c2c1f91d0d292ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:02 +0000

    Add package pyEight/0.3.1/pyEight-0.3.1.tar.gz

---
+++ packages/pyEight/pyEight-0.3.1.tar.gz/pyEight-0.3.1/pyeight/constants.py
@@ -0,0 +1,27 @@
+"""
+pyeight.constants
+~~~~~~~~~~~~~~~~~~~~
+Constants list
+Copyright (c) 2017-2022 John Mihalic <https://github.com/mezz64>
+Licensed under the MIT license.
+"""
+
+MAJOR_VERSION = 0
+MINOR_VERSION = 3
+SUB_MINOR_VERSION = 1
+__version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}.{SUB_MINOR_VERSION}"
+
+API_URL = "https://client-api.8slp.net/v1"
+
+DEFAULT_TIMEOUT = 240
+DATE_TIME_ISO_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
+DATE_FORMAT = "%Y-%m-%d"
+
+DEFAULT_HEADERS = {
+    "content-type": "application/json",
+    "connection": "keep-alive",
+    "user-agent": "okhttp/4.9.1",
+    "accept-encoding": "gzip",
+    "accept": "application/json",
+    "authority": "client-api.8slp.net",
+}
