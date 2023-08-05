aed5cd2d4d4568be23d8f2fbbe906250a1ce8f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:09 +0000

    Add package rl-replicas/0.0.6/rl_replicas-0.0.6-py3-none-any.whl

---
+++ packages/rl-replicas/rl_replicas-0.0.6-py3-none-any.whl/rl_replicas/algorithms/__init__.py
@@ -0,0 +1,9 @@
+"""
+Reinfrocement Learning Algorithms
+"""
+
+from rl_replicas.algorithms.ddpg import DDPG
+from rl_replicas.algorithms.ppo import PPO
+from rl_replicas.algorithms.td3 import TD3
+from rl_replicas.algorithms.trpo import TRPO
+from rl_replicas.algorithms.vpg import VPG
