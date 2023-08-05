c9db167ed4af65f57b2947f20ffd771dcbbe04d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:24 +0000

    Add package juandb/0.2.2/juandb-0.2.2-py3-none-any.whl

---
+++ packages/juandb/juandb-0.2.2-py3-none-any.whl/juandb/__init__.py
@@ -0,0 +1,13 @@
+from .client import Client
+from .client import AsyncClient
+
+from .const import COINS
+from .const import TYPES
+from .const import STATUS
+from .const import CURRENCIES
+
+
+__all__ = ['Client', 'AsyncClient']
+
+__version__ = '0.2.2'
+__author__ = 'AMiWR'
---
+++ packages/juandb/juandb-0.2.2-py3-none-any.whl/juandb/client/__init__.py
@@ -0,0 +1,5 @@
+from .main import Client
+from .main import AsyncClient
+
+
+__all__ = ['Client', 'AsyncClient']
