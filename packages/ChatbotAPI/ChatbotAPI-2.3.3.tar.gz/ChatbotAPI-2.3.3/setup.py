1708ee998ff08717261ada6e99b5635b20b5df2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:38 +0000

    Add package ChatbotAPI/2.3.3/ChatbotAPI-2.3.3.tar.gz

---
+++ packages/ChatbotAPI/ChatbotAPI-2.3.3.tar.gz/ChatbotAPI-2.3.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ChatbotAPI",
+    version="2.3.3",
+    author="HilFing",
+    author_email="indradip.paul@outlook.com",
+    description="Lib to make AI chatbot with other additional utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hilfing/ChatbotAPI",
+    project_urls={
+        "Bug Tracker": "https://github.com/hilfing/ChatbotAPI/issues",
+        "Wiki": "https://github.com/hilfing/ChatbotAPI/wiki/Python"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+	'Intended Audience :: Developers'
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=['requests','pyjokes','textblob'],
+    python_requires='>=3',
+)
