49c6ec15b0d4f1ddc79aba18147a33547449eb54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:29 +0000

    Add package pyeditorjs/1.0.0b0/pyeditorjs-1.0.0b0.tar.gz

---
+++ packages/pyeditorjs/pyeditorjs-1.0.0b0.tar.gz/pyeditorjs-1.0.0b0/pyeditorjs/__init__.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+
+__all__ = [
+    'EditorJsParser',
+    'EditorJsParseError',
+
+    'EditorJsBlock',
+    'HeaderBlock',
+]
+
+
+# Overwrite __doc__ with README, so that pdoc can render it:
+README_PATH = Path(__file__).parent.parent.absolute() / Path('README.md')
+try:
+    with open(README_PATH, 'r', encoding="UTF-8") as readme:
+        __readme__ = readme.read()
+except Exception:
+    __readme__ = "Failed to read README.md!" # fallback message, for example when there's no README
+
+__doc__ = __readme__
+
+
+
+from .parser import EditorJsParser
+from .blocks import *
+from .exceptions import EditorJsParseError
+
+
+
+if __name__ == "__main__":
+    _ = [
+        EditorJsParser
+    ]
