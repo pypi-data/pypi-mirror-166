8216c5972a29d5b54bd1e17e9174637a6c2ad8dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:02 +0000

    Add package tintx/2022.9.2/tintx-2022.9.2-py3-none-any.whl

---
+++ packages/tintx/tintx-2022.9.2-py3-none-any.whl/tintx/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+TintX main tracking utility.
+
+TintX offers a simple ``RunDirectory`` class that uses the ``xarray`` library
+to read datasets such as model output data or observational datasets.
+
+The cell tracking as well as visualisation of cell tracks is realised through
+instances of the ``RunDirectory`` class.
+"""
+
+from tintx import config  # noqa: F401
+from .reader import RunDirectory
+
+__all__ = ["RunDirectory"]
+
+__version__ = "2022.9.2"
