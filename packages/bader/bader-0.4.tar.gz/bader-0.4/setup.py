f74655ddc7a39205a81f2e6424e4283bb945a2aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:28 +0000

    Add package bader/0.4/bader-0.4.tar.gz

---
+++ packages/bader/bader-0.4.tar.gz/bader-0.4/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='bader',
+    version='0.4',
+    packages=setuptools.find_packages(),
+    license='GPL-3.0',
+    author='jack',
+    author_email='kinginjack@gmail.com',
+    description='an automation software',
+    install_requires=['selenium', 'colorama', 'datetime', 'cryptography', 'webdriver-manager', 'termcolor', 'tinydb'],
+    python_requires='>=3.8'
+)
