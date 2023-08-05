f0a219e18d2d841c0064da0df1adeb7702a5cce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:15 +0000

    Add package input-detect/1.0.0/input_detect-1.0.0.tar.gz

---
+++ packages/input-detect/input_detect-1.0.0.tar.gz/input_detect-1.0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='input_detect',
+    version='1.0.0',
+    author='Huang Bo-Xuan',
+    author_email='r09142003@gmail.com',
+    install_requires=[
+        'pyaudio'
+    ]
+)
