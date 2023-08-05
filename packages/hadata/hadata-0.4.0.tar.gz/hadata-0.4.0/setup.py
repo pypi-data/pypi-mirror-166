e01bde65cb2d79b6430bc69ed68716f25df477b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:13 +0000

    Add package hadata/0.4.0/hadata-0.4.0.tar.gz

---
+++ packages/hadata/hadata-0.4.0.tar.gz/hadata-0.4.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='hadata',
+    version='0.4.0',
+    description='HiAcuity common Data Models',
+    url='https://github.com/hiacuity/ha-data',
+    author='Nayana Hettiarachchi',
+    author_email='nayana@hiacuity.com',
+    license='BSD 2-clause',
+    packages=['hadata'],
+    install_requires=['mongoengine==0.23.1'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
