c87dad81ea7216ed6588bd0f8f4a2ce36303d654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:10 +0000

    Add package wmb/0.1.0/wmb-0.1.0.tar.gz

---
+++ packages/wmb/wmb-0.1.0.tar.gz/wmb-0.1.0/wmb/files/__init__.py
@@ -0,0 +1,34 @@
+from ._cemba import *
+from ._aibs import *
+from ._broad import *
+from ._cemba_epi_retro import *
+from ._brain_region import *
+from ._ref import *
+from ._integration import *
+from .palette import PALETTES
+import pathlib
+
+
+class AutoPathMixIn:
+    def _check_file_path_attrs(self):
+        for attr in dir(self):
+            if not attr.startswith('__') and attr.endswith('_PATH'):
+                cur_path = self.__getattribute__(attr)
+                found = False
+                try:
+                    if pathlib.Path(cur_path).exists():
+                        found = True
+                    else:
+                        # try GCP path
+                        # change everything before BICCN
+                        new_path = cur_path.replace('/gale/netapp/cemba3c', '/cemba')
+                        if pathlib.Path(new_path).exists():
+                            cur_path = new_path
+                            found = True
+                except TypeError:
+                    continue
+
+                if found:
+                    self.__setattr__(attr, cur_path)
+                else:
+                    print(f'{attr} do not exist: {cur_path}')
