07523d69c221aaf3fe71a87d5e2022f51e6dcd0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:38 +0000

    Add package touca/1.5.8/touca-1.5.8-py3-none-any.whl

---
+++ packages/touca/touca-1.5.8-py3-none-any.whl/touca/cli/_execute.py
@@ -0,0 +1,81 @@
+# Copyright 2022 Touca, Inc. Subject to Apache-2.0 License.
+
+import sys
+from argparse import ArgumentParser
+from pathlib import Path
+
+from touca._options import config_file_parse
+from touca._runner import Workflow, prepare_parser, run_workflows
+from touca.cli._common import Operation
+import logging
+
+
+def is_test_module(module: str):
+    with open(module, "rt") as file:
+        return "@touca.Workflow" in file.read()
+
+
+def find_test_modules(testdir: str):
+    return [p for p in Path(testdir).glob("**/*.py") if is_test_module(p)]
+
+
+def extract_workflows(modules: list):
+    import importlib
+    import inspect
+    import sys
+
+    for module in modules:
+        relpath = Path(module).relative_to(Path.cwd())
+        syspath = Path(relpath.parent).absolute()
+        sys.path.append(f"{syspath}/")
+        mod = importlib.import_module(relpath.stem)
+        for (name, member) in inspect.getmembers(mod):
+            if isinstance(member, Workflow):
+                yield name, member
+        sys.path.remove(f"{syspath}/")
+
+
+class Execute(Operation):
+    name = "test"
+    help = "Run your Touca tests"
+
+    @classmethod
+    def parser(cls, parser: ArgumentParser):
+        parser.add_argument(
+            "--testdir",
+            nargs=1,
+            help="path to regression tests directory",
+        )
+        prepare_parser(parser)
+
+    def __init__(self, options: dict):
+        self.__options = {k: v for k, v in options.items() if v is not None}
+
+    def _find_arguments(self):
+        args = self.__options
+        flags = [
+            "save-as-binary",
+            "save-as-json",
+            "offline",
+            "overwrite",
+            "colored-output",
+        ]
+        config_content = config_file_parse()
+        if config_content:
+            for key in config_content.options("settings"):
+                func = config_content.getboolean if key in flags else config_content.get
+                args.update({key: func("settings", key)})
+        return args
+
+    def run(self):
+        logging.disable(logging.CRITICAL)
+        dir_test = Path(self.__options.get("testdir", [Path.cwd()])[0]).resolve()
+        args = self._find_arguments()
+        modules = find_test_modules(dir_test)
+        workflows = list(extract_workflows(modules))
+        try:
+            run_workflows(args, workflows)
+        except Exception as err:
+            print(f"test failed: {err}", file=sys.stderr)
+            return False
+        return True
