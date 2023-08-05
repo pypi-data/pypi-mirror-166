30c85b83179f594c668f46c49695af4b8214a243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:48 +0000

    Add package zephyr-ml/0.0.0/zephyr_ml-0.0.0-py2.py3-none-any.whl

---
+++ packages/zephyr-ml/zephyr_ml-0.0.0-py2.py3-none-any.whl/gpe/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for GreenGuard Prediction Engineering."""
+
+__author__ = 'MIT Data To AI Lab'
+__email__ = 'dai-lab@mit.edu'
+__version__ = '0.2.4.dev0'
+
+from gpe.entityset import create_pidata_entityset, create_scada_entityset
+from gpe.labeling import DataLabeler
