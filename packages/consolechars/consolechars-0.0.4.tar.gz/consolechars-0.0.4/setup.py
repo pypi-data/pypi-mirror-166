b9835b96d1eff641dfab2b75984c0dfb5beb8f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:20 +0000

    Add package consolechars/0.0.4/consolechars-0.0.4.tar.gz

---
+++ packages/consolechars/consolechars-0.0.4.tar.gz/consolechars-0.0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='consolechars',
+      version='0.0.4',
+      description='Chars!',
+      packages=['consolechars'],
+      author_email='matvey.dibrov123@gmail.com',
+      zip_safe=False)
