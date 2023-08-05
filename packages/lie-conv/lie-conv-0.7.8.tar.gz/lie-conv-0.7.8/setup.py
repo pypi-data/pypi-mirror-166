11ec401c1a9296b41fe2057f31ee30807e0a8fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:21 +0000

    Add package lie-conv/0.7.8/lie-conv-0.7.8.tar.gz

---
+++ packages/lie-conv/lie-conv-0.7.8.tar.gz/lie-conv-0.7.8/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="lie-conv",
+    version="0.7.8",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="lie-conv of lie-conv of lie-conv",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
