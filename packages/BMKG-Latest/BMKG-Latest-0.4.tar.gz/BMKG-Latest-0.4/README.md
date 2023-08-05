f3ae2021c0a9143967c4cac24cf8285caad73030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:59 +0000

    Add package BMKG-Latest/0.4/BMKG-Latest-0.4.tar.gz

---
+++ packages/BMKG-Latest/BMKG-Latest-0.4.tar.gz/BMKG-Latest-0.4/README.md
@@ -0,0 +1,20 @@
+# BMKG-Latest
+This package will guide us to receive the latest Eartquake Information from BMKG Indonesia
+
+## How it works?
+This package is getting the latest data of Indonesia Earthquake by scrapping from [BMKG](https://www.bmkg.go.id/) 
+
+BeautifulSoup4 and Requests pacakge are used to produce JSON-form output which is ready to use in web or mobile apps
+
+# How to use?
+
+```
+import gempaterkini
+if __name__ == '__main__':
+    print('Aplikasi utama')
+    result = gempaterkini.ekstraksi_data()
+    gempaterkini.tampilkan_data(result)
+```
+
+# Author
+Firdaus Wahyu Nugroho
---
+++ packages/BMKG-Latest/BMKG-Latest-0.4.tar.gz/BMKG-Latest-0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+
+    name="BMKG-Latest",
+    version="0.4",
+    author="Firdaus W N",
+    author_email="<firdauswnn@gmail.com>",
+    description="This package will guide us to receive the latest Earthquake Information from BMKG Indonesia",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/futagoya/bmkg-latest",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
