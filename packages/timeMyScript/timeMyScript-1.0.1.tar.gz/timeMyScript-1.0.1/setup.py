23a399b2953678984f76eaa736daff273d451153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:34 +0000

    Add package timeMyScript/1.0.1/timeMyScript-1.0.1.tar.gz

---
+++ packages/timeMyScript/timeMyScript-1.0.1.tar.gz/timeMyScript-1.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="timeMyScript",
+    version="1.0.1",
+    author="Mattia Iiriti",
+    description="Time your script with an import and read the result on exit",
+    url="https://github.com/DeusAres/timeMyScript",
+    license="MIT",
+    long_description="README.md"
+)
---
+++ packages/timeMyScript/timeMyScript-1.0.1.tar.gz/timeMyScript-1.0.1/timeMyScript/__init__.py
@@ -0,0 +1,2 @@
+name="timeMyScript"
+from .timeMyScript import *
