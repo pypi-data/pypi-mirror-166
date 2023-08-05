f829f8ebd76852e7f887f9bb2cae8ae96fa82d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:21 +0000

    Add package pyQuickSortArray/1.5/pyQuickSortArray-1.5.tar.gz

---
+++ packages/pyQuickSortArray/pyQuickSortArray-1.5.tar.gz/pyQuickSortArray-1.5/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pyQuickSortArray',
+      version='1.05',
+      description='Sorts an number array multiple ways',
+      packages=['pyQuickSortArray'],
+      author_email='vovaefanov478@gmail.com',
+      zip_safe=False)
