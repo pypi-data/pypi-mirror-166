a709bbe35318f77bc744cf0fc35106cd8746f40d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:59 +0000

    Add package job-orchestration/0.0.1/job_orchestration-0.0.1-py3-none-any.whl

---
+++ packages/job-orchestration/job_orchestration-0.0.1-py3-none-any.whl/job_orchestration/rereadyFailedConfigs.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+
+from job_orchestration.Config import Config
+from job_orchestration.Constants import config_failed_location, config_ready_location, output_location
+
+
+def reReadyFailedConfigs():
+    for configFilename in os.listdir(config_failed_location):
+        configFilepath = os.path.join(config_failed_location, configFilename)
+        config = Config(configFilepath)
+        shutil.rmtree(os.path.join(output_location, config.outputDir))
+        shutil.move(configFilepath, os.path.join(config_ready_location, configFilename))
