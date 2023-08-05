5f4067cd601d3f096db1decf2d6cd117cfbaa1c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:59 +0000

    Add package boaw/1.0.0/boaw-1.0.0.tar.gz

---
+++ packages/boaw/boaw-1.0.0.tar.gz/boaw-1.0.0/README.md
@@ -0,0 +1,20 @@
+# boaw
+
+## Description:
+This library is used to keep us online at work.
+Note: This is just for educational purpose. We are not responsible if you get in conflicts because of this library. Please use this library
+only if you agree with the above Note
+
+## Usage:
+
+1. To Stay in online:
+
+    Import the boaw library:
+
+        import boaw
+
+    Using the boaw library:
+
+        boaw.stay_online()
+
+    Now just hover your mouse on start button and it keeps clicking it every 5 seconds, thereby helping you to stay online.
---
+++ packages/boaw/boaw-1.0.0.tar.gz/boaw-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="boaw",                     # This is the name of the package
+    version="1.0.0",                        # The initial release version
+    author="Muttineni Sai Rohith",                     # Full name of the author
+    description="boaw is a library used to keep us stay in online always at work",
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.6',                # Minimum version requirement of the package
+    py_modules=["boaw"],             # Name of the python package
+    package_dir={'':'src'},     # Directory of the source code of the package
+    install_requires=["pyautogui"]                     # Install other dependencies if any
+)
