a988bf8bcc68d46f15043e24b6a06d8de6181e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:40 +0000

    Add package pygaps/4.4.0/pygaps-4.4.0-py3-none-any.whl

---
+++ packages/pygaps/pygaps-4.4.0-py3-none-any.whl/pygaps/__init__.py
@@ -0,0 +1,57 @@
+# pylint: disable=W0614,W0611,W0622,C0411
+# flake8: noqa
+# isort:skip_file
+
+__author__ = 'Paul Iacomi'
+__docformat__ = 'restructuredtext'
+
+import sys
+from .logging import logger
+
+# This code is written for Python 3.
+if sys.version_info[0] != 3:
+    logger.error("pyGAPS requires Python 3.")
+    sys.exit(1)
+
+# Let users know if they're missing any hard dependencies
+hard_dependencies = ("numpy", "pandas", "scipy")
+soft_dependencies = {"CoolProp": "Used for many thermodynamic backend calculations."}
+missing_dependencies = []
+
+import importlib
+for dependency in hard_dependencies:
+    if not importlib.util.find_spec(dependency):
+        missing_dependencies.append(dependency)
+
+if missing_dependencies:
+    raise ImportError(f"Missing required dependencies {missing_dependencies}")
+
+for dependency, reason in soft_dependencies.items():
+    if not importlib.util.find_spec(dependency):
+        logger.warning(f"Missing important package {dependency}. {reason}")
+
+del hard_dependencies, soft_dependencies, dependency, missing_dependencies
+
+# Data
+from pygaps.data import DATABASE
+from pygaps.data import ADSORBATE_LIST
+from pygaps.data import MATERIAL_LIST
+from pygaps.data import load_data
+
+# Thermodynamic backend
+from pygaps.utilities.coolprop_utilities import thermodynamic_backend
+from pygaps.utilities.coolprop_utilities import backend_use_coolprop
+from pygaps.utilities.coolprop_utilities import backend_use_refprop
+
+# Core classes
+from pygaps.core.adsorbate import Adsorbate
+from pygaps.core.material import Material
+from pygaps.core.pointisotherm import PointIsotherm
+from pygaps.core.modelisotherm import ModelIsotherm
+
+# Data load
+load_data()
+del load_data
+
+# Other user-facing functions
+# from .api import *
