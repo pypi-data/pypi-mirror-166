40145b0e6534b543dde57280b411256601041c4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:17 +0000

    Add package nice-sql/0.0.6/nice_sql-0.0.6-py3-none-any.whl

---
+++ packages/nice-sql/nice_sql-0.0.6-py3-none-any.whl/nicesql/__init__.py
@@ -0,0 +1,4 @@
+from nicesql.engine.sqlite import Sqlite
+from nicesql.sqlengine import execute, select, update, insert, delete, ddl, register, close
+from nicesql.sqlmodel import SqlModel
+from nicesql.sqlresult import SqlResult
