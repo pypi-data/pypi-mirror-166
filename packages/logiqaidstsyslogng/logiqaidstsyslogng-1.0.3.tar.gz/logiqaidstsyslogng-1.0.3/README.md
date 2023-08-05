d582be41ef98017494ccd0817c165e772cefb3ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:33 +0000

    Add package logiqaidstsyslogng/1.0.3/logiqaidstsyslogng-1.0.3.tar.gz

---
+++ packages/logiqaidstsyslogng/logiqaidstsyslogng-1.0.3.tar.gz/logiqaidstsyslogng-1.0.3/README.md
@@ -0,0 +1,28 @@
+# Syslog-ng python destination for LOGIQ.AI
+
+## Description
+
+A python destination to push data via syslog-ng to LOGIQ.AI
+
+## Help
+
+Please reach out to us on https://www.logiq.ai/contact/
+
+## Authors
+
+Contributors names and contact info
+
+Ranjan Parthasarathy (support@logiq.ai)
+
+## Version History
+
+* 1.0.3
+    * Publish periodic stats to log file
+    * Skip SSL verification for publish by default
+    * Use filename vs pathname for logger
+* 1.0.2
+    * Initial Release
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
---
+++ packages/logiqaidstsyslogng/logiqaidstsyslogng-1.0.3.tar.gz/logiqaidstsyslogng-1.0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="logiqaidstsyslogng",           # This is the name of the package
+    version="1.0.3",                        # The initial release version
+    author="Ranjan Parthasarathy",                     # Full name of the author
+    author_email='support@logiq.ai',
+    url='https://www.logiq.ai',
+    description="Syslog-ng Python destination for LOGIQ.AI",
+    long_description=long_description,      # Long description read from the the readme file
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),    # List of all python modules to be installed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.8',                # Minimum version requirement of the package
+    py_modules=["logiqaidstsyslogng"],             # Name of the python package
+    package_dir={'':'logiqaidstsyslogng/src'},     # Directory of the source code of the package
+    install_requires=["configparser","requests","urllib3"]                     # Install other dependencies if any
+)
