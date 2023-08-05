596b61a6b467329ae6d0aca4ff85112c3ad839e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:14 +0000

    Add package ingredient-parser-nlp/0.1.0a1/ingredient_parser_nlp-0.1.0a1.tar.gz

---
+++ packages/ingredient-parser-nlp/ingredient_parser_nlp-0.1.0a1.tar.gz/ingredient_parser_nlp-0.1.0a1/ingredient_parser/__init__.py
@@ -0,0 +1,4 @@
+from .parsers import parse_ingredient, parse_multiple_ingredients
+from .preprocess import PreProcessor
+
+__all__ = ["parse_ingredient", "parse_multiple_ingredients", "PreProcessor"]
