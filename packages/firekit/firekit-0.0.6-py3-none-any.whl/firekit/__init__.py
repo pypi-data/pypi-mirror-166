9b185c8fd015a552abe39ad4ab269f71c5ae888d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:45 +0000

    Add package firekit/0.0.6/firekit-0.0.6-py3-none-any.whl

---
+++ packages/firekit/firekit-0.0.6-py3-none-any.whl/firekit/__init__.py
@@ -0,0 +1,13 @@
+from firekit import data
+from firekit import evaluate
+from firekit import train
+from firekit import utils
+from firekit import vision
+
+__all__ = [
+  "data",
+  "evaluate",
+  "train",
+  "utils",
+  "vision"
+]
