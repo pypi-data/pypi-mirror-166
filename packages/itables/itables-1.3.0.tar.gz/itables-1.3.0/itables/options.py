726f1e0c38b88cd7402fc52e828992bc8dfee2e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:08 +0000

    Add package itables/1.3.0/itables-1.3.0.tar.gz

---
+++ packages/itables/itables-1.3.0.tar.gz/itables-1.3.0/itables/options.py
@@ -0,0 +1,30 @@
+"""Global options for the Interactive Tables"""
+
+"""Show the index? Possible values: True, False and 'auto'. In mode 'auto', the index is not shown
+if it has no name and its content is range(N)"""
+showIndex = "auto"
+
+"""Default styling options. See https://datatables.net/manual/styling/classes"""
+classes = ["display"]
+
+"""Default table style"""
+style = "max-width:100%"
+
+"""Additional tags like e.g. caption"""
+tags = ""
+
+"""Maximum bytes for displaying a table"""
+maxBytes = 2**16
+
+"""Maximum number of rows or columns"""
+# maxRows = 10000
+# maxColumns = 1000
+
+"""Pre dt code"""
+pre_dt_code = ""
+
+"""Optional table footer"""
+footer = False
+
+"""Column filters"""
+column_filters = False
