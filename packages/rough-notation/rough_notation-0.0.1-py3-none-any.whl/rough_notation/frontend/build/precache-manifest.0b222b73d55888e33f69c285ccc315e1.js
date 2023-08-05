e332b3dbc76e4fb5f88f37f75c095418e43f51cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:40 +0000

    Add package rough-notation/0.0.1/rough_notation-0.0.1-py3-none-any.whl

---
+++ packages/rough-notation/rough_notation-0.0.1-py3-none-any.whl/rough_notation/frontend/build/precache-manifest.0b222b73d55888e33f69c285ccc315e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d896294a3463b539103632e5155dd325",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aae4f51a04419e918ab5",
+    "url": "./static/js/2.5280e3f5.chunk.js"
+  },
+  {
+    "revision": "7c671161f6719cfc3830a8065d3db268",
+    "url": "./static/js/2.5280e3f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2734e9b23b11ecfc0608",
+    "url": "./static/js/main.f894e524.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
