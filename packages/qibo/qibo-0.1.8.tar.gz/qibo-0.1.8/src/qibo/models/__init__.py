4a3ad0128b9ed97911d1c7e834b0a96a72ad8383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:41 +0000

    Add package qibo/0.1.8/qibo-0.1.8.tar.gz

---
+++ packages/qibo/qibo-0.1.8.tar.gz/qibo-0.1.8/src/qibo/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from qibo.models import hep, tsp
+from qibo.models.circuit import Circuit
+from qibo.models.evolution import AdiabaticEvolution, StateEvolution
+from qibo.models.grover import Grover
+from qibo.models.qft import QFT
+from qibo.models.qgan import StyleQGAN
+from qibo.models.variational import AAVQE, FALQON, QAOA, VQE
