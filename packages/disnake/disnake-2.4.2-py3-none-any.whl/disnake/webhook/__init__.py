40d983a5b07eeb737e201963aa1c6745156e45b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:42 +0000

    Add package disnake/2.4.2/disnake-2.4.2-py3-none-any.whl

---
+++ packages/disnake/disnake-2.4.2-py3-none-any.whl/disnake/webhook/__init__.py
@@ -0,0 +1,13 @@
+"""
+disnake.webhook
+~~~~~~~~~~~~~~
+
+Webhook support
+
+:copyright: (c) 2015-2021 Rapptz, 2021-present Disnake Development
+:license: MIT, see LICENSE for more details.
+
+"""
+
+from .async_ import *
+from .sync import *
