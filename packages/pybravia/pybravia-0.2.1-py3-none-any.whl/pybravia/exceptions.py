699529e4ada6f51addd964e3953b72c10505dc00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:12 +0000

    Add package pybravia/0.2.1/pybravia-0.2.1-py3-none-any.whl

---
+++ packages/pybravia/pybravia-0.2.1-py3-none-any.whl/pybravia/exceptions.py
@@ -0,0 +1,25 @@
+"""BraviaTV exceptions."""
+
+
+class BraviaTVError(Exception):
+    """Base BraviaTV exception."""
+
+
+class BraviaTVAuthError(BraviaTVError):
+    """Raised to indicate auth error."""
+
+
+class BraviaTVNotFound(BraviaTVError):
+    """Raised to indicate not found error."""
+
+
+class BraviaTVNotSupported(BraviaTVError):
+    """Raised to indicate not supported error."""
+
+
+class BraviaTVConnectionError(BraviaTVError):
+    """Raised to indicate connection error."""
+
+
+class BraviaTVConnectionTimeout(BraviaTVError):
+    """Raised to indicate connection timeout."""
