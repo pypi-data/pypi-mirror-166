a0513d6992ab11428461ca9a99e8cae31814c50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:35 +0000

    Add package yetl-framework/0.0.11/yetl-framework-0.0.11.tar.gz

---
+++ packages/yetl-framework/yetl-framework-0.0.11.tar.gz/yetl-framework-0.0.11/yetl_flow/dataset/__init__.py
@@ -0,0 +1,37 @@
+from ._dataset import Dataset
+from ._source import Source
+from ._destination import Destination
+from ._reader import Reader
+from ._writer import Writer
+from ._stream_reader import StreamReader
+from ._stream_writer import StreamWriter
+from ._factory import factory as dataset_factory
+from ._save import (
+    AppendSave,
+    IgnoreSave,
+    OverwriteSave,
+    OverwriteSchemaSave,
+    ErrorIfExistsSave,
+    MergeSave,
+    DefaultSave,
+    Save,
+)
+
+__all__ = [
+    "Source",
+    "Destination",
+    "Dataset",
+    "Reader",
+    "Writer",
+    "StreamReader",
+    "StreamWriter",
+    "dataset_factory",
+    "AppendSave",
+    "IgnoreSave",
+    "OverwriteSave",
+    "OverwriteSchemaSave",
+    "ErrorIfExistsSave",
+    "MergeSave",
+    "DefaultSave",
+    "Save",
+]
