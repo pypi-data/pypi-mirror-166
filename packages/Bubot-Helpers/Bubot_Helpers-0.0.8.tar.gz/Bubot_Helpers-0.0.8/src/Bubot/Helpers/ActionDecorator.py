a6a7eb57f4deeff39cc1897ab61ea8417e8d4fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:17 +0000

    Add package Bubot-Helpers/0.0.8/Bubot_Helpers-0.0.8.tar.gz

---
+++ packages/Bubot-Helpers/Bubot_Helpers-0.0.8.tar.gz/Bubot_Helpers-0.0.8/src/Bubot/Helpers/ActionDecorator.py
@@ -0,0 +1,61 @@
+import asyncio
+
+from .Action import Action
+from .ExtException import ExtException
+
+
+def async_action(f):
+    def prepare_action(args, kwargs):
+        try:
+            name = f'{args[0].__name__}.{f.__name__}'
+        except Exception:
+            try:
+                name = f'{args[0].__class__.__name__}.{f.__name__}'
+            except Exception:
+                name = f.__name__
+        _action = Action(name)
+        kwargs['_action'] = _action
+        return _action
+
+    async def wrapper(*args, **kwargs):
+        _action = prepare_action(args, kwargs)
+        try:
+            result = await f(*args, **kwargs)
+            if isinstance(result, Action):
+                result = _action.add_stat(result)
+            _action.set_end(result)
+            return _action
+
+        except asyncio.CancelledError as err:
+            raise err from err
+        except ExtException as err:
+            raise ExtException(parent=err, action=_action, skip_traceback=1) from err
+        except Exception as err:
+            raise ExtException(parent=err, action=_action, skip_traceback=1) from err
+
+    return wrapper
+
+
+def action(f):
+    def wrapper(*args, **kwargs):
+        try:
+            name = f'{args[0].__name__}.{f.__name__}'
+        except Exception:
+            try:
+                name = f'{args[0].__class__.__name__}.{f.__name__}'
+            except Exception:
+                name = f.__name__
+        _action = Action(name)
+        try:
+            kwargs['_action'] = _action
+            result = f(*args, **kwargs)
+            result = _action.add_stat(result)
+            if not isinstance(result, Action):
+                return result
+            return _action.set_end(result)
+        except ExtException as err:
+            raise ExtException(parent=err, action=_action, skip_traceback=1) from err
+        except Exception as err:
+            raise ExtException(parent=err, action=_action, skip_traceback=1) from err
+
+    return wrapper
