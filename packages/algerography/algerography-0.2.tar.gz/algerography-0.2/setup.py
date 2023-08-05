8b37150de773144e75174e287b765af7feea91c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:45 +0000

    Add package algerography/0.2/algerography-0.2.tar.gz

---
+++ packages/algerography/algerography-0.2.tar.gz/algerography-0.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['algerography', 'algerography.management.commands', 'algerography.migrations']
+
+package_data = \
+{'': ['*'], 'algerography': ['fixtures/*']}
+
+setup_kwargs = {
+    'name': 'algerography',
+    'version': '0.2',
+    'description': '',
+    'long_description': None,
+    'author': 'fathibensari',
+    'author_email': 'fethibensari@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
