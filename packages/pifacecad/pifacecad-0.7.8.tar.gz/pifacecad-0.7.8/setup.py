2bc5c7b3d5dd267bdb56ed3d8cc0a29a42baf7a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:51 +0000

    Add package pifacecad/0.7.8/pifacecad-0.7.8.tar.gz

---
+++ packages/pifacecad/pifacecad-0.7.8.tar.gz/pifacecad-0.7.8/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="pifacecad",
+    version="0.7.8",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="pifacecad of pifacecad of pifacecad",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
