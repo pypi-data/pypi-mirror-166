20fb54d45a2064df35f15128197d6ed2ea43749e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:46 +0000

    Add package calc-famus100/0.1.0/calc_famus100-0.1.0-py3-none-any.whl

---
+++ packages/calc-famus100/calc_famus100-0.1.0-py3-none-any.whl/calc_famus100/main.py
@@ -0,0 +1,20 @@
+class calculator:
+    def __init__(self,value_1,value_2):
+        self.value_1 = value_1
+        self.value_2 = value_2
+
+    def add(a,b):
+        addition = a + b
+        return addition
+
+    def subtract(a, b):
+        subtract = a - b
+        return subtract
+
+    def multiply(a,b):
+        multiply = a * b
+        return multiply
+
+    def divide(a,b):
+        divide = a / b
+        return divide
