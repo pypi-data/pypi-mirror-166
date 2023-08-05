76af14b1b44027e4b2827f216cff34b37060c289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:12 +0000

    Add package darlog-py23/1.2.0/darlog-py23-1.2.0.tar.gz

---
+++ packages/darlog-py23/darlog-py23-1.2.0.tar.gz/darlog-py23-1.2.0/src/darlog/__init__.py
@@ -0,0 +1,3 @@
+# encoding: utf-8
+# https://packaging.python.org/en/latest/guides/packaging-namespace-packages/#pkgutil-style-namespace-packages
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
