ca6e0be4183adfabdfa74ef2ff72e53975df6aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:50 +0000

    Add package slack-hooks/0.2.0/slack_hooks-0.2.0-py3-none-any.whl

---
+++ packages/slack-hooks/slack_hooks-0.2.0-py3-none-any.whl/slackhooks/blocks/text.py
@@ -0,0 +1,30 @@
+PLAIN_TEXT = "plain_text"
+MARKDOWN = "mrkdwn"
+
+
+class Text:
+    def __init__(self, text: str = None, type: str = None):
+        self.text = text
+        self.type = type
+
+    def serialize(self) -> dict:
+        return {
+            "text": self.text,
+            "type": self.type,
+        }
+
+
+class PlainText(Text):
+    def __init__(self, text: str = None, emoji: bool = True):
+        super().__init__(text=text, type=PLAIN_TEXT)
+        self.emoji = emoji
+
+    def serialize(self) -> dict:
+        return super().serialize() | {
+            "emoji": self.emoji,
+        }
+
+
+class MarkdownText(Text):
+    def __init__(self, text: str = None):
+        super().__init__(text=text, type=MARKDOWN)
