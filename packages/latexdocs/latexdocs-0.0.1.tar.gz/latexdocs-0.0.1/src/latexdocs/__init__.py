11c38f12b99e12b9306cc5e95eae8ea09dd11dbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:24 +0000

    Add package latexdocs/0.0.1/latexdocs-0.0.1.tar.gz

---
+++ packages/latexdocs/latexdocs-0.0.1.tar.gz/latexdocs-0.0.1/src/latexdocs/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from .document import *
+from .items import *
+
+__version__ = "v0.0.1"
+
+__description__ = "A Python library to generate publication-ready documents with LaTeX."
