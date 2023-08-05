fc96a5eca0421c569836a373cd3999298a71fdc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:02 +0000

    Add package pyfa-converter/1.0.1.0/pyfa-converter-1.0.1.0.tar.gz

---
+++ packages/pyfa-converter/pyfa-converter-1.0.1.0.tar.gz/pyfa-converter-1.0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyfa_converter', 'pyfa_converter.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fastapi>=0.65', 'pydantic>=1.6', 'python-multipart>=0.0.5,<0.0.6']
+
+setup_kwargs = {
+    'name': 'pyfa-converter',
+    'version': '1.0.1.0',
+    'description': 'Pydantic to fastapi model converter.',
+    'long_description': 'None',
+    'author': 'dotX12',
+    'author_email': 'dev@shitposting.team',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8',
+}
+
+
+setup(**setup_kwargs)
