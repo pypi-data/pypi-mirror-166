ade3a1520411b2da67414e9f26f142c206547037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:32 +0000

    Add package granted-flask/0.1.9a7/granted-flask-0.1.9a7.tar.gz

---
+++ packages/granted-flask/granted-flask-0.1.9a7.tar.gz/granted-flask-0.1.9a7/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['granted_flask']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-dotenv>=0.20.0,<0.21.0', 'requests>=2.28.1,<3.0.0']
+
+entry_points = \
+{'flask.commands': ['shell = granted_flask.shell:shell_command']}
+
+setup_kwargs = {
+    'name': 'granted-flask',
+    'version': '0.1.9a7',
+    'description': '',
+    'long_description': None,
+    'author': 'Common Fate',
+    'author_email': 'hello@commonfate.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
