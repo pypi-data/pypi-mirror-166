df717bb4fc6cd4338ad127ca9bdef9ec21bb5aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:27 +0000

    Add package Remilia/2022.9.4.6.38.34/Remilia-2022.9.4.6.38.34.tar.gz

---
+++ packages/Remilia/Remilia-2022.9.4.6.38.34.tar.gz/Remilia-2022.9.4.6.38.34/Remilia/__init__.py
@@ -0,0 +1,16 @@
+from . import LiteLog,LiteMixin,LiteUtils,LitePGL,LiteTasks,LiteEvent,LiteConfig,LiteResource,LiteI18n,LiteThread,DecoratorUtils,base
+
+__all__=[
+    LiteLog,
+    LiteMixin,
+    LiteUtils,
+    LiteThread,
+    LitePGL,
+    LiteResource,
+    LiteTasks,
+    LiteEvent,
+    LiteConfig,
+    LiteI18n,
+    DecoratorUtils,
+    base
+    ]
---
+++ packages/Remilia/Remilia-2022.9.4.6.38.34.tar.gz/Remilia-2022.9.4.6.38.34/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from time import localtime, strftime
+from setuptools import setup, find_packages
+setup(
+    name='Remilia',
+    version=strftime("%Y.%m.%d.%H.%M.%S", localtime()),
+    description=(
+        'Use python with dignity,here offer some utils'
+    ),
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    author='h2sxxa',
+    author_email='H2Sxxa0w0@gmail.com',
+    maintainer='h2sxxa',
+    maintainer_email='H2Sxxa0w0@gmail.com',
+    license='MIT License',
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://github.com/IAXRetailer/Remilia',
+    classifiers=[
+    'License :: OSI Approved :: MIT License',
+
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    ],
+)
