f1e25238fdfb7e81d52ca1c862e4bd277ebf76e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:14 +0000

    Add package conversation-qa/0.0.10/conversation_qa-0.0.10-py3-none-any.whl

---
+++ packages/conversation-qa/conversation_qa-0.0.10-py3-none-any.whl/conversation_qa/utils.py
@@ -0,0 +1,42 @@
+import torch
+
+_device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+def get_answer_prompt(text, query, dialogue=None):
+    text = text.strip()
+    query = query.strip()
+
+    prompt = "In the text below two people are discussing a story.\n\n"
+    prompt += "Story:\n" + text + "\n\n"
+    prompt += "Discussion:\n"
+    if dialogue:
+        dialogue = dialogue.strip()
+        prompt += dialogue + "\n"
+    prompt += "Q: " + query + "\n"
+    return prompt
+
+
+
+def generate_answer(model, tokenizer, prompt, length=5):
+    model.eval()
+    with torch.no_grad():
+        tokens = tokenizer.encode(prompt, return_tensors="pt")
+        text = prompt
+        start = len(prompt)
+        while "\n" not in text[start:]:
+            output = _inference(model, tokenizer, tokens.to(_device), length)
+            decoded = tokenizer.decode(output[0], skip_special_tokens=True)
+            text += decoded[len(text) :]
+            tokens = output
+
+    end = text.find("\n", start)
+    return text[start:end].split(":")[-1].strip()
+
+
+def _inference(model, tokenizer, tokens, length):
+    return model.generate(
+        tokens.to(_device),
+        max_length=tokens.shape[1] + length,
+        pad_token_id=tokenizer.eos_token_id,
+    )
