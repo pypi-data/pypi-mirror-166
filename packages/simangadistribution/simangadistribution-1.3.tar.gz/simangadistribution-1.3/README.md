963cfa7016586cb8fc890e68f073d0854a81bb62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:21 +0000

    Add package simangadistribution/1.3/simangadistribution-1.3.tar.gz

---
+++ packages/simangadistribution/simangadistribution-1.3.tar.gz/simangadistribution-1.3/README.md
@@ -0,0 +1,3 @@
+# python_package_module
+AWS MAchine learning program 
+with pacage development 
---
+++ packages/simangadistribution/simangadistribution-1.3.tar.gz/simangadistribution-1.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name = 'simangadistribution',
+	  version = '1.3',
+	  description = 'Gaussian distribution',
+	  packages = ['binomial_package'],
+	  zip_safe = False)
