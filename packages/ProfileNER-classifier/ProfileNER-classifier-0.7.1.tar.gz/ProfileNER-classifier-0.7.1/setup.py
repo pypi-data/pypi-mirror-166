e2e86cbc427ba6c293bae366e36a266fece61e8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:02 +0000

    Add package ProfileNER-classifier/0.7.1/ProfileNER-classifier-0.7.1.tar.gz

---
+++ packages/ProfileNER-classifier/ProfileNER-classifier-0.7.1.tar.gz/ProfileNER-classifier-0.7.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Science/Research',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='ProfileNER-classifier',
+  version='0.7.1',
+  description="Performs Classification using Regex for social media user's profile categorization and NER tasks",
+  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  long_description_content_type='text/markdown',
+  url='',  
+  author='Arthur Lopes',
+  author_email='arthurdslopes@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords=['NER', 'profile categories','classification','social media'],
+  py_modules= ['nlpclassifier_profilener'],
+  install_requires=[''],
+  include_package_data=True
+)
