ecae0e22c95e7aa7fc9f104cf4f0d873b3472173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:09 +0000

    Add package kih-api/0.9.1/kih_api-0.9.1.tar.gz

---
+++ packages/kih-api/kih_api-0.9.1.tar.gz/kih_api-0.9.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='kih_api',
+    version='0.9.1',
+    url='https://github.com/Kontinuum-Investment-Holdings/KIH_API',
+    author='Kavindu Athaudha',
+    author_email='kavindu@k-ih.co.uk',
+    packages=['kih_api'],
+    install_requires=[
+        "requests",
+        "urllib3",
+        "pytz",
+        "python-dateutil",
+        "pandas",
+        "numpy",
+        "ibapi",
+        "validators",
+        "dacite",
+        "mongoengine",
+        "pymongo",
+        "dataclass_csv"
+    ]
+)
