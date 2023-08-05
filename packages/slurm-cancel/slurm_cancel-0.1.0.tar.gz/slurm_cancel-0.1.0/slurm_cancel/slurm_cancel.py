c8831d4454e239914472a06a2c582bc02e424093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:23 +0000

    Add package slurm-cancel/0.1.0/slurm_cancel-0.1.0.tar.gz

---
+++ packages/slurm-cancel/slurm_cancel-0.1.0.tar.gz/slurm_cancel-0.1.0/slurm_cancel/slurm_cancel.py
@@ -0,0 +1,16 @@
+import subprocess
+import getpass
+
+def slurm_cancel_current_job():
+    hostname = os.uname().nodename
+    username = getpass.getuser()
+    bashCommand = f'squeue -u {username} | grep {hostname}'
+    process = subprocess.Popen(bashCommand, shell=True, stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    job_id = output.decode().split()[0]
+    cancel_command = f'scancel {job_id}'
+    print(f"Cancelling the job...")
+    process = subprocess.Popen(cancel_command, shell=True, stdout=subprocess.PIPE)
+    output, error = process.communicate()
+if __name__ == '__main__':
+    slurm_cancel_current_job()
