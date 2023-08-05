c623ae98927683298fde6b0d39ade60fdfef9a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:32 +0000

    Add package websockets-actions/0.3.0/websockets-actions-0.3.0.tar.gz

---
+++ packages/websockets-actions/websockets-actions-0.3.0.tar.gz/websockets-actions-0.3.0/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['websockets_actions',
+ 'websockets_actions.broadcast',
+ 'websockets_actions.fastapi',
+ 'websockets_actions.starlette']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'websockets-actions',
+    'version': '0.3.0',
+    'description': 'Using Actions for Websockets',
+    'long_description': '# WebSocket Actions\n\nНадстройка над Starlette, FastAPI для более удобной работы с WebSockets.\n\n## Установка\n\n```shell\n$ pip install wensockets-actions\n```\n\n\n## Пример\n\n**example.py**:\n\n```python\n\n```\n\nЗапустить приложение...\n\n```shell\n$ uvicorn example:app\n```\n\nБольше примеров, [здесь]().\n',
+    'author': 'Omelchenko Michael',
+    'author_email': 'socanime@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/DJWOMS/websockets-actions',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
