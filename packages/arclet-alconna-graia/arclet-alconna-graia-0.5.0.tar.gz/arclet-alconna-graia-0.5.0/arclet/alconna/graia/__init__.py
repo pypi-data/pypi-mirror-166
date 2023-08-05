e804b1b5a0ea57aafface66e8a939c12b6be0cfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:09 +0000

    Add package arclet-alconna-graia/0.5.0/arclet-alconna-graia-0.5.0.tar.gz

---
+++ packages/arclet-alconna-graia/arclet-alconna-graia-0.5.0.tar.gz/arclet-alconna-graia-0.5.0/arclet/alconna/graia/__init__.py
@@ -0,0 +1,16 @@
+"""
+Alconna 对于 Graia 系列的支持
+
+注意：
+    现阶段仍使用 graia.ariadne 的 MessageChain;
+
+    后续将会更新为 graia.amnesia 的 MessageChain
+"""
+from arclet.alconna import Alconna
+from .analyser import GraiaCommandAnalyser
+from .dispatcher import AlconnaDispatcher, AlconnaOutputMessage, AlconnaProperty, Query, Match, success_record
+from .saya import AlconnaSchema, AlconnaBehaviour
+from .utils import ImgOrUrl, fetch_name, match_path, AtID, command, match_value, from_command, shortcuts
+
+Alconna.config(analyser_type=GraiaCommandAnalyser)
+Alc = AlconnaDispatcher
