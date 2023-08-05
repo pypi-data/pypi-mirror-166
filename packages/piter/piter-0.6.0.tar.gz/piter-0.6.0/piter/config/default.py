2661aead62b61b8e7da34c9d52f02be5b028a54f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:11 +0000

    Add package piter/0.6.0/piter-0.6.0.tar.gz

---
+++ packages/piter/piter-0.6.0.tar.gz/piter-0.6.0/piter/config/default.py
@@ -0,0 +1,14 @@
+DEFAULT_CONFIG_PITER = {"env_root": "pienvs", "dependencies": None, "env": None}
+
+DEFAULT_CONFIG_ENV = {
+    "system_site_packages": False,
+    "clear": False,
+    "symlinks": True,
+    "upgrade": False,
+    "with_pip": True,
+    "prompt": "venv",
+    "upgrade_deps": False,
+    "dependencies": None,
+    "scripts": None,
+    # "vars": None
+}
