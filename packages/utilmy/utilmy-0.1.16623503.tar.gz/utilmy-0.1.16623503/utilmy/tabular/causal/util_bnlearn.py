537cd413edbcc02aa23a50c8c6e4796fa0ee0117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:29 +0000

    Add package utilmy/0.1.16623503/utilmy-0.1.16623503.tar.gz

---
+++ packages/utilmy/utilmy-0.1.16623503.tar.gz/utilmy-0.1.16623503/utilmy/tabular/causal/util_bnlearn.py
@@ -0,0 +1,17 @@
+""""
+R models for structured Learning
+
+https://rdrr.io/github/rlebron-bioinfo/gnlearn/man/notears.html
+
+
+
+https://www.bnlearn.com/examples/
+
+
+https://proceedings.neurips.cc/paper/2020/file/28a7602724ba16600d5ccc644c19bf18-Supplemental.pdf
+
+
+
+https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8421796/
+
+"""
