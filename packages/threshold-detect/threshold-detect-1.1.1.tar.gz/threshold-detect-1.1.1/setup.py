169f1be43a767136731c89af2478e076e85e5103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:27:55 +0000

    Add package threshold-detect/1.1.1/threshold-detect-1.1.1.tar.gz

---
+++ packages/threshold-detect/threshold-detect-1.1.1.tar.gz/threshold-detect-1.1.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='threshold-detect',
+    version='1.1.1',
+    author='Huang Bo-Xuan',
+    author_email='r09142003@gmail.com',
+    install_requires=[
+        'pyaudio'
+    ]
+)
