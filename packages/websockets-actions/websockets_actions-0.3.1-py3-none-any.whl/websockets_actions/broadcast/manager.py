67701c6240e1adcd6e70dcf80e8d82c6e182cf8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:50 +0000

    Add package websockets-actions/0.3.1/websockets_actions-0.3.1-py3-none-any.whl

---
+++ packages/websockets-actions/websockets_actions-0.3.1-py3-none-any.whl/websockets_actions/broadcast/manager.py
@@ -0,0 +1,29 @@
+import typing
+
+from starlette.websockets import WebSocket
+
+
+class WebSocketManager:
+    def __init__(self):
+        self.connections: typing.List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket) -> None:
+        await self.add_connection(websocket)
+
+    async def disconnect(self, websocket: WebSocket) -> None:
+        self.connections.remove(websocket)
+
+    async def add_connection(self, websocket: WebSocket) -> None:
+        self.connections.append(websocket)
+
+    async def send_message(self, websocket: WebSocket, message: dict) -> None:
+        await websocket.send_json(message)
+
+    async def broadcast(self, message: dict) -> None:
+        for connection in self.connections:
+            await connection.send_json(message)
+
+    async def broadcast_exclude(self, websockets: typing.List[WebSocket], message: dict) -> None:
+        for connection in self.connections:
+            if connection not in websockets:
+                await connection.send_json(message)
