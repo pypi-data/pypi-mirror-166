119611103a39dc41a9bc3ed775354d3720342091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:34 +0000

    Add package yetl-framework/0.0.13/yetl-framework-0.0.13.tar.gz

---
+++ packages/yetl-framework/yetl-framework-0.0.13.tar.gz/yetl-framework-0.0.13/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup, find_packages
+
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="yetl-framework",
+    version="0.0.13",
+    description="yet (another spark) etl framework",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://www.yetl.io/",
+    project_urls={
+        "GitHub": "https://github.com/sibytes/yetl",
+        "Documentation": "https://www.yetl.io/",
+    },
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=find_packages(),
+    install_requires=["pyyaml", "pyspark", "regex", "delta-spark"],
+    zip_safe=False,
+)
