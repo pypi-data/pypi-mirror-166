63197a36881a390b423bee5c2adeb43bcaada606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:18 +0000

    Add package st-tokenization-vis-component/0.0.3/st_tokenization_vis_component-0.0.3-py3-none-any.whl

---
+++ packages/st-tokenization-vis-component/st_tokenization_vis_component-0.0.3-py3-none-any.whl/viz_tokens_component/frontend/build/precache-manifest.6008e479325906ca72c271d6306517d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e193f7d9a4d40f35fd754b4c8229ace3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b222adebf333f07f82b4",
+    "url": "./static/js/2.91c99cb8.chunk.js"
+  },
+  {
+    "revision": "90d85258a7227bbb926dec29dcb29a18",
+    "url": "./static/js/2.91c99cb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c38bbccc84d5824b95ff",
+    "url": "./static/js/main.db8d4976.chunk.js"
+  },
+  {
+    "revision": "1534b57579d070af0362",
+    "url": "./static/js/runtime-main.d58b2606.js"
+  }
+]);
