07f7464feb153ddd9dbd62281a286eb9c77734ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:50 +0000

    Add package pih/1.40008/pih-1.40008.tar.gz

---
+++ packages/pih/pih-1.40008.tar.gz/pih-1.40008/setup.py
@@ -0,0 +1,65 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+#for facade
+#prettytable
+#colorama
+#protobuf
+#grpcio (six)
+#pyperclip
+#win32com -> pywin32 (!!!)
+
+#MC
+#pywhatkit
+
+#for orion
+#myssql
+
+#for log
+#telegram_send
+
+#for dc2
+#docs:
+    #mailmerge (pip install docx-mailmerge)
+    #xlsxwriter
+#ad:
+    #pyad
+    #pywin32 (!!!)
+#transliterate
+
+#for printer (dc2)
+#pysnmp
+
+#########################################################################################################
+"""
+1. python setup.py sdist --dist-dir pih_dist bdist_wheel --dist-dir pih_dist build --build-base pih_build
+2. twine upload pih_dist/*
+"""
+
+# This call to setup() does all the work
+setup(
+    name="pih",
+    version="1.40008",
+    description="PIH library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pacifichosp.com/",
+    author="Nikita Karachentsev",
+    author_email="it@pacifichosp.com",
+    license="MIT",
+    classifiers=[],
+    packages=["pih"],
+    include_package_data=True,
+    install_requires=["prettytable", "colorama", "protobuf", "grpcio"]
+)
