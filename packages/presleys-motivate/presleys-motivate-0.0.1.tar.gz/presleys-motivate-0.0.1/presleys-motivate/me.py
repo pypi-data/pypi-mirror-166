8a95b6ebb3700021fae0ac3e9b5c78ac2d77766f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:41 +0000

    Add package presleys-motivate/0.0.1/presleys-motivate-0.0.1.tar.gz

---
+++ packages/presleys-motivate/presleys-motivate-0.0.1.tar.gz/presleys-motivate-0.0.1/presleys-motivate/me.py
@@ -0,0 +1,3 @@
+def motivate_me():
+	print("You are doing great, keep it up")
+	
---
+++ packages/presleys-motivate/presleys-motivate-0.0.1.tar.gz/presleys-motivate-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+__project__ = "presleys-motivate"
+__version__ = "0.0.1"
+__description__ = "A ptyhon module to motivate you"
+__packages__ = ["presleys-motivate"]
+__author__ = "Presley Acuna"
+__author_email__ = "presleyacuna@gmail.com"
+__classifiers__ = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Education",
+    "Programming Language :: Python :: 3",
+]
+__keywords__ = ["motivation", "python", "presley's programs"]
+
+setup(
+    name = __project__,
+    version = __version__,
+    description = __description__,
+    packages = __packages__,
+    author = __author__,
+    author_email = __author_email__,
+    classifiers = __classifiers__,
+    keywords = __keywords__,
+    )
