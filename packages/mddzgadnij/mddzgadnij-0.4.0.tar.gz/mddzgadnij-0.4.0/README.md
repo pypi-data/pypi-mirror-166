23d1b0d010f404aeb706ad19512379ed42b9b964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:46 +0000

    Add package mddzgadnij/0.4.0/mddzgadnij-0.4.0.tar.gz

---
+++ packages/mddzgadnij/mddzgadnij-0.4.0.tar.gz/mddzgadnij-0.4.0/README.md
@@ -0,0 +1,5 @@
+# mddzgadnij
+
+Simple tool that will try to guess your number.
+Whole functionality of this project is **guess()** function.
+This function will guess any number you can think of in range of 1 to 100.
---
+++ packages/mddzgadnij/mddzgadnij-0.4.0.tar.gz/mddzgadnij-0.4.0/mddzgadnij/main.py
@@ -0,0 +1,25 @@
+def guess():
+    input("Please think of a number from 1 to 100 and press enter.")
+    minimum = 1
+    maximum = 100
+    predict = 50
+
+    while True:
+        ans = input(f"Is {predict} your number? (y - yes, m - more, l - less): ")
+        ans = ans.lower()
+        if ans == 'y':
+            break
+        elif ans == 'm':
+            minimum = predict + 1
+            predict = (predict + maximum) // 2
+        elif ans == 'l':
+            maximum = predict - 1
+            predict = (predict + minimum) // 2
+        else:
+            print("Error, use 'y', 'm' or 'l'.\n")
+
+    print("Yes I have guessed correctly!")
+
+
+if __name__ == "__main__":
+    guess()
