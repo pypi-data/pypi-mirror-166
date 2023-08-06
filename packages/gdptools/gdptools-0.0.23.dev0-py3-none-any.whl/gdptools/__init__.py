0f8fc145007a23bb2ded7243da3661bf39bc3867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:18:50 +0000

    Add package gdptools/0.0.23.dev0/gdptools-0.0.23.dev0-py3-none-any.whl

---
+++ packages/gdptools/gdptools-0.0.23.dev0-py3-none-any.whl/gdptools/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for pygeoapi plugin: Gdptools."""
+
+__author__ = "Richard McDonald"
+__email__ = "rmcd@usgs.gov"
+__version__ = "0.0.23-dev0"
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
