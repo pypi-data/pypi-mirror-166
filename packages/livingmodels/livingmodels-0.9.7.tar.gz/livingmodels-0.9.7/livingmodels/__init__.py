9d6fb57899772bdb96217339203bf3ae53023624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:51 +0000

    Add package livingmodels/0.9.7/livingmodels-0.9.7.tar.gz

---
+++ packages/livingmodels/livingmodels-0.9.7.tar.gz/livingmodels-0.9.7/livingmodels/__init__.py
@@ -0,0 +1,7 @@
+from .services_guide_models import *
+from .estate_models import *
+from .house_management_models import *
+from .base_model import Base
+from .order_models import *
+from .files_models import *
+from .service_requests_models import *
