b799ef1ef1a627808894d2457dedde1380237307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:52 +0000

    Add package magnum-api/0.5.3/magnum_api-0.5.3-py3-none-any.whl

---
+++ packages/magnum-api/magnum_api-0.5.3-py3-none-any.whl/magnumapi/geometry/GeometryFactory.py
@@ -0,0 +1,162 @@
+from typing import List, Dict, Union
+
+import pandas as pd
+from roxieapi.cadata.CableDatabase import CableDatabase
+
+from magnumapi.geometry.GeometryBuilder import GeometryBuilder, SlottedGeometryBuilder
+from magnumapi.geometry.Geometry import Geometry
+import pymbse.commons.json_file as json_file
+from roxieapi import api
+
+
+class GeometryFactory:
+    """ GeometryFactory implements a factory design pattern and is used to produce:
+    - rectangular geometry
+    - absolute cos-theta geometry
+    - relative cos-theta geometry
+    - slotted absolute cos-theta geometry
+    - slotted relative cos-theta geometry
+
+    """
+
+    @classmethod
+    def init_geometry(cls, geometry_definition: Union[dict, str], cadata: CableDatabase) -> Geometry:
+        """
+        A class method initializing geometry from a geometry definition. The method supports the following inputs
+        - if geometry_definition is a dict
+            - either a regular geometry
+            - or a slotted geometry
+        - if geometry_definition is a string path
+            - if string is a path to a data file
+                - regular geometry
+            - if string is a path to a json file
+                - either a regular
+                - or a slotted geometry
+        """
+
+        # if geometry_definition is a string and ends with .data
+        # # return geometry initialized with data
+
+        # if geometry is a string and ends with .json
+        # # read json as a dict
+
+        # if the dictionary has keys block_defs, layer_defs, r_aperture
+        # #  initialize a slotted geometry
+        # else
+        # # initialize regular geometry
+
+    @classmethod
+    def init_with_json(cls, json_file_path: str, cadata: CableDatabase) -> Geometry:
+        """ Class method initializing a Geometry instance from a JSON file.
+
+        :param json_file_path: a path to a json file
+        :param cadata: a CableDatabase instance
+        :return: initialized geometry instance
+        """
+        json_content = json_file.read(json_file_path)
+        return cls.init_with_dict(**json_content, cadata=cadata)
+
+    @classmethod
+    def init_with_dict(cls,
+                       block_defs: List[Dict],
+                       layer_defs: List[Dict],
+                       cadata: CableDatabase) -> Geometry:
+        """ Class method initializing a Geometry instance from a list of dictionaries with block definition.
+
+        :param block_defs: a list of dictionaries with geometry definition (block definition)
+        :param layer_defs: a list of dictionaries with layer definitions
+        :param cadata: a CableDatabase instance
+        :return: initialized geometry instance
+        """
+        return GeometryBuilder() \
+            .with_block_defs(block_defs, cadata) \
+            .with_layer_defs(layer_defs) \
+            .build()
+
+    @classmethod
+    def init_slotted_with_json(cls, json_file_path: str, cadata: CableDatabase) -> Geometry:
+        """ Class method initializing a Geometry instance from a JSON file.
+
+        :param json_file_path: a path to a json file
+        :param cadata: a CableDatabase instance
+        :return: initialized geometry instance
+        """
+        json_content = json_file.read(json_file_path)
+        return cls.init_slotted_with_dict(**json_content, cadata=cadata)
+
+    @classmethod
+    def init_slotted_with_dict(cls,
+                               block_defs: List[Dict],
+                               layer_defs: List[Dict],
+                               cadata: CableDatabase,
+                               r_aperture: float) -> Geometry:
+        """ Class method initializing a Geometry instance from a list of dictionaries with block definition.
+
+        :param block_defs: a list of dictionaries with geometry definition (block definition)
+        :param layer_defs: a list of dictionaries with layer definitions
+        :param cadata: a CableDatabase instance
+        :param r_aperture: aperture radius in mm
+        :return: initialized geometry instance
+        """
+        return SlottedGeometryBuilder() \
+            .with_block_defs(block_defs, cadata) \
+            .with_layer_defs(layer_defs) \
+            .with_r_aperture(r_aperture) \
+            .build()
+
+    @classmethod
+    def init_with_data(cls, data_file_path: str, cadata: CableDatabase) -> Geometry:
+        """ Class method initializing a Geometry instance from a DATA ROXIE file.
+
+        :param data_file_path: a path to a json file
+        :param cadata: a CableDatabase instance
+        :return: initialized geometry instance
+        """
+        block_df = api.read_bottom_header_table(data_file_path, keyword='BLOCK')
+        layer_df = api.read_nested_bottom_header_table(data_file_path, keyword='LAYER')
+        return cls.init_with_df(block_df, layer_df, cadata)
+
+    @classmethod
+    def init_with_df(cls, block_df: pd.DataFrame, layer_df, cadata: CableDatabase) -> Geometry:
+        """ Class method initializing a Geometry instance from a dataframe with block definition.
+
+        :param block_df: a dataframe with geometry definition (block definition)
+        :param cadata: a CableDatabase instance
+        :return: initialized geometry instance
+        """
+        return GeometryBuilder() \
+            .with_block_df(block_df, cadata) \
+            .with_layer_df(layer_df) \
+            .build()
+
+    @classmethod
+    def init_slotted_with_data(cls, data_file_path: str, cadata: CableDatabase, r_aperture: float) -> Geometry:
+        """ Class method initializing a Geometry instance from a DATA ROXIE file.
+
+        :param data_file_path: a path to a json file
+        :param cadata: a CableDatabase instance
+        :param r_aperture: aperture radius in mm
+        :return: initialized geometry instance
+        """
+        block_df = api.read_bottom_header_table(data_file_path, keyword='BLOCK')
+        layer_df = api.read_nested_bottom_header_table(data_file_path, keyword='LAYER')
+        return cls.init_slotted_with_df(block_df, layer_df, cadata, r_aperture)
+
+    @classmethod
+    def init_slotted_with_df(cls,
+                             block_df: pd.DataFrame,
+                             layer_df: pd.DataFrame,
+                             cadata: CableDatabase,
+                             r_aperture: float) -> Geometry:
+        """ Class method initializing a Geometry instance from a dataframe with block definition.
+
+        :param block_df: a dataframe with geometry definition (block definition)
+        :param cadata: a CableDatabase instance
+        :param r_aperture: aperture radius in mm
+        :return: initialized geometry instance
+        """
+        return SlottedGeometryBuilder() \
+            .with_block_df(block_df, cadata) \
+            .with_layer_df(layer_df) \
+            .with_r_aperture(r_aperture) \
+            .build()
