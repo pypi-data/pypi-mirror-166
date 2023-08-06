edce66bd3ded0705536377a1921801dac08f87ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:05 +0000

    Add package aiortc-datachannel-only/1.3.2.post2/aiortc_datachannel_only-1.3.2.post2-py3-none-any.whl

---
+++ packages/aiortc-datachannel-only/aiortc_datachannel_only-1.3.2.post2-py3-none-any.whl/aiortc/about.py
@@ -0,0 +1,7 @@
+__author__ = "Olli Paloviita"
+__email__ = "olli.paloviita@gmail.com"
+__license__ = "BSD"
+__summary__ = "Jeremy Lainé's aiortc library with only datachannel support and no extra dependencies"
+__title__ = "aiortc-datachannel-only"
+__uri__ = "https://github.com/ollipal/aiortc-datachannel-only"
+__version__ = "1.3.2post2"
