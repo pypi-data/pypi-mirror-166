1c9206c14bb62ca362a21f586823f6a13099f048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:25 +0000

    Add package h-transport-materials/0.4.1/h_transport_materials-0.4.1.tar.gz

---
+++ packages/h-transport-materials/h_transport_materials-0.4.1.tar.gz/h_transport_materials-0.4.1/h_transport_materials/materials/__init__.py
@@ -0,0 +1,6 @@
+from .material import Material
+from .copper import copper_diffusivities, copper_solubilities
+from .cucrzr import cucrzr_diffusivities, cucrzr_solubilities
+from .tungsten import tungsten_diffusivities, tungsten_solubilities
+from .flinak import flinak_diffusivities, flinak_solubilities
+from .lipb import lipb_diffusivities, lipb_solubilities
