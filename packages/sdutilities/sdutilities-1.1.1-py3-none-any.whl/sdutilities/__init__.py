66f38488f6fefb59ec24e91e199f48661c08bb8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:32 +0000

    Add package sdutilities/1.1.1/sdutilities-1.1.1-py3-none-any.whl

---
+++ packages/sdutilities/sdutilities-1.1.1-py3-none-any.whl/sdutilities/__init__.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+
+# Allow general functions to be immediately available upon import
+
+from sdutilities.general import *
+from sdutilities.logger import *
+from sdutilities.datalink import *
+from sdutilities.acsclient import *
+from sdutilities.awsclient import *
+from sdutilities.matching import *
