c93079f114066d0c83e51e80611063bc6b2062c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:45 +0000

    Add package dsw-config/3.15.1/dsw-config-3.15.1.tar.gz

---
+++ packages/dsw-config/dsw-config-3.15.1.tar.gz/dsw-config-3.15.1/dsw/config/build_info.py
@@ -0,0 +1,17 @@
+# Generated file
+# - do not overwrite
+# - do not include in git
+from collections import namedtuple
+
+BuildInfo = namedtuple(
+    'BuildInfo',
+    ['version', 'built_at', 'sha', 'branch', 'tag'],
+)
+
+BUILD_INFO = BuildInfo(
+    version='v3.15.1~2e9efdb',
+    built_at='2022-09-07 10:05:53Z',
+    sha='2e9efdb70aa3066e86558cb5eb20a76977fa956d',
+    branch='HEAD',
+    tag='v3.15.1',
+)
