6d83a10842fa462415b0a2c6d11e6f86592c4dcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:50 +0000

    Add package psychopy-bids/0.1.0/psychopy_bids-0.1.0-py3-none-any.whl

---
+++ packages/psychopy-bids/psychopy_bids-0.1.0-py3-none-any.whl/psychopy_bids/__init__.py
@@ -0,0 +1,7 @@
+"""
+    Package to support the creation of valid bids-datasets.
+"""
+
+# read version from installed package
+from importlib.metadata import version
+__version__ = version("psychopy_bids")
