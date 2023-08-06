eacaa265c82db965ab386f304fa3e37ca6584540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:41 +0000

    Add package sfkit/0.0.48/sfkit-0.0.48-py3-none-any.whl

---
+++ packages/sfkit/sfkit-0.0.48-py3-none-any.whl/sfkit/cli.py
@@ -0,0 +1,37 @@
+from sfkit.auth.auth import auth
+from sfkit.auth.setup_networking import setup_networking
+from sfkit.encryption_keys.encrypt_data import encrypt_data
+from sfkit.encryption_keys.generate_personal_keys import generate_personal_keys
+from sfkit.parser import get_parser
+from sfkit.protocol.register_data import register_data
+from sfkit.protocol.run_protocol import run_protocol
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+
+    if args.command == "auth":
+        study_title = args.study_title or ""
+        auth(study_title)
+    elif args.command == "networking":
+        setup_networking()
+    elif args.command == "generate_keys":
+        study_title: str = args.study_title or ""
+        generate_personal_keys(study_title)
+    elif args.command == "encrypt_data":
+        encrypt_data()
+    elif args.command == "register_data":
+        geno_binary_file_prefix = args.geno_binary_file_prefix or ""
+        data_path = args.data_path or ""
+        register_data(geno_binary_file_prefix, data_path)
+    elif args.command == "run_protocol":
+        study_title: str = args.study_title or ""
+        phase: str = args.phase or ""
+        run_protocol(study_title, phase)
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/sfkit/sfkit-0.0.48-py3-none-any.whl/sfkit/protocol/utils/helper_functions.py
@@ -0,0 +1,17 @@
+import subprocess
+from sfkit.protocol.utils import constants
+
+from typing import Tuple
+
+
+def get_authentication() -> Tuple[str, str]:
+    with open(constants.AUTH_FILE, "r") as f:
+        email = f.readline().rstrip()
+        study_title = f.readline().rstrip()
+    return email, study_title
+
+
+def run_command(command: str) -> None:
+    if subprocess.run(command, shell=True, executable="/bin/bash").returncode != 0:
+        print(f"Failed to perform command {command}")
+        exit(1)
