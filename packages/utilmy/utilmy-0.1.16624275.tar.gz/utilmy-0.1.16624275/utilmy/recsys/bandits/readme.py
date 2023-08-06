e749693a1f791de50c052d3bd51b3ac9a05f6201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:44 +0000

    Add package utilmy/0.1.16624275/utilmy-0.1.16624275.tar.gz

---
+++ packages/utilmy/utilmy-0.1.16624275.tar.gz/utilmy-0.1.16624275/utilmy/recsys/bandits/readme.py
@@ -0,0 +1,28 @@
+""" All bandits
+Docs:
+
+
+
+
+zozotown
+
+  offline bandit  Offlien Evaluation
+ https://github.com/st-tech/zr-obp
+
+
+
+
+
+
+
+Offline Replay :
+
+    https://jamesrledoux.com/algorithms/offline-bandit-evaluation/
+
+    https://github.com/topics/off-policy-evaluation
+
+
+
+https://edoconti.medium.com/offline-policy-evaluation-run-fewer-better-a-b-tests-60ce8f93fa15
+
+"""
