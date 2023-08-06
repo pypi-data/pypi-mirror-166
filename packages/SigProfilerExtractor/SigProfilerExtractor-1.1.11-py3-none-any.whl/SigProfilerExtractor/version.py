32cc4e20ca03ae9affd84f9139fde9afc4ed8738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:42 +0000

    Add package SigProfilerExtractor/1.1.11/SigProfilerExtractor-1.1.11-py3-none-any.whl

---
+++ packages/SigProfilerExtractor/SigProfilerExtractor-1.1.11-py3-none-any.whl/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.1.11'
+version = '1.1.11'
+Update = '1. Add support for CNV and SV. 2. MutationType consistency. 3. README updates.'
+    
+    
