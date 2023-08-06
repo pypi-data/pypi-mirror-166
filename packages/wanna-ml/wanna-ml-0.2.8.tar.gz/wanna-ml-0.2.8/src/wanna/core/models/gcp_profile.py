ee004228a55f06a22897566be5130efa82660f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:20:48 +0000

    Add package wanna-ml/0.2.8/wanna-ml-0.2.8.tar.gz

---
+++ packages/wanna-ml/wanna-ml-0.2.8.tar.gz/wanna-ml-0.2.8/src/wanna/core/models/gcp_profile.py
@@ -0,0 +1,40 @@
+from typing import Dict, Optional
+
+from pydantic import BaseModel, Extra, root_validator, validator
+
+from wanna.core.utils import validators
+from wanna.core.utils.gcp import get_region_from_zone
+
+
+class GCPProfileModel(BaseModel, extra=Extra.forbid):
+    profile_name: str
+    project_id: str
+    zone: Optional[str]
+    region: str
+    labels: Optional[Dict[str, str]]
+    bucket: str
+    service_account: Optional[str]
+    network: Optional[str]
+    subnet: Optional[str]
+    kms_key: Optional[str]
+    docker_repository: str = "wanna"
+    docker_registry: Optional[str]
+
+    _ = validator("project_id", allow_reuse=True)(validators.validate_project_id)
+    _ = validator("zone", allow_reuse=True)(validators.validate_zone)
+
+    @root_validator(pre=True)
+    def parse_region_from_zone(cls, values):  # pylint: disable=no-self-argument,no-self-use
+        """
+        In some cases, the zone is defined and region not.
+        Region can be easily parsed from zone.
+        """
+        zone, region = (
+            values.get("zone"),
+            values.get("region"),
+        )
+        if (region is None) and (zone is not None):
+            values["region"] = get_region_from_zone(zone)
+        return values
+
+    _ = validator("region", allow_reuse=True)(validators.validate_region)
---
+++ packages/wanna-ml/wanna-ml-0.2.8.tar.gz/wanna-ml-0.2.8/src/wanna/core/models/notebook.py
@@ -0,0 +1,56 @@
+from typing import List, Optional
+
+from pydantic import BaseModel, EmailStr, Extra, Field, root_validator, validator
+
+from wanna.core.models.base_instance import BaseInstanceModel
+from wanna.core.models.gcp_components import GPU, Disk, VMImage
+from wanna.core.utils import validators
+
+
+class BucketMount(BaseModel, extra=Extra.forbid):
+    bucket_name: str
+    mount_path: str = "/gcs"
+
+    _bucket_name = validator("bucket_name")(validators.validate_bucket_name)
+
+
+class NotebookEnvironment(BaseModel, extra=Extra.forbid):
+    vm_image: Optional[VMImage]
+    docker_image_ref: Optional[str]
+
+    _ = root_validator()(validators.validate_only_one_must_be_set)
+
+
+class NotebookModel(BaseInstanceModel):
+    name: str = Field(min_length=3, max_length=63, to_lower=True, regex="^[a-z][a-z0-9-]*[a-z0-9]$")
+    zone: str
+    machine_type: str = "n1-standard-4"
+    environment: NotebookEnvironment = NotebookEnvironment(vm_image=VMImage(framework="common", version="cpu"))
+    instance_owner: Optional[EmailStr]
+    gpu: Optional[GPU]
+    boot_disk: Optional[Disk]
+    data_disk: Optional[Disk]
+    bucket_mounts: Optional[List[BucketMount]]
+    network: Optional[str]
+    subnet: Optional[str]
+    tensorboard_ref: Optional[str]
+    enable_monitoring: bool = True
+    no_public_ip: bool = True
+    no_proxy_access: bool = False
+
+    _machine_type = validator("machine_type")(validators.validate_machine_type)
+
+
+class ManagedNotebookModel(BaseInstanceModel):
+    name: str = Field(min_length=3, max_length=63, to_lower=True, regex="^[a-z][a-z0-9-]*[a-z0-9]$")
+    owner: str
+    machine_type: Optional[str] = "n1-standard-4"
+    gpu: Optional[GPU]
+    data_disk: Optional[Disk]
+    kernel_docker_image_refs: Optional[List[str]]
+    tensorboard_ref: Optional[str]
+    network: Optional[str]
+    subnet: Optional[str]
+    internal_ip_only: Optional[bool] = True
+    idle_shutdown: Optional[bool]
+    idle_shutdown_timeout: Optional[int] = Field(ge=10, le=1440)
