9c41e4c3b1bcf56007be47dbc1aed13f7b4de01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:17 +0000

    Add package so4gp/0.2.8/so4gp-0.2.8.tar.gz

---
+++ packages/so4gp/so4gp-0.2.8.tar.gz/so4gp-0.2.8/src/so4gp/test.py
@@ -0,0 +1,11 @@
+from src.so4gp import so4gp
+import json
+
+out_json, gps = so4gp.rsgps('DATASET.csv', max_iteration=20, return_gps=True)
+# out_json, gps = so4gp.graank('DATASET.csv', return_gps=True)
+# out_json, gps = so4gp.clugps('DATASET.csv', e_probability=0, return_gps=True)
+print(out_json)
+# print(so4gp.compare_gps('DATASET.csv', 0.5, gps))
+
+# out_obj = json.loads(out_json)
+# print(out_obj["Invalid Count"])
