2af9591c5bdef2179bd496702000f7b036e158bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:46 +0000

    Add package edx-name-affirmation/2.3.5/edx_name_affirmation-2.3.5-py2.py3-none-any.whl

---
+++ packages/edx-name-affirmation/edx_name_affirmation-2.3.5-py2.py3-none-any.whl/edx_name_affirmation/__init__.py
@@ -0,0 +1,7 @@
+"""
+Django app housing name affirmation logic.
+"""
+
+__version__ = '2.3.5'
+
+default_app_config = 'edx_name_affirmation.apps.EdxNameAffirmationConfig'  # pylint: disable=invalid-name
