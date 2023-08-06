9fbea59ebbefd095ff3e1ab0c2e0e3b089800414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:51 +0000

    Add package brep-part-finder/0.4.4/brep_part_finder-0.4.4.tar.gz

---
+++ packages/brep-part-finder/brep_part_finder-0.4.4.tar.gz/brep_part_finder-0.4.4/examples/example_finding_part_id_by_center.py
@@ -0,0 +1,18 @@
+import brep_part_finder as bpf
+
+
+# brep file is imported
+my_brep_part_properties = bpf.get_brep_part_properties("ball_reactor.brep")
+
+# from the printed json dictionary we know that there is are two parts with a
+# volume 95467959.26023674 which is the blanket_rear_wall component
+part_id = bpf.get_part_id(
+    brep_part_properties=my_brep_part_properties,
+    center=[0, 0, 0],
+    center_atol=1e-6,
+)
+
+# prints the part id found
+# in this case there are several parts found with this center of mass
+print(f" there is {len(part_id)} with a matching volume")
+print(f" the part id with a matching volume is {part_id}")
