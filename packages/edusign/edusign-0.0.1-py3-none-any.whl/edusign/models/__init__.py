04a613bdb27f24dba3667f0f460c6c72c1fe5ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:53 +0000

    Add package edusign/0.0.1/edusign-0.0.1-py3-none-any.whl

---
+++ packages/edusign/edusign-0.0.1-py3-none-any.whl/edusign/models/__init__.py
@@ -0,0 +1,12 @@
+from edusign.models.students import Students
+from edusign.models.professors import Professors
+from edusign.models.groups import Groups
+from edusign.models.courses import Courses
+
+
+__all__ = [
+    "Students",
+    "Professors",
+    "Groups",
+    "Courses"
+]
