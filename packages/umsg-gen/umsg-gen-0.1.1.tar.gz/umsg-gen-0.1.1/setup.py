ef801ce6db50282b448659d9baa5413fdd3ea3b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:57 +0000

    Add package umsg-gen/0.1.1/umsg-gen-0.1.1.tar.gz

---
+++ packages/umsg-gen/umsg-gen-0.1.1.tar.gz/umsg-gen-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='umsg-gen',
+    version='0.1.1',
+    author="Alex Pabouctsidis",
+    author_email='alex.pabouct@gmail.com',
+    url='https://github.com/Amcolex/umsg_gen.git',
+    py_modules=['umsg_gen'],
+    install_requires=[
+        'jinja2',
+    ],
+    entry_points='''
+        [console_scripts]
+        umsg-gen=umsg_gen:main
+    ''',
+)
