1cd8bfd44f6857ac4e41bfc1bd9a6dee708af8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:24 +0000

    Add package leafmap/0.10.5/leafmap-0.10.5-py2.py3-none-any.whl

---
+++ packages/leafmap/leafmap-0.10.5-py2.py3-none-any.whl/leafmap/__init__.py
@@ -0,0 +1,38 @@
+"""Top-level package for leafmap."""
+
+__author__ = """Qiusheng Wu"""
+__email__ = "giswqs@gmail.com"
+__version__ = "0.10.5"
+
+import os
+
+
+def _in_colab_shell():
+    """Tests if the code is being executed within Google Colab."""
+    import sys
+
+    if "google.colab" in sys.modules:
+        return True
+    else:
+        return False
+
+
+def _use_folium():
+    """Whether to use the folium or ipyleaflet plotting backend."""
+    if os.environ.get("USE_FOLIUM") is not None:
+        return True
+    else:
+        return False
+
+
+if _use_folium():
+    from .foliumap import *
+else:
+    from .leafmap import *
+
+    if _in_colab_shell():
+        from google.colab import output
+
+        output.enable_custom_widget_manager()
+
+from .report import Report
