7b0434369a07850e787d084699e9854817538840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:01 +0000

    Add package githubdata/6.2/githubdata-6.2.tar.gz

---
+++ packages/githubdata/githubdata-6.2.tar.gz/githubdata-6.2/tests/test.py
@@ -0,0 +1,56 @@
+"""
+
+  """
+
+##
+import json
+
+import dulwich.porcelain
+
+from src.githubdata import get_data_from_github
+from src.githubdata import GithubData
+from src.githubdata.main import get_github_token_pathes
+
+
+## the most simple usage
+u = 'https://github.com/imahdimir/d-TSETMC_ID-2-FirmTicker'
+df = get_data_from_github(u)
+
+## test get_github_token_pathes()
+fp = get_github_token_pathes()
+print(fp)
+
+## clone a public repo
+u = 'https://github.com/imahdimir/d-TSETMC_ID-2-FirmTicker'
+repo = GithubData(u)
+repo.overwriting_clone()
+
+##
+repo.rmdir()
+
+## clone a public repo and commit back
+u = 'https://github.com/imahdimir/test-public'
+repo = GithubData(u)
+repo.overwriting_clone()
+
+##
+js = repo.meta
+##
+js['desc'] = 'test'
+with open(repo.meta_fp , 'w') as fi :
+    json.dump(js , fi , indent = 4)
+
+##
+msg = 'test commit'
+repo.commit_and_push(msg)
+
+##
+repo.rmdir()
+
+##
+ur = 'https://github.com/imahdimir/test-private'
+
+
+##
+
+##
