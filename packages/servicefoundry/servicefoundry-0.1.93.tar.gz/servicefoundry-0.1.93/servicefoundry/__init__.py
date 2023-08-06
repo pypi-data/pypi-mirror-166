95439f711ff66f4fa3b1b3d005a503ab7011b049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:30 +0000

    Add package servicefoundry/0.1.93/servicefoundry-0.1.93.tar.gz

---
+++ packages/servicefoundry/servicefoundry-0.1.93.tar.gz/servicefoundry-0.1.93/servicefoundry/__init__.py
@@ -0,0 +1,23 @@
+from servicefoundry.auto_gen.models import (
+    Autoscaling,
+    HealthProbe,
+    Image,
+    Port,
+    Resources,
+)
+from servicefoundry.v2 import (
+    Application,
+    Build,
+    DockerFileBuild,
+    GithubSource,
+    HttpProbe,
+    Job,
+    LocalSource,
+    Manual,
+    Notebook,
+    PythonBuild,
+    RemoteSource,
+    Schedule,
+    Service,
+)
+from servicefoundry.version import __version__
