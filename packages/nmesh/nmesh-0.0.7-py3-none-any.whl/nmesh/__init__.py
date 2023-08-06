08dbb49277e357f5b9ff856efc90265c4a2c3674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:03 +0000

    Add package nmesh/0.0.7/nmesh-0.0.7-py3-none-any.whl

---
+++ packages/nmesh/nmesh-0.0.7-py3-none-any.whl/nmesh/__init__.py
@@ -0,0 +1,6 @@
+
+__version__ = "0.0.7"
+from nmesh.core import *
+from nmesh.functional import *
+
+cfg = load_config()
---
+++ packages/nmesh/nmesh-0.0.7-py3-none-any.whl/nmesh/functional.py
@@ -0,0 +1,14 @@
+from gnutools.fs import load_config as _load_config, parent
+import os
+
+
+def load_config():
+    # Priority is on global variable
+    try:
+        filename = os.environ["NMESH_CONFIG"]
+        cfg = _load_config(filename)
+    # Then default path
+    except:
+        filename = f"{parent(__file__)}/config.yml"
+        cfg = _load_config(filename)
+    return cfg
