66f642e0c463a56e101344c2516d9d39dd5fce99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:28 +0000

    Add package arkindex-base-worker/0.3.0rc6/arkindex-base-worker-0.3.0rc6.tar.gz

---
+++ packages/arkindex-base-worker/arkindex-base-worker-0.3.0rc6.tar.gz/arkindex-base-worker-0.3.0rc6/tests/test_elements_worker/test_training.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+import pytest
+import responses
+
+from arkindex.mock import MockApiClient
+from arkindex_worker.worker import BaseWorker
+from arkindex_worker.worker.training import TrainingMixin, create_archive
+
+
+class TrainingWorker(BaseWorker, TrainingMixin):
+    """
+    This class is only needed for tests
+    """
+
+    pass
+
+
+@pytest.fixture
+def mock_training_worker(monkeypatch):
+    monkeypatch.setattr(sys, "argv", ["worker"])
+    training_worker = TrainingWorker()
+    training_worker.api_client = MockApiClient()
+    training_worker.args = training_worker.parser.parse_args()
+    return training_worker
+
+
+def test_create_archive(model_file_dir):
+    """Create an archive when the model's file is in a folder"""
+
+    with create_archive(path=model_file_dir) as (
+        zst_archive_path,
+        hash,
+        size,
+        archive_hash,
+    ):
+        assert os.path.exists(zst_archive_path), "The archive was not created"
+        assert (
+            hash == "c5aedde18a768757351068b840c8c8f9"
+        ), "Hash was not properly computed"
+        assert 300 < size < 700
+
+    assert not os.path.exists(zst_archive_path), "Auto removal failed"
+
+
+@pytest.mark.parametrize(
+    "tag, description",
+    [
+        ("tag", "description"),
+        (None, "description"),
+        ("", "description"),
+        ("tag", ""),
+        ("", ""),
+        (None, ""),
+    ],
+)
+def test_create_model_version(mock_training_worker, tag, description):
+    """A new model version is returned"""
+
+    model_version_id = "fake_model_version_id"
+    model_id = "fake_model_id"
+    model_hash = "hash"
+    archive_hash = "archive_hash"
+    size = "30"
+    model_version_details = {
+        "id": model_version_id,
+        "model_id": model_id,
+        "hash": model_hash,
+        "archive_hash": archive_hash,
+        "size": size,
+        "tag": tag,
+        "description": description,
+        "s3_url": "http://hehehe.com",
+        "s3_put_url": "http://hehehe.com",
+    }
+
+    mock_training_worker.api_client.add_response(
+        "CreateModelVersion",
+        id=model_id,
+        response=model_version_details,
+        body={
+            "hash": model_hash,
+            "archive_hash": archive_hash,
+            "size": size,
+            "tag": tag,
+            "description": description,
+        },
+    )
+    assert (
+        mock_training_worker.create_model_version(
+            model_id, model_hash, size, archive_hash, tag, description
+        )
+        == model_version_details
+    )
+
+
+@pytest.mark.parametrize(
+    "content, status_code",
+    [
+        (
+            {
+                "hash": {
+                    "id": "fake_model_version_id",
+                    "model_id": "fake_model_id",
+                    "hash": "hash",
+                    "archive_hash": "archive_hash",
+                    "size": "size",
+                    "tag": "tag",
+                    "description": "description",
+                    "s3_url": "http://hehehe.com",
+                    "s3_put_url": "http://hehehe.com",
+                }
+            },
+            400,
+        ),
+        (
+            {
+                "hash": {
+                    "id": "fake_model_version_id",
+                    "model_id": "fake_model_id",
+                    "hash": "hash",
+                    "archive_hash": "archive_hash",
+                    "size": "size",
+                    "tag": None,
+                    "description": "",
+                    "s3_url": "http://hehehe.com",
+                    "s3_put_url": "http://hehehe.com",
+                }
+            },
+            400,
+        ),
+        ({"hash": ["A version for this model with this hash already exists."]}, 403),
+    ],
+)
+def test_retrieve_created_model_version(mock_training_worker, content, status_code):
+    """
+    If there is an existing model version in Created mode,
+    A 400 was raised, but the model is still returned in error content.
+    Else if an existing model version in Available mode,
+    403 was raised, but None will be returned
+    """
+
+    model_id = "fake_model_id"
+    model_hash = "hash"
+    archive_hash = "archive_hash"
+    size = "30"
+    tag = "tag"
+    description = "description"
+    mock_training_worker.api_client.add_error_response(
+        "CreateModelVersion",
+        id=model_id,
+        status_code=status_code,
+        body={
+            "hash": model_hash,
+            "archive_hash": archive_hash,
+            "size": size,
+            "tag": tag,
+            "description": description,
+        },
+        content=content,
+    )
+    if status_code == 400:
+        assert (
+            mock_training_worker.create_model_version(
+                model_id, model_hash, size, archive_hash, tag, description
+            )
+            == content["hash"]
+        )
+    elif status_code == 403:
+        assert (
+            mock_training_worker.create_model_version(
+                model_id, model_hash, size, archive_hash, tag, description
+            )
+            is None
+        )
+
+
+def test_handle_s3_uploading_errors(mock_training_worker, model_file_dir):
+    s3_endpoint_url = "http://s3.localhost.com"
+    responses.add_passthru(s3_endpoint_url)
+    responses.add(responses.Response(method="PUT", url=s3_endpoint_url, status=400))
+    file_path = model_file_dir / "model_file.pth"
+    with pytest.raises(Exception):
+        mock_training_worker.upload_to_s3(file_path, {"s3_put_url": s3_endpoint_url})
