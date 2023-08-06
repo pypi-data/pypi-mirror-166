7123b8e8d0f8d57e48710bc6c34abff09a3f58d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:50 +0000

    Add package Mypackage-665/0.0.1/Mypackage-665-0.0.1.tar.gz

---
+++ packages/Mypackage-665/Mypackage-665-0.0.1.tar.gz/Mypackage-665-0.0.1/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup, find_packages # importam setarile pentru un pachet din pachetul instalat in terminal
+
+
+setup(name='Mypackage-665', description='This is my package', author='me', packages=find_packages(), version='0.0.1')
