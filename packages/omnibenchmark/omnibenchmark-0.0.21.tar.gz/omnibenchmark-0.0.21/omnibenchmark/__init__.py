91f8e9f741fcc12d315294e03b8d35fb9b76a297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:19 +0000

    Add package omnibenchmark/0.0.21/omnibenchmark-0.0.21.tar.gz

---
+++ packages/omnibenchmark/omnibenchmark-0.0.21.tar.gz/omnibenchmark-0.0.21/omnibenchmark/__init__.py
@@ -0,0 +1,8 @@
+"""Omnibenchmark"""
+
+# Version of the omnibenchmark package
+__version__ = "0.0.21"
+
+from omnibenchmark import core, management, renku_commands, utils
+
+__all__ = ("core", "management", "renku_commands", "utils")
