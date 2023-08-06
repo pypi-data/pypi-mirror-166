55872f383dabf9cfb7cd6f9266c3a93ab194636f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:46 +0000

    Add package singlestoredb/0.3.0/singlestoredb-0.3.0.tar.gz

---
+++ packages/singlestoredb/singlestoredb-0.3.0.tar.gz/singlestoredb-0.3.0/singlestoredb/drivers/pymysql.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from typing import Any
+from typing import Callable
+from typing import Dict
+
+try:
+    from pymysql.converters import encoders
+except ImportError:
+    encoders = {}
+
+from .base import Driver
+from ..converters import converters as convs
+
+converters: Dict[Any, Callable[..., Any]] = dict(convs)
+converters.update(encoders)
+
+
+class PyMySQLDriver(Driver):
+
+    name = 'PyMySQL'
+
+    pkg_name = 'pymysql'
+    pypi = 'pymysql'
+    anaconda = 'pymysql'
+
+    def remap_params(self, params: Dict[str, Any]) -> Dict[str, Any]:
+        params.pop('driver', None)
+        params.pop('odbc_driver', None)
+        params.pop('pure_python', None)
+        params.pop('credential_type', None)
+        # This is a dummy set of options used to create the side-effect of
+        # SSL being enabled if the server also has it enabled.
+        params['ssl'] = dict(enable=True)
+        params['port'] = params['port'] or 3306
+        params['conv'] = self.merge_converters(params.pop('converters', {}), converters)
+        return params
+
+    def is_connected(self, conn: Any, reconnect: bool = False) -> bool:
+        return conn.open
