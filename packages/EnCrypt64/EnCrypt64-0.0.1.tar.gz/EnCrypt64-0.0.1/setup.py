16e70cf9f367fabd38d687f01a51a94301263d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:16 +0000

    Add package EnCrypt64/0.0.1/EnCrypt64-0.0.1.tar.gz

---
+++ packages/EnCrypt64/EnCrypt64-0.0.1.tar.gz/EnCrypt64-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='EnCrypt64',
+    version='0.0.1',
+    url='https://github.com/Jacoblightning/EnCrypt64',
+    author='Jacoblightning',
+    author_email='t00niyf5@duck.com',
+    license='GNU',
+    packages=['src.EnCrypt64'])
