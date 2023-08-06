c86fc776985d940d71968332be1a05c4cf68ec19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:10 +0000

    Add package mt3scm/0.4.4/mt3scm-0.4.4.tar.gz

---
+++ packages/mt3scm/mt3scm-0.4.4.tar.gz/mt3scm-0.4.4/mt3scm/__init__.py
@@ -0,0 +1,10 @@
+"""
+MT3SCM
+======
+This module provides 'Multivariate Time Series Sub-Sequence CLustering Metric' computation function
+"""
+__version__ = "0.4.4"
+
+from .mt3scm_computation import MT3SCM, mt3scm_score
+
+__all__ = ["mt3scm_score", "MT3SCM"]
