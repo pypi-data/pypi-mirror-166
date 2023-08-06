e4fe150938b75e4fe5879a92025c98d64161fa6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:14 +0000

    Add package zappadock/0.1.10/zappadock-0.1.10.tar.gz

---
+++ packages/zappadock/zappadock-0.1.10.tar.gz/zappadock-0.1.10/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="zappadock",
+    version='0.1.10',
+    author="Moshe Dicker",
+    author_email='dickermoshe@gmail.com',
+    description="A tool for running Zappa commands in a Lambda-like' environment.",
+    url='https://github.com/dickermoshe/zappadock',
+    license='GNU General Public License v3.0',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'Click',
+        'docker'
+    ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'zappadock = zappadock.zappadock:zappadock',
+        ],
+    },
+    python_requires=">=3.6,!=3.10.*,!=3.11.*",
+)
