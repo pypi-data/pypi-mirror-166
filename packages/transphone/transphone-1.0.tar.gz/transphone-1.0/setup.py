dc786758a4c1d232e2ac91da89f0c2fe2fa2c77a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:18 +0000

    Add package transphone/1.0/transphone-1.0.tar.gz

---
+++ packages/transphone/transphone-1.0.tar.gz/transphone-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+   name='transphone',
+   version='1.0',
+   description='a multilingual g2p/p2g model',
+   author='Xinjian Li',
+   author_email='xinjianl@cs.cmu.edu',
+   url="https://github.com/xinjli/transphone",
+   packages=['transphone'],
+)
