0cf4fccced58b13036377583eaa6a08402b9bf32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:03 +0000

    Add package dune-client/0.0.2/dune_client-0.0.2-py3-none-any.whl

---
+++ packages/dune-client/dune_client-0.0.2-py3-none-any.whl/dune_client/query.py
@@ -0,0 +1,31 @@
+"""
+Data Class Representing a Dune Query
+"""
+import urllib.parse
+from dataclasses import dataclass
+from typing import Optional
+
+from dune_client.types import QueryParameter
+
+
+@dataclass
+class Query:
+    """Basic data structure constituting a Dune Analytics Query."""
+
+    name: str
+    query_id: int
+    params: Optional[list[QueryParameter]] = None
+
+    def base_url(self) -> str:
+        """Returns a link to query results excluding fixed parameters"""
+        return f"https://dune.com/queries/{self.query_id}"
+
+    def parameters(self) -> list[QueryParameter]:
+        """Non-null version of self.params"""
+        return self.params or []
+
+    def url(self) -> str:
+        """Returns a parameterized link to the query"""
+        # Include variable parameters in the URL so they are set
+        query = "&".join([f"{p.key}={p.value}" for p in self.parameters()])
+        return "?".join([self.base_url(), urllib.parse.quote_plus(query, safe="=&?")])
