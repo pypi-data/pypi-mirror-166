20ebe2aa6473935952380f4ec2b03edf3ab0d8cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:41 +0000

    Add package mkrepo/0.1.10/mkrepo-0.1.10.tar.gz

---
+++ packages/mkrepo/mkrepo-0.1.10.tar.gz/mkrepo-0.1.10/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from os import path
+
+from setuptools import setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='mkrepo',
+    packages=[''],
+    version='0.1.10',
+    description='Maintain deb and rpm repos on s3',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Konstantin Nazarov',
+    author_email='mail@kn.am',
+    url='https://github.com/tarantool/mkrepo',
+    keywords=['rpm', 'deb'],
+    classifiers=[],
+    scripts=['mkrepo']
+)
