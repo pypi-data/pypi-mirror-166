a205aba218b6abae9aa4ccb68827cac130419af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:35 +0000

    Add package borys-peex/2.0/borys_peex-2.0.tar.gz

---
+++ packages/borys-peex/borys_peex-2.0.tar.gz/borys_peex-2.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+        name='borys_peex',
+        version='2.0',
+        description='Incrediable and not reproducible python app!!!111',
+        author='borys',
+        author_email='borys@deathstar.com',
+        license="MIT",
+        url="https://long.long.time.ago.com",
+        packages=['borys_peex'],
+        entry_points={
+                'console_scripts': [
+                    'borys_ninja=borys_peex.main:main',
+                ],
+        },
+        # long_description=long_description
+)
