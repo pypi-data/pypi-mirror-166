9cbfd4f40a3581de5ecccd64b61458b03843ca90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:57 +0000

    Add package rhdzmota/1.0.0/rhdzmota-1.0.0.tar.gz

---
+++ packages/rhdzmota/rhdzmota-1.0.0.tar.gz/rhdzmota-1.0.0/src/rhdzmota/cli.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+from .settings import logger_manager
+from .interface.cli import CLIBase
+from .utils.gists import Gist
+
+
+logger = logger_manager.get_logger(name=__name__)
+
+
+class CLI(CLIBase):
+
+    @CLIBase.Formatter()
+    def hello(self, world: Optional[str] = None):
+        logger.debug("CLI Hello command execution.")
+        world = world or "world"
+        return f"Hello, {world}!"
+
+    @CLIBase.Formatter()
+    def gist(
+            self,
+            username: str,
+            gist_id: str,
+            filename: str,
+            commit: Optional[str] = None,
+            save: bool = False
+    ):
+        # Retrieve code
+        code = Gist(username=username, gist_id=gist_id, filename=filename, commit=commit).get_code()
+        # Save code into a file when requested
+        if save:
+            with open(filename, "w") as file:
+                file.write(code)
+        return f"\n\n{code}"
