6dad751c18a255018eac0eb6dc15c6ad97ec713d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:07 +0000

    Add package ontor/0.4.4/ontor-0.4.4.tar.gz

---
+++ packages/ontor/ontor-0.4.4.tar.gz/ontor-0.4.4/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ontor",
+    version="0.4.4",
+    author="Felix Ocker",
+    author_email="felix.ocker@googlemail.com",
+    description="ontor - an ontology editor based on Owlready2",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/felixocker/ontor",
+    project_urls={
+        "Bug Tracker": "https://github.com/felixocker/ontor/issues",
+    },
+    download_url="https://github.com/felixocker/ontor/archive/refs/tags/v0.4.3.tar.gz",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["ontology", "owl"],
+
+    include_package_data=True,  # include non-code files during installation
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+    install_requires=[
+        'networkx',
+        'owlready2',
+        'pandas',
+        'pyvis',
+    ],
+)
+
