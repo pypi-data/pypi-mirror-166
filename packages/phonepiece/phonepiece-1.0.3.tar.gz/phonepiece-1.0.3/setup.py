706a9232f3de60459aa058e3aa027874394da834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:59 +0000

    Add package phonepiece/1.0.3/phonepiece-1.0.3.tar.gz

---
+++ packages/phonepiece/phonepiece-1.0.3.tar.gz/phonepiece-1.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+   name='phonepiece',
+   version='1.0.3',
+   description='a multilingual phone tokenizer',
+   author='Xinjian Li',
+   author_email='xinjianl@cs.cmu.edu',
+   url="https://github.com/xinjli/phonepiece",
+   packages=find_packages(),
+)
