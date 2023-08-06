7bcb47ded920739235dbad70f36c583ba461f292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:14 +0000

    Add package hadata/1.7.9/hadata-1.7.9-py2.py3-none-any.whl

---
+++ packages/hadata/hadata-1.7.9-py2.py3-none-any.whl/hautils/health.py
@@ -0,0 +1,16 @@
+import datetime
+import json
+
+from hadata.meta import MongoDiagnostics
+
+
+def get_diagnostic_record(service):
+    return MongoDiagnostics.objects(service=service).order_by('-timestamp').first()
+
+
+def add_diagnostic_record(service, remark):
+    diagnostic = MongoDiagnostics(service=service)
+    diagnostic.timestamp = datetime.datetime.now().timestamp()
+    diagnostic.remarks = json.dumps(remark)
+    diagnostic.save()
+    return diagnostic.id
