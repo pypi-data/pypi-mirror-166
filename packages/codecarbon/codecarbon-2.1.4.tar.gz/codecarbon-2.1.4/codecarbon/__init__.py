55a9053ed1d7644a019bac7606ceabac4c2d2da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:06 +0000

    Add package codecarbon/2.1.4/codecarbon-2.1.4.tar.gz

---
+++ packages/codecarbon/codecarbon-2.1.4.tar.gz/codecarbon-2.1.4/codecarbon/__init__.py
@@ -0,0 +1,13 @@
+"""
+The Carbon Tracker module. The following objects/decorators belong to the Public API
+"""
+
+from .emissions_tracker import (
+    EmissionsTracker,
+    OfflineEmissionsTracker,
+    track_emissions,
+)
+
+__all__ = ["EmissionsTracker", "OfflineEmissionsTracker", "track_emissions"]
+
+__version__ = "2.1.4"
