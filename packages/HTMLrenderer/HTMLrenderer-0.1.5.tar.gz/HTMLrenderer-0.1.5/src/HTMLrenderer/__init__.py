ff09adac9c6cf044eaaa7c93729c148b9f99812f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:21 +0000

    Add package HTMLrenderer/0.1.5/HTMLrenderer-0.1.5.tar.gz

---
+++ packages/HTMLrenderer/HTMLrenderer-0.1.5.tar.gz/HTMLrenderer-0.1.5/src/HTMLrenderer/__init__.py
@@ -0,0 +1,10 @@
+from HTMLrenderer.youtube import (
+    render_YouTube_video,
+    get_id_and_start_time,
+)
+from HTMLrenderer.site import (
+    is_valid,
+    render_site,
+)
+
+_ = render_site, render_YouTube_video, is_valid, get_id_and_start_time
