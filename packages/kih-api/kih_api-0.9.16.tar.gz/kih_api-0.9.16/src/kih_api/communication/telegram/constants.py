bd08cf23ce5901111f743c051116f2d5585b6df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:29 +0000

    Add package kih-api/0.9.16/kih_api-0.9.16.tar.gz

---
+++ packages/kih-api/kih_api-0.9.16.tar.gz/kih_api-0.9.16/src/kih_api/communication/telegram/constants.py
@@ -0,0 +1,16 @@
+#   Telegram API
+import os
+
+from kih_api import global_common
+
+telegram_url = "https://api.telegram.org/bot<token>/"
+telegram_bot_token = global_common.get_environment_variable("KIH_API_TELEGRAM_BOT_TOKEN")
+telegram_bot_username = "kontinuum_bot"
+telegram_channel_username = global_common.get_environment_variable("TELEGRAM_BOT_USERNAME")
+telegram_channel_development_username = global_common.get_environment_variable("TELEGRAM_BOT_DEV_USERNAME")
+telegram_stocks_data_template = "${Stock}: ${Stock-Price} | ${Stock-PriceChange}"
+telegram_currencies_data_template = "${CurrencyPair}: ${ExchangeRate} | ${ExchangeRateChange}"
+
+#   Telegram API - Methods
+telegram_method_get_chat_id = "getChat"
+telegram_method_send_message = "sendMessage"
