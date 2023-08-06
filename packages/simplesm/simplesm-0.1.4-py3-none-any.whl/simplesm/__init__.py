00cdb9455d1840ebc4d9bab5c7816aedc36f8270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:52 +0000

    Add package simplesm/0.1.4/simplesm-0.1.4-py3-none-any.whl

---
+++ packages/simplesm/simplesm-0.1.4-py3-none-any.whl/simplesm/__init__.py
@@ -0,0 +1,10 @@
+from collections import namedtuple
+from .simplesm import SimpleSM
+
+
+VersionInfo = namedtuple("VersionInfo", ("major", "minor", "patch"))
+
+VERSION = VersionInfo(0, 1, 4)
+
+__version__ = "{0.major}.{0.minor}.{0.patch}".format(VERSION)
+__all__ = [SimpleSM]
