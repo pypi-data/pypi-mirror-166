537edb8dc038ff6bc7104f83e06c667576a2c28e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:55 +0000

    Add package pycognaize/1.1.1/pycognaize-1.1.1-py3-none-any.whl

---
+++ packages/pycognaize/pycognaize-1.1.1-py3-none-any.whl/pycognaize/common/field_collection.py
@@ -0,0 +1,31 @@
+from collections import OrderedDict
+
+from pycognaize.common.lazy_group_dict import LazyGroupDict
+from pycognaize.document.field import Field
+
+
+class FieldCollection(OrderedDict):
+    """Contains fields included in the document by also providing
+    functionality to group fields by group_name and group_key"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._groups = None
+
+    @property
+    def groups(self) -> dict:
+        """Return Groups"""
+        if self._groups is None:
+            self._groups = LazyGroupDict(self).groups
+        return self._groups
+
+    def groups_by_name(self, name: str) -> dict:
+        """Returns fields that are contained in group with the
+        given group name"""
+        if name in self.groups.keys():
+            return self.groups[name]
+        else:
+            raise KeyError
+
+    def groups_by_field(self, field: Field) -> dict:
+        """Returns groups that contain the given field"""
+        return self.groups_by_name(field.group_name)
