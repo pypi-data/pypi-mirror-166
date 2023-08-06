65743b3019a72eaa2c3190b1d0583316cbdb1ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:19 +0000

    Add package ujenkins/0.6.1/ujenkins-0.6.1-py3-none-any.whl

---
+++ packages/ujenkins/ujenkins-0.6.1-py3-none-any.whl/ujenkins/endpoints/__init__.py
@@ -0,0 +1,15 @@
+from ujenkins.endpoints.builds import Builds
+from ujenkins.endpoints.jobs import Jobs
+from ujenkins.endpoints.nodes import Nodes
+from ujenkins.endpoints.plugins import Plugins
+from ujenkins.endpoints.system import System
+from ujenkins.endpoints.views import Views
+
+__all__ = (
+    'Builds',
+    'Jobs',
+    'Nodes',
+    'Plugins',
+    'System',
+    'Views',
+)
