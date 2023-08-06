81fd0b504752158326b85a391acd234d1b86d7ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:39 +0000

    Add package pyg-base/0.0.30/pyg_base-0.0.30-py3-none-any.whl

---
+++ packages/pyg-base/pyg_base-0.0.30-py3-none-any.whl/pyg_base/_cfg.py
@@ -0,0 +1,55 @@
+import os
+import json
+CFG = os.environ.get('PYG_CFG', 'e:/etc/pyg.json')
+_backup = 'c:/etc/pyg.json'
+
+def mkdir(path):
+    """
+    sames as os.mkdir but 
+    1) allows for subdir i.e.:
+    2) will not attempt to create if path exits, hence can run safely
+
+
+    :Example:
+    ---------
+    >>> with pytest.raises(Exception):       
+    >>>     os.mkdir('c:/temp/some/subdir/that/we/want')
+
+    >>> print('but', mkdir('c:/temp/some/subdir/that/we/want'), 'now exists')
+    
+    """
+    if not os.path.isdir(path):
+        p = path.replace('\\', '/')
+        paths = p.split('/')
+        if '.' in paths[-1]:
+            paths = paths[:-1]
+        for i in range(2, len(paths)+1):
+            d = '/'.join(paths[:i])
+            if not os.path.isdir(d):
+                os.mkdir(d)
+    return path
+
+def cfg_read():
+    for path in CFG.split(',') + [_backup]:
+        if os.path.isfile(path):
+            with open(path, 'r') as f:
+                cfg = json.load(f)
+            return cfg
+    else:
+        return {}
+cfg_read.__doc__ = 'reads the config file from %s' % CFG
+
+
+
+def cfg_write(cfg):
+    for path in CFG.split(',') + [_backup]:
+        try:
+            with open(mkdir(path), 'w') as f:
+                json.dump(cfg, f)
+            return
+        except Exception:
+            pass
+        
+cfg_write.__doc__ = 'writes the config file provided to %s' % CFG
+    
+
