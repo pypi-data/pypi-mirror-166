b0f6c027bc696029de1cc98b09aff4d0cd5a8f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:12 +0000

    Add package selene-api/0.0.1.post1/selene_api-0.0.1.post1.tar.gz

---
+++ packages/selene-api/selene_api-0.0.1.post1.tar.gz/selene_api-0.0.1.post1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='selene_api',
+    version='0.0.1post1',
+    packages=['selene_api'],
+    url='https://github.com/OpenVoiceOS/selene_api',
+    license='Apache2',
+    author='jarbasai',
+    author_email='jarbasai@mailfence.com',
+    description='unofficial api for selene backend'
+)
