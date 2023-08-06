fdfba2ec48e498a797896950488716d893fcec58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:43 +0000

    Add package cyberdrop-dl/3.3.15/cyberdrop-dl-3.3.15.tar.gz

---
+++ packages/cyberdrop-dl/cyberdrop-dl-3.3.15.tar.gz/cyberdrop-dl-3.3.15/cyberdrop_dl/scraper/scraper.py
@@ -0,0 +1,23 @@
+import asyncio
+
+from .scraper_helper import ScrapeMapper
+from ..base_functions.base_functions import log
+from ..base_functions.data_classes import AuthData, SkipData
+from ..client.client import Client
+
+
+async def scrape(urls, client: Client, include_id: bool, leakednudes_auth: AuthData, socialmediagirls_auth: AuthData,
+                 simpcity_auth: AuthData, separate_posts: bool, skip_data: SkipData):
+    await log("Starting Scrape")
+
+    scraper = ScrapeMapper(client=client, include_id=include_id, leakednudes_auth=leakednudes_auth,
+                           socialmediagirls_auth=socialmediagirls_auth, simpcity_auth=simpcity_auth,
+                           separate_posts=separate_posts, skip_data=skip_data)
+    tasks = []
+    for link in urls:
+        tasks.append(scraper.map_url(link))
+    await asyncio.gather(*tasks)
+
+    Cascade = scraper.Cascade
+    await Cascade.dedupe()
+    return Cascade
