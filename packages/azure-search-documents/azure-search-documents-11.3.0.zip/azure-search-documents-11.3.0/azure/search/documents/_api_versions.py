1e744b01a7e66f712bbfb83db925309d0a42b2d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:26 +0000

    Add package azure-search-documents/11.3.0/azure-search-documents-11.3.0.zip

---
+++ packages/azure-search-documents/azure-search-documents-11.3.0.zip/azure-search-documents-11.3.0/azure/search/documents/_api_versions.py
@@ -0,0 +1,14 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
+from enum import Enum
+from azure.core import CaseInsensitiveEnumMeta
+
+class ApiVersion(str, Enum, metaclass=CaseInsensitiveEnumMeta):
+    #: this is the default version
+    V2020_06_30 = "2020-06-30"
+
+
+DEFAULT_VERSION = ApiVersion.V2020_06_30
