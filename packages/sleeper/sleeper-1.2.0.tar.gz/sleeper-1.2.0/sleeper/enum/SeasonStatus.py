f4366be96c73c4ad4a64c58904bb2172c505e5c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:59 +0000

    Add package sleeper/1.2.0/sleeper-1.2.0.tar.gz

---
+++ packages/sleeper/sleeper-1.2.0.tar.gz/sleeper-1.2.0/sleeper/enum/SeasonStatus.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from enum import unique
+
+from sleeper.enum.ModelEnum import ModelEnum
+
+
+@unique
+class SeasonStatus(ModelEnum):
+    COMPLETE = "COMPLETE"
+    DRAFTING = "DRAFTING"
+    IN_SEASON = "IN_SEASON"
+    PRE_DRAFT = "PRE_DRAFT"
+
+    @classmethod
+    def from_str(cls, s: str) -> SeasonStatus:
+        if s.upper() == "COMPLETE":
+            return SeasonStatus.COMPLETE
+        elif s.upper() == "DRAFTING":
+            return SeasonStatus.DRAFTING
+        elif s.upper() == "IN_SEASON":
+            return SeasonStatus.IN_SEASON
+        elif s.upper() == "PRE_DRAFT":
+            return SeasonStatus.PRE_DRAFT
+        else:
+            cls._handle_unknown_value(SeasonStatus, s)
