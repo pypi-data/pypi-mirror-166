bc77572dc2a17af453263970990ca95b175e3093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:04 +0000

    Add package ktechpayapi/0.0.1/ktechpayapi-0.0.1-py3-none-any.whl

---
+++ packages/ktechpayapi/ktechpayapi-0.0.1-py3-none-any.whl/ktechpayapi/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+"""Script used to define constants used across codebase."""
+
+TEST_KEY = os.getenv("TEST_KEY", None)
+
+PRODUCTION_KEY = os.getenv("PRODUCTION_KEY", None)
+
+API_URL = "https://pay.ktechhub.com/"
