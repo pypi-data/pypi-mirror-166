5b43dab47893819768ce5778b00fc020f9a99786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:32 +0000

    Add package arkcloud/0.0.5/arkcloud-0.0.5.tar.gz

---
+++ packages/arkcloud/arkcloud-0.0.5.tar.gz/arkcloud-0.0.5/src/arkcloud/web/driver/chrome/chrome.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from arkcloud.web.driver.chrome.service import Service
+from arkcloud.web.driver.chrome.options import Options
+
+
+class Chrome:
+    def __init__(self, **kwargs):
+        service = Service()
+        options = Options(**kwargs)
+        self._driver = webdriver.Chrome(service=service.bind(), options=options.bind())
+        self._kwargs = kwargs
+
+    def bind(self):
+        return self._driver
+
+    def destroy(self):
+        self._driver.quit()
+        self._driver = None
+        self._kwargs = None
+
+    def exists(self):
+        return self._driver is not None
+
+    def __repr__(self):
+        args = ', '.join([f"{k}={repr(v)}" for k, v in self._kwargs.items()])
+        return f"""<Chrome({args})>"""
