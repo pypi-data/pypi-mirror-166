e43ece02a7c7193b0dd3bde7e41c7c0b9265929e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:36 +0000

    Add package olexiybakum/3.0.0/olexiybakum-3.0.0.tar.gz

---
+++ packages/olexiybakum/olexiybakum-3.0.0.tar.gz/olexiybakum-3.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="olexiybakum",
+    version="3.0.0",
+    url="https://t.me/RobbyBakum",
+    license="robbyvan",
+    author="Robby Ivanov",
+    author_email="robbyivash@teml.net",
+    description="Юзербот для Телеграм")
