277b9e3a6bde5e1ee7c6b3845eb885262ee22456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:40 +0000

    Add package torchcommon/0.5.0/torchcommon-0.5.0-py3-none-any.whl

---
+++ packages/torchcommon/torchcommon-0.5.0-py3-none-any.whl/torchcommon/nn/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+@author: Guangyi
+@since: 2021-08-03
+"""
+
+from .attention import BiLinearAttention
+from .attention import DotProductAttention
+from .attention import MLPAttention
+
+from .normalization import LayerNorm
+from .normalization import AdaptiveGroupNorm
---
+++ packages/torchcommon/torchcommon-0.5.0-py3-none-any.whl/torchcommon/optim/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+from .adamx import AdamX
+from .rmsprop import RMSpropW
