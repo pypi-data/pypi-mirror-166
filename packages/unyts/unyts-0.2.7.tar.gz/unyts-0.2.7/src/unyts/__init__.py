f0233ceeff11d39a86a8f2662a7befb4ad4ffb69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:32 +0000

    Add package unyts/0.2.7/unyts-0.2.7.tar.gz

---
+++ packages/unyts/unyts-0.2.7.tar.gz/unyts-0.2.7/src/unyts/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 24 18:24:20 2020
+
+@author: Martín Carlos Araya <martinaraya@gmail.com>
+"""
+
+__version__ = '0.2.7'
+__release__ = 20220908
+
+# from .units.custom import *
+from .units.define import units
+#from ._convert import convertUnit
+#from ._convert import convertUnit_old as convert_old  # mainteined while testing
+from ._convert import convertUnit as convert
+
+def print_path(switch=None):
+    from ._database import unitsNetwork
+    if type(switch) is str and switch.lower().strip() == 'off':
+        switch = False
+    elif type(switch) is str:
+        unitsNetwork.print = bool(switch)
+    else:
+        unitsNetwork.print = False if unitsNetwork.print is True else True
