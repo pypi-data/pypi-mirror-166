8e31fd7b469837bb526360c7e6d2c14a075a6486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:20 +0000

    Add package PasswordGeneratorTool/0.3.0/PasswordGeneratorTool-0.3.0.tar.gz

---
+++ packages/PasswordGeneratorTool/PasswordGeneratorTool-0.3.0.tar.gz/PasswordGeneratorTool-0.3.0/README.md
@@ -0,0 +1,5 @@
+# PasswordGeneratorTool
+
+Simple tool that will generate strong password for You.     
+Remember that input should be in 'int' format.  
+Whole functionality is inside create_password() function
---
+++ packages/PasswordGeneratorTool/PasswordGeneratorTool-0.3.0.tar.gz/PasswordGeneratorTool-0.3.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+import os
+
+
+def read_file(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as file:
+        return file.read()
+
+
+setup(
+    name='PasswordGeneratorTool',
+    version='0.3.0',
+    author='Daniel Czarnecki',
+    author_email='Daniel.Czarnecki.94@wp.pl',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Tool that will generate password from given length',
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown'
+)
