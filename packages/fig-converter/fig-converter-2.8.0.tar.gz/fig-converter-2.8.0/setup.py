24a520910eb61b28317f7df5cb33be389da7011d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:26 +0000

    Add package fig-converter/2.8.0/fig-converter-2.8.0.tar.gz

---
+++ packages/fig-converter/fig-converter-2.8.0.tar.gz/fig-converter-2.8.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from pathlib import Path
+import figconverter
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='fig-converter',
+    description='A python package and CLI to turn videos into GIFs and vice-versa.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=figconverter.__version__,
+    license='MIT',
+    author="blahberi, kamoodi",
+    author_email='droplet.org@gmail.com',
+    packages=["figconverter"],
+    py_modules=["figconverter"],
+    url='https://github.com/dropletOrg/FIG',
+    install_requires=['opencv-python', 'numpy', 'pillow', 'click', 'tqdm', 'pygifsicle', 'more-itertools', 'imageio'],
+    scripts=['fig.py'],
+    keywords='convert converter video gif fig pyfig figpy python-fig fig-python py-fig fig-py caption text edit editor cli package command command-line gif-convert gif-converter tool gif-tool gif-editor gif-editor-tool',
+)
