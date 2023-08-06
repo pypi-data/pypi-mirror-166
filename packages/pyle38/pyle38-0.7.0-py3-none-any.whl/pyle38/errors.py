abc132f60974d07117948609c4cf76e25687d2ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:52 +0000

    Add package pyle38/0.7.0/pyle38-0.7.0-py3-none-any.whl

---
+++ packages/pyle38/pyle38-0.7.0-py3-none-any.whl/pyle38/errors.py
@@ -0,0 +1,14 @@
+class Tile38Error(Exception):
+    pass
+
+
+class Tile38IdNotFoundError(Exception):
+    pass
+
+
+class Tile38KeyNotFoundError(Exception):
+    pass
+
+
+class Tile38NotCaughtUpError(Exception):
+    pass
