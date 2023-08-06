200717fe3009139ba31b625913b7df5e3b782e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:53 +0000

    Add package nicegui/0.8.12/nicegui-0.8.12.tar.gz

---
+++ packages/nicegui/nicegui-0.8.12.tar.gz/nicegui-0.8.12/nicegui/globals.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import asyncio
+import logging
+from typing import TYPE_CHECKING, Awaitable, Callable, List, Union
+
+if TYPE_CHECKING:
+    import justpy as jp
+    from starlette.applications import Starlette
+
+    from .config import Config
+    from .elements.page import Page
+
+app: 'Starlette'
+config: 'Config'
+page_stack: List['Page'] = []
+view_stack: List['jp.HTMLBaseComponent'] = []
+tasks: List[asyncio.tasks.Task] = []
+log: logging.Logger = logging.getLogger('nicegui')
+connect_handlers: List[Union[Callable, Awaitable]] = []
+disconnect_handlers: List[Union[Callable, Awaitable]] = []
+startup_handlers: List[Union[Callable, Awaitable]] = []
+shutdown_handlers: List[Union[Callable, Awaitable]] = []
+pre_evaluation_succeeded: bool = False
