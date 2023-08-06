8ec5a55e4296ddabd56a2a5d8e1ad7647a9a57c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:42 +0000

    Add package robotframework-PuppeteerLibrary/3.1.10/robotframework_PuppeteerLibrary-3.1.10-py3.10.egg

---
+++ packages/robotframework-PuppeteerLibrary/robotframework_PuppeteerLibrary-3.1.10-py3.10.egg/PuppeteerLibrary/keywords/browsermanagement.py
@@ -0,0 +1,292 @@
+import os
+import shutil
+from robot.api import logger
+from PuppeteerLibrary.base.librarycomponent import LibraryComponent
+from PuppeteerLibrary.base.robotlibcore import keyword
+from PuppeteerLibrary.ikeywords.ibrowsermanagement_async import iBrowserManagementAsync
+from robot.libraries.BuiltIn import BuiltIn
+
+
+class BrowserManagementKeywords(LibraryComponent):
+
+    def __init__(self, ctx):
+        super().__init__(ctx)
+        self.STATES_FOLDER = './states'
+
+    def get_async_keyword_group(self) -> iBrowserManagementAsync:
+        return self.ctx.get_current_library_context().get_async_keyword_group(type(self).__name__)
+
+    @keyword
+    def open_browser(self, url, browser="chrome", alias=None, options={}):
+        """Opens a new browser instance to the specific ``url``.
+
+        The ``browser`` argument specifies which browser to use.
+
+        |    = Browser =           |    = Name(s) =   |    = Engine =   |
+        | Google Chrome Default    | chrome           | Playwright      |
+        | Google Chrome Playwright | pwchrome         | Playwright      |
+        | Google Chrome Puppeteer  | ptchrome         | Puppeteer       |
+        | Webkit (Safari engine)   | webkit           | Playwright      |
+        | Firefox                  | firefox          | Playwright      |
+
+        The ``options`` argument as a dictionary
+
+        |    = Property =    |        = Value =       |
+        | headless           | default True           |
+        | width              | default 1366           |
+        | height             | default 768            |
+        | emulate            | iPhone 11              |
+        | state_ref          | State Reference        |
+
+        **Other options**
+        pwchrome, webkit and firefox please visit: https://playwright.dev/python/docs/api/class-browser?_highlight=new_page#browsernew_pagekwargs
+        chrome please visit: https://pptr.dev/#?product=Puppeteer&version=v8.0.0&show=api-puppeteerlaunchoptions
+
+        Example:
+
+        | &{options} =   | create dictionary                             | headless=${False}  |
+        | `Open browser` | https://www.w3schools.com/html/html_forms.asp | options=${options} |
+
+        """
+        if options is None:
+            options = {}
+
+        self.info(url)
+        library_context = self.ctx.get_library_context_by_name(alias)
+        if library_context is None:
+            library_context = self.ctx.create_library_context(alias, browser)
+        self.loop.run_until_complete(
+            self.ctx.set_current_library_context(alias))
+        self.loop.run_until_complete(library_context.start_server(options))
+        self.loop.run_until_complete(library_context.create_new_page(options))
+        self.loop.run_until_complete(self.get_async_keyword_group().go_to(url))
+
+    @keyword
+    def close_window(self):
+        """ Close current browser tab/page
+        """
+        self.loop.run_until_complete(
+            self.ctx.get_current_library_context().close_window())
+
+    @keyword
+    def close_browser(self, alias=None):
+        """Closes the current browser
+        """
+        library_context = self.ctx.get_current_library_context()
+        if alias is not None:
+            library_context = self.ctx.get_library_context_by_name(alias)
+        self.loop.run_until_complete(library_context.close_browser_context())
+
+    @keyword
+    def close_all_browser(self):
+        """Close all browser
+        """
+        library_contexts = self.ctx.get_all_library_context()
+        for library_context in library_contexts:
+            self.loop.run_until_complete(
+                library_context.close_browser_context())
+
+    @keyword
+    def close_puppeteer(self):
+        library_contexts_dict = self.ctx.get_all_library_context_dict()
+        for key in list(library_contexts_dict.keys()):
+            self.loop.run_until_complete(
+                library_contexts_dict[key].stop_server())
+            self.ctx.remove_library_context(key)
+
+    @keyword
+    def get_title(self):
+        """Get page title"""
+        return self.loop.run_until_complete(self.ctx.get_current_library_context().get_current_page().title())
+
+    @keyword
+    def get_location(self):
+        """Get page location"""
+        return self.ctx.get_current_library_context().get_current_page().get_page().url
+
+    @keyword
+    def go_back(self):
+        """Simulate browser go back"""
+        self.loop.run_until_complete(self.get_async_keyword_group().go_back())
+
+    @keyword
+    def go_to(self, url):
+        """Navigates the current page to the ``url``"""
+        self.info(url)
+        self.loop.run_until_complete(self.get_async_keyword_group().go_to(url))
+
+    @keyword
+    def reload_page(self):
+        """Reload the current page"""
+        self.loop.run_until_complete(
+            self.get_async_keyword_group().reload_page())
+
+    @keyword
+    def get_window_count(self):
+        """ Get windows count
+        """
+        return self.loop.run_until_complete(self.get_async_keyword_group().get_window_count())
+
+    @keyword
+    def wait_for_new_window_open(self, timeout=None):
+        """
+        Waits until new page or tab opens.
+
+        Example:
+
+        | Run Async Keywords | Click Element              | id:view_conditions          | AND  |
+        | ...                | `Wait For New Window Open` |                             |      |
+        """
+        self.loop.run_until_complete(
+            self.get_async_keyword_group().wait_for_new_window_open(timeout))
+
+    @keyword
+    def switch_window(self, locator='MAIN'):
+        """Switches to tabs matching locator
+        locator support options NEW, MAIN and query using name, title and url
+            - NEW: latest opened window
+            - MAIN: main window
+            - title="QAHive": window title. Page title will have have error if new tab have auto redirection
+            - url="https://qahive.com": url support regex Example: url=.*qahive.com
+        """
+        self.loop.run_until_complete(
+            self.get_async_keyword_group().switch_window(locator))
+
+    @keyword
+    def switch_browser(self, alias):
+        """Switch browser context based on alias name
+        """
+        return self.loop.run_until_complete(self.ctx.set_current_library_context(alias))
+
+    @keyword
+    def enable_emulate_mode(self, emulate_name):
+        """Emulate specific mobile or tablet
+
+        The ``emulate_name`` argument specifies which emulator to use.
+        Only support for chrome (Puppeteer)
+
+        | = Example Options = |
+        | iPhone X            |
+        | Pixel 2             |
+
+        More emulate_name please visit [device_descriptors.py](https://github.com/qahive/robotframework-puppeteer/tree/master/PuppeteerLibrary/utils/device_descriptors.py)
+
+        """
+        return self.loop.run_until_complete(self.get_async_keyword_group().enable_emulate_mode_async(emulate_name))
+
+    ##############################
+    # Trace
+    ##############################
+    @keyword
+    def start_tracing(self):
+        """Create trace log file
+
+        # View the trace by running following command
+
+        playwright show-trace trace.zip
+        """
+        return self.loop.run_until_complete(self.get_async_keyword_group().start_tracing())
+
+    @keyword
+    def stop_tracing(self, path=None):
+        """Stop trace and generate the trace file.
+
+        Default will be ``traces/<testcasename>.zip``
+        """
+        if path is None:
+            test_name = BuiltIn().get_variable_value("${TEST_NAME}")
+            path = 'traces/'+test_name+'.zip'
+        return self.loop.run_until_complete(self.get_async_keyword_group().stop_tracing(path))
+
+    ##############################
+    # Page
+    ##############################
+    @keyword
+    def set_view_port_size(self, width, height):
+        return self.loop.run_until_complete(self.get_async_keyword_group().set_view_port_size(width, height))
+
+    ##############################
+    # iFrame
+    ##############################
+    @keyword
+    def select_frame(self, locator):
+        return self.loop.run_until_complete(self.get_async_keyword_group().select_frame(locator))
+
+    @keyword
+    def unselect_frame(self):
+        self.get_async_keyword_group().unselect_iframe()
+
+    ##############################
+    # Cookies
+    ##############################
+    @keyword
+    def get_cookie(self, name):
+        """ Get cookie by name
+
+            Returns Cookie value
+        """
+        return self.loop.run_until_complete(self.get_async_keyword_group().get_cookie(name))
+
+    @keyword
+    def get_cookies(self):
+        """ Get all cookies
+
+            Returns Dictionary for all cookies of the current page.
+        """
+        return self.loop.run_until_complete(self.get_async_keyword_group().get_cookies())
+
+    @keyword
+    def add_cookie(self, name, value):
+        """ Add cookie
+        """
+        return self.loop.run_until_complete(self.get_async_keyword_group().add_cookie(name, value))
+
+    @keyword
+    def delete_all_cookies(self):
+        """ Deletes all cookies.
+        """
+        return self.loop.run_until_complete(self.get_async_keyword_group().delete_all_cookies())
+
+    ##############################
+    # State
+    ##############################
+    @keyword
+    def save_browser_storage_state(self, ref='user'):
+        """ Save browser storage state that can resue Authentication state
+
+            *ref* : reference state name
+
+            *Limitation* only support Playwright browser
+        """
+        self.info('Save storate state for ' + ref)
+        try:
+            os.mkdir(self.STATES_FOLDER)
+        except:
+            self.info('states folder already exists.')
+        return self.loop.run_until_complete(self.get_async_keyword_group().save_browser_storage_state(self.STATES_FOLDER, ref))
+
+    @keyword
+    def delete_browser_storage_state(self, ref):
+        """ Delete browser storage state
+
+            *ref* : reference state name
+
+            *Limitation* only support Playwright browser
+        """
+        file_path = self.STATES_FOLDER + '/state-' + ref + '.json'
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        else:
+            self.warn('Can not delete the storate ' +
+                      ref+' as it doesn\'t exists')
+
+    @keyword
+    def delete_all_browser_storage_states(self):
+        """ Delete all browser storage state
+
+            *Limitation* only support Playwright browser
+        """
+        try:
+            shutil.rmtree(self.STATES_FOLDER)
+        except OSError as e:
+            self.warn("Error: %s - %s." % (e.filename, e.strerror))
---
+++ packages/robotframework-PuppeteerLibrary/robotframework_PuppeteerLibrary-3.1.10-py3.10.egg/PuppeteerLibrary/playwright/playwright_context.py
@@ -0,0 +1,198 @@
+import asyncio
+from robot.api import logger
+from PuppeteerLibrary.custom_elements.base_page import BasePage
+from PuppeteerLibrary.playwright.custom_elements.playwright_page import PlaywrightPage
+from PuppeteerLibrary.playwright.async_keywords.playwright_checkbox import PlaywrightCheckbox
+from PuppeteerLibrary.playwright.async_keywords.playwright_mockresponse import PlaywrightMockResponse
+from PuppeteerLibrary.playwright.async_keywords.playwright_formelement import PlaywrightFormElement
+from PuppeteerLibrary.playwright.async_keywords.playwright_dropdown import PlaywrightDropdown
+from PuppeteerLibrary.playwright.async_keywords.playwright_alert import PlaywrightAlert
+from PuppeteerLibrary.playwright.async_keywords.playwright_screenshot import PlaywrightScreenshot
+from PuppeteerLibrary.playwright.async_keywords.playwright_waiting import PlaywrightWaiting
+from PuppeteerLibrary.playwright.async_keywords.playwright_element import PlaywrightElement
+from PuppeteerLibrary.playwright.async_keywords.playwright_dropdown import PlaywrightDropdown
+from PuppeteerLibrary.playwright.async_keywords.playwright_mouseevent import PlaywrightMouseEvent
+from PuppeteerLibrary.playwright.async_keywords.playwright_browsermanagement import PlaywrightBrowserManagement
+from PuppeteerLibrary.playwright.async_keywords.playwright_pdf import PlaywrightPDF
+from PuppeteerLibrary.playwright.async_keywords.playwright_javascript import PlaywrightJavascript
+from PuppeteerLibrary.library_context.ilibrary_context import iLibraryContext
+from PuppeteerLibrary.utils.coverter import str2bool, str2int
+try:
+    from playwright.async_api import async_playwright
+    from playwright.playwright import Playwright as AsyncPlaywright
+    from playwright.browser import Browser
+except ImportError:
+    print('import playwright error')
+
+
+class PlaywrightContext(iLibraryContext):
+
+    playwright: any = None
+    browser: any = None
+    current_context: any = None
+    current_page: any = None
+    current_iframe = None
+
+    page_support_options = ['accept_downloads', 'bypass_csp', 'color_scheme', 'device_scale_factor', 'extra_http_headers', 'geolocation', 'has_touch', 'http_credentials', 'ignore_https_errors', 'is_mobile',
+                            'java_script_enabled', 'locale', 'no_viewport', 'offline', 'permissions', 'proxy', 'record_har_omit_content', 'record_har_path', 'record_video_dir', 'record_video_size', 'timezone_id', 'user_agent', 'viewport']
+
+    def __init__(self, browser_type: str):
+        super().__init__(browser_type)
+
+    async def start_server(self, options: dict = {}):
+        default_options = {
+            'slowMo': 0,
+            'headless': True,
+            'devtools': False,
+            'viewport': {
+                'width': 1366,
+                'height': 768,
+            },
+            'accept_downloads': True
+        }
+        merged_options = default_options
+        merged_options = {**merged_options, **options}
+        for key in merged_options.keys():
+            if key in ['headless', 'devtools', 'accept_downloads', 'is_mobile']:
+                merged_options[key] = str2bool(merged_options[key])
+            elif key == 'width':
+                merged_options['viewport']['width'] = str2int(
+                    merged_options[key])
+            elif key == 'height':
+                merged_options['viewport']['height'] = str2int(
+                    merged_options[key])
+            elif key in ['slowMo']:
+                merged_options[key] = str2int(merged_options[key])
+
+        # Only start new Playwright server. If server didn't start before
+        if self.playwright is None:
+            self.playwright = await async_playwright().start()
+
+        if self.browser_type == "chrome" or self.browser_type == "pwchrome":
+            proxy = None
+            if 'proxy' in merged_options:
+                proxy = merged_options['proxy']
+                # Chromium can use proxy only via global launch
+                del merged_options['proxy']
+            self.browser = await self.playwright.chromium.launch(
+                headless=merged_options['headless'], proxy=proxy)
+        elif self.browser_type == "webkit":
+            self.browser = await self.playwright.webkit.launch(
+                headless=merged_options['headless'])
+        elif self.browser_type == "firefox":
+            self.browser = await self.playwright.firefox.launch(
+                headless=merged_options['headless'])
+        self.browser.accept_downloads = True
+
+    async def stop_server(self):
+        try:
+            await asyncio.wait_for(self.playwright.stop(), timeout=5.0)
+        except Exception:
+            logger.warn('Can\'t stop server properly...')
+        self._reset_server_context()
+
+    def is_server_started(self) -> bool:
+        if self.browser is not None:
+            return True
+        return False
+
+    async def set_default_timeout(self, timeout):
+        self.timeout = timeout
+        self.get_current_page().get_page().set_default_timeout(timeout * 1000)
+
+    async def create_new_page(self, options: dict = {}) -> BasePage:
+        device_options = {
+            'accept_downloads': True,
+            'viewport': {
+                'width': 1366,
+                'height': 768
+            }
+        }
+
+        for support_key in self.page_support_options:
+            if support_key in options:
+                device_options[support_key] = options[support_key]
+                if support_key in ['accept_downloads', 'ignore_https_errors']:
+                    device_options[support_key] = str2bool(
+                        device_options[support_key])
+
+            # Force support viewport
+            if support_key == 'viewport':
+                if 'width' in options.keys():
+                    device_options['viewport']['width'] = str2int(
+                        options['width'])
+                if 'height' in device_options.keys():
+                    device_options['viewport']['height'] = str2int(
+                        options['height'])
+
+        if 'emulate' in options:
+            device_options = self.playwright.devices[options['emulate']]
+
+        if 'state_ref' in options:
+            device_options['storage_state'] = './states/state-' + \
+                options['state_ref'] + '.json'
+
+        new_page = await self.browser.new_page(**device_options)
+        self.current_page = PlaywrightPage(new_page)
+        return self.current_page
+
+    def get_current_page(self) -> BasePage:
+        return self.current_page
+
+    def set_current_page(self, page: any) -> BasePage:
+        self.current_page = PlaywrightPage(page)
+        return self.current_page
+
+    async def get_all_pages(self):
+        return self.browser.contexts[0].pages
+
+    def get_browser_context(self):
+        return self.browser
+
+    async def close_browser_context(self):
+        if self.browser is not None:
+            try:
+                await asyncio.wait_for(self.browser.close(), timeout=5)
+            except asyncio.TimeoutError:
+                None
+        self._reset_context()
+
+    async def close_window(self):
+        await self.get_current_page().get_page().close()
+        pages = await self.get_all_pages()
+        self.set_current_page(pages[-1])
+
+    def get_async_keyword_group(self, keyword_group_name: str):
+        switcher = {
+            "AlertKeywords": PlaywrightAlert(self),
+            "BrowserManagementKeywords": PlaywrightBrowserManagement(self),
+            "CheckboxKeywords": PlaywrightCheckbox(self),
+            "DropdownKeywords": PlaywrightDropdown(self),
+            "ElementKeywords": PlaywrightElement(self),
+            "FormElementKeywords": PlaywrightFormElement(self),
+            "JavascriptKeywords": PlaywrightJavascript(self),
+            "MockResponseKeywords": PlaywrightMockResponse(self),
+            "MouseEventKeywords": PlaywrightMouseEvent(self),
+            "PDFKeywords": PlaywrightPDF(self),
+            "ScreenshotKeywords": PlaywrightScreenshot(self),
+            "WaitingKeywords": PlaywrightWaiting(self)
+        }
+        return switcher.get(keyword_group_name)
+
+    async def start_tracing(self):
+        if len(self.browser.contexts) > 0:
+            await self.browser.contexts[0].tracing.start(screenshots=True, snapshots=True)
+
+    async def stop_tracing(self, path):
+        if len(self.browser.contexts) > 0:
+            await self.browser.contexts[0].tracing.stop(path=path)
+
+    def _reset_context(self):
+        self.browser = None
+        self.current_context = None
+        self.current_page = None
+        self.current_iframe = None
+
+    def _reset_server_context(self):
+        self._reset_context()
+        self.playwright = None
