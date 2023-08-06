90bb9f834788f2e0d387bbd9d599559a8efe2ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:02 +0000

    Add package drug-smile-fet/0.0/drug-smile-fet-0.0.tar.gz

---
+++ packages/drug-smile-fet/drug-smile-fet-0.0.tar.gz/drug-smile-fet-0.0/README.md
@@ -0,0 +1,12 @@
+## Usage
+
+- Make sure you have Python installed in your system.
+- Run Following command in the CMD.
+ ```
+  pip install drug-smile-fet
+  ```
+## Example
+
+ ```
+# example.py
+from  DSFET import oneMolFeatureExtraction
---
+++ packages/drug-smile-fet/drug-smile-fet-0.0.tar.gz/drug-smile-fet-0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='drug-smile-fet',
+    version='0.0',
+    packages=['dsfet'],
+    url='https://github.com/rahulsharma-rs/drug-smile-fet',
+    license='MIT',
+    author='Rahul Sharma',
+    author_email='rahul_rs.sharma@hotmail.com',
+    description='This tool provides methods to extract meaningful features from drug SMILES for Machine Learning operation'
+)
