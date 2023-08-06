6f12183a579f7e3e8de39102891b6f04e1e25ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:44 +0000

    Add package mypackjcal/0.0.1/mypackjcal-0.0.1.tar.gz

---
+++ packages/mypackjcal/mypackjcal-0.0.1.tar.gz/mypackjcal-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+classifiers = [
+   'Development Status:: 5 - Production/Stable',
+   'Intended Audience :: Education',
+   'Operating System :: Microsoft :: Windows :: Windows 10',
+   'Topic :: Utilities',
+   'License :: OSI Approved :: MIT License',
+   'Programming Language :: Python 3.0.9'
+]
+setup(
+    name='mypackjcal',
+    version='0.0.1',
+    description='this is a trial package used for learning.',
+    url='',
+    author='Jayashree',
+    author_email='jayukudari@gmail.com',
+    licence='MIT',
+    packages=['mypackjcal'],
+    install_requires=[]
+)
+
