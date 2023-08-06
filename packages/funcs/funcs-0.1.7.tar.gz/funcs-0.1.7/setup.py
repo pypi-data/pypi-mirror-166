7f4237dad9a832fa212b86a997765b50b9d9a584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:29 +0000

    Add package funcs/0.1.7/funcs-0.1.7.tar.gz

---
+++ packages/funcs/funcs-0.1.7.tar.gz/funcs-0.1.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Read the requirements from the TXT file
+with open(path.join(here, 'requirements.txt')) as f:
+    requirements = [req for req in f.read().split('\n') if not ('#' in req or req == '')]
+
+exec(open('funcs/version.py').read())
+
+setup(
+    name='funcs',
+    version=__version__,
+    author='Ayoub Assis',
+    author_email='assis.ayoub@gmail.com',
+    url='https://github.com/the-beee/utils',
+    license='LICENSE',
+    packages=find_packages(include=['funcs']),
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+    python_requires='>=3.7',
+
+)
