dc4319a7444e4fed303a126d28863f3ee7c4d00b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:03 +0000

    Add package evdsts/1.0rc3/evdsts-1.0rc3.tar.gz

---
+++ packages/evdsts/evdsts-1.0rc3.tar.gz/evdsts-1.0rc3/evdsts/configuration/types.py
@@ -0,0 +1,19 @@
+"""evdsts Defined Custom Types"""
+
+__author__ = "Burak CELIK"
+__copyright__ = "Copyright (c) 2022 Burak CELIK"
+__license__ = "MIT"
+__version__ = "1.0rc3"
+__internal__ = "0.0.1"
+
+
+from datetime import datetime
+from typing import Any, Union, Dict, List
+
+from pandas import Timestamp
+
+#* Define a JSON type for annotating raw returns.
+JSONType = Union[str, int, float, bool, None, List[Any], Dict[str, Any]]
+
+#* Define a DateLike type for date/time operations
+DateLike = Union[None, str, datetime, Timestamp]
