312b74fde1283f6915fab575f0edd49af1a54bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:57 +0000

    Add package optom-tools/0.2.0/optom_tools-0.2.0-py3-none-any.whl

---
+++ packages/optom-tools/optom_tools-0.2.0-py3-none-any.whl/optom_tools/main.py
@@ -0,0 +1,16 @@
+"""Main entry point for optom_tools."""
+
+from optom_tools.prescription import Prescription
+from optom_tools.utils import log
+from optom_tools.visual_acuity import VisualAcuity
+
+__all__ = ["Prescription", "VisualAcuity", "log"]
+
+
+def main():
+    """Entry function."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
