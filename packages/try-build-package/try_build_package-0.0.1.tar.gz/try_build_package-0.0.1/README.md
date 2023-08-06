03f8be6cbdd7c9646402b4cf654ee7b1a34a31f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:55 +0000

    Add package try-build-package/0.0.1/try_build_package-0.0.1.tar.gz

---
+++ packages/try-build-package/try_build_package-0.0.1.tar.gz/try_build_package-0.0.1/README.md
@@ -0,0 +1,2 @@
+# try_build_package
+trying build package that do some mathematical operations
---
+++ packages/try-build-package/try_build_package-0.0.1.tar.gz/try_build_package-0.0.1/try_build_package/math.py
@@ -0,0 +1,13 @@
+def add(n):
+    sum = 0
+    for i in range(1, n+1):
+        sum += i
+    return sum
+
+
+def multiple(n):
+    multi = 1
+    for i in range(1, n+1):
+        multi *= 1
+
+    return multi
