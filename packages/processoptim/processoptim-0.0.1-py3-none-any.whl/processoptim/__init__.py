d603b8278feab282e394853d67b1945a5d243526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:41 +0000

    Add package processoptim/0.0.1/processoptim-0.0.1-py3-none-any.whl

---
+++ packages/processoptim/processoptim-0.0.1-py3-none-any.whl/processoptim/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 08:46:03 2020
+
+@author: HEDI
+"""
+
+PY = 3.14
+from .__version__ import __version__
+#from .module import xl_waterpinch
+#__all__ = ['__version__', "xl_waterpinch"]
