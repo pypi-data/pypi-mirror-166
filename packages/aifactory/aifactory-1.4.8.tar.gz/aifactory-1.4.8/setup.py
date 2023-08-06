aeafc5816a86a3bae32f41e8f3040c98ad11fc8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:37 +0000

    Add package aifactory/1.4.8/aifactory-1.4.8.tar.gz

---
+++ packages/aifactory/aifactory-1.4.8.tar.gz/aifactory-1.4.8/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='aifactory',
+      version='1.4.8',
+      description='aifactory-cli',
+      author='aifactory',
+      author_email='contact@aifactory.page',
+      url='https://aifactory.space',
+      license='MIT',
+      py_modules=['submit'],
+      python_requires='>=3',
+      install_requires=["pipreqs","ipynbname", "gdown", "requests"],
+      packages=['aifactory', 'aifactory.make_zip']
+      )
