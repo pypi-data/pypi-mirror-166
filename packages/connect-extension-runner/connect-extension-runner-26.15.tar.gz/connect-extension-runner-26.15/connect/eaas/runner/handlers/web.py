b47b6bd78f6913977620b0bff70a99f086ede70b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:20 +0000

    Add package connect-extension-runner/26.15/connect-extension-runner-26.15.tar.gz

---
+++ packages/connect-extension-runner/connect-extension-runner-26.15.tar.gz/connect-extension-runner-26.15/connect/eaas/runner/handlers/web.py
@@ -0,0 +1,69 @@
+import logging
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from pkg_resources import iter_entry_points
+
+from connect.eaas.core.decorators import router
+from connect.eaas.runner.config import ConfigHelper
+
+
+logger = logging.getLogger(__name__)
+
+
+class WebApp:
+    """
+    Handle the lifecycle of an extension.
+    """
+    def __init__(self, config: ConfigHelper):
+        self._config = config
+        self._webapp_class = self.get_webapp_class()
+        self._logging_handler = None
+        self._app = None
+
+    @property
+    def config(self):
+        return self._config
+
+    @property
+    def should_start(self):
+        return self._webapp_class is not None
+
+    @property
+    def ui_modules(self):
+        return self._webapp_class.get_descriptor().get('ui', {})
+
+    @property
+    def variables(self):
+        return self._webapp_class.get_variables()
+
+    @property
+    def readme(self):
+        return self._webapp_class.get_descriptor()['readme_url']
+
+    @property
+    def changelog(self):
+        return self._webapp_class.get_descriptor()['changelog_url']
+
+    @property
+    def icon(self):
+        return self._webapp_class.get_descriptor().get('icon')
+
+    @property
+    def app(self):
+        if not self._app:
+            self._app = self.get_asgi_application()
+        return self._app
+
+    def get_asgi_application(self):
+        app = FastAPI(openapi_url='/openapi/spec.json')
+        app.include_router(router)
+        static_root = self._webapp_class.get_static_root()
+        if static_root:
+            app.mount('/static', StaticFiles(directory=static_root), name='static')
+
+        return app
+
+    def get_webapp_class(self):
+        ext_class = next(iter_entry_points('connect.eaas.ext', 'webapp'), None)
+        return ext_class.load() if ext_class else None
