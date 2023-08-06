01c61ea4017a114cf76bb3f09448e7f121222084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:41 +0000

    Add package rolimons/1.2.3/rolimons-1.2.3.tar.gz

---
+++ packages/rolimons/rolimons-1.2.3.tar.gz/rolimons-1.2.3/rolimons/client.py
@@ -0,0 +1,28 @@
+import requests
+
+class Client:
+  def __init__(self, token):
+    self.client = requests.Session()
+    self.client.headers.update({'cookie': token})
+
+  def update_wishlist(self, items):
+    if not isinstance(items, list): items = [items]
+    data = {'asset_ids': items}
+    request = self.client.post('https://www.rolimons.com/tradesettingsapi/updatewishlist', json=data)
+    return request.status_code
+
+  def update_asking(self, items):
+    # format
+    # {item: [tags, ...]}
+      
+    data = {'assets': []}
+    for i, item in enumerate(items):
+      data['assets'].append({'id': item})
+      for option in items[item]:
+        data['assets'][i][option] = True
+    request = self.client.post('https://www.rolimons.com/tradesettingsapi/updateaskinglist', json=data)
+    return request.status_code
+
+  def add_player(self, username):
+    request = self.client.post(f'https://www.rolimons.com/playerapi/addplayer?playername={username}')
+    return request.status_code
