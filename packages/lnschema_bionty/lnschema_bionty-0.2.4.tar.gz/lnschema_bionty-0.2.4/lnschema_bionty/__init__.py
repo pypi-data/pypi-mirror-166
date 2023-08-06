32cc08ba9f6f91531acf85e2b3ebd267e83ef657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:59 +0000

    Add package lnschema_bionty/0.2.4/lnschema_bionty-0.2.4.tar.gz

---
+++ packages/lnschema_bionty/lnschema_bionty-0.2.4.tar.gz/lnschema_bionty-0.2.4/lnschema_bionty/__init__.py
@@ -0,0 +1,43 @@
+"""Schema module for universal biological entities (`zdno`).
+
+Import the package::
+
+   import lnschema_bionty
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   gene
+   protein
+   cell_marker
+   cell_type
+   species
+   tissue
+   disease
+   featureset
+   featureset_gene
+   featureset_protein
+   featureset_cell_marker
+   version_zdno
+
+"""
+# This is lnschema-module zdno.
+_schema_module_id = "zdno"
+__version__ = "0.2.4"
+
+from ._core import (  # noqa
+    cell_marker,
+    cell_type,
+    disease,
+    featureset,
+    featureset_cell_marker,
+    featureset_gene,
+    featureset_protein,
+    gene,
+    protein,
+    species,
+    tissue,
+    version_zdno,
+)
