daca9b64206dca2bf78b8e67bffd4203a295a454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:59 +0000

    Add package ffast/0.4.5/ffast-0.4.5.tar.gz

---
+++ packages/ffast/ffast-0.4.5.tar.gz/ffast-0.4.5/ffast/wordnet/utils.py
@@ -0,0 +1,52 @@
+from typing import Generator, Tuple
+from enum import Enum 
+from pathlib import Path 
+
+import nltk
+
+DATA_PATH = f"{Path(__file__).parent}/nltk_data/corpora"
+if DATA_PATH not in nltk.data.path:
+    nltk.data.path.append(DATA_PATH)
+
+from nltk.corpus import stopwords, wordnet
+from nltk.corpus.reader.wordnet import Synset
+from nltk.corpus.reader.wordnet import ADJ, ADJ_SAT, ADV, NOUN, VERB
+
+from ffast.preprocessor import PreprocessingPipeline
+
+def create_vocabulary() -> Generator[Tuple[str,Synset],None,None]:
+    for meaning in wordnet.all_synsets():
+        for lemma in meaning.lemmas():
+            yield (
+                PREPROCESSOR.normalise(lemma.name()),
+                meaning.name()
+            )
+
+PREPROCESSOR = PreprocessingPipeline()
+STOPWORDS = sorted(stopwords.words('english'))
+VOCABULARY_WORDNET = dict(create_vocabulary())
+VOCABULARY = sorted(map(lambda meaning:meaning.name(),wordnet.all_synsets()))
+SIZE_STOPWORDS = len(STOPWORDS)
+SIZE_WORDNET = len(VOCABULARY)
+METAPHONES = "ABCEFHIJKLMNOPRSTUWXY0. "
+SIZE_METAPHONES = len(METAPHONES)
+SIZE_SEMANTIC_VECTOR = SIZE_STOPWORDS + SIZE_WORDNET 
+SIZE_WORD_VECTOR = SIZE_SEMANTIC_VECTOR + SIZE_METAPHONES 
+SIZE_SENTENCE_VECTOR = 3*SIZE_WORD_VECTOR 
+
+class WordNet(Enum):
+    SKIP = "skip"
+    UNKNOWN = "<Unknown>"
+    SPECIAL = "<Special>"
+    STOPWORD = "<StopWord>"
+    POS_NOUN = "Noun"
+    POS_VERB = "Verb"
+    POS_MAP = {
+        NOUN:"Noun",
+        VERB:"Verb",
+        ADV:"Adverb",
+        ADJ:"Adjective",
+        ADJ_SAT:"Adjective"
+    }
+    EXAMPLE_DELIMITER = "; "
+    SYNSET_NAME_DELIMITER = "."
---
+++ packages/ffast/ffast-0.4.5.tar.gz/ffast-0.4.5/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from glob import glob 
+
+VERSION = '0.4.5'
+DESCRIPTION = 'FFAST: Fast Fourier Analysis for Sentence embeddings and Tokenisation'
+LONG_DESCRIPTION = 'Fast and lightweight NLP pipeline for ML tasks: powerful tokeniser and (model-free) sentence embeddings using Fast Fourier transforms, power means, positional encoding and Wordnet or Poincare Embeddings'
+WORDNET_FILENAMES = glob('wordnet/nltk_data/corpora/wordnet/*')
+
+setup(
+    name="ffast",
+    version=VERSION,
+    author="Mohammed Terry-Jack",
+    author_email="<mohammedterryjack@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    setup_requires = ['nltk'],
+    include_package_data=True,
+    package_data={'':[
+        'poincare/poincare.txt',
+        'wordnet/nltk_data/corpora/stopwords/english',
+    ] + WORDNET_FILENAMES},
+    install_requires=['nltk', 'jellyfish', 'Unidecode', 'numpy', 'scipy'],
+    keywords=['python', 'embedding', 'tokenisation', 'fast fourier', 'nlp', 'nlu', "poincare", "wordnet", "lite", "fast", "sentence encoder"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+    ]
+)
