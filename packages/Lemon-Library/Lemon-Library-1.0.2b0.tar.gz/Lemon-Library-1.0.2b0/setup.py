286f7a4c16b6ea3dabb1b90373be47a737c8039e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:52 +0000

    Add package Lemon-Library/1.0.2b0/Lemon-Library-1.0.2b0.tar.gz

---
+++ packages/Lemon-Library/Lemon-Library-1.0.2b0.tar.gz/Lemon-Library-1.0.2b0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+long_description = open('README.md', "r", encoding="utf-8").read()
+
+setup(
+    name="Lemon-Library",
+    version="1.0.2-beta",
+    packages=find_packages(),
+    author="Sasen Perera",
+    author_email="sas8.communications@gmail.com",
+    description="An experimental Full-Stack Framework for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sas2k/Lemon"
+)
