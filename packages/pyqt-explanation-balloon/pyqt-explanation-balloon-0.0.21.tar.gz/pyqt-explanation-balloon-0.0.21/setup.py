40ed79994e4e981eb22c29d51e5f07696bb1c8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:28 +0000

    Add package pyqt-explanation-balloon/0.0.21/pyqt-explanation-balloon-0.0.21.tar.gz

---
+++ packages/pyqt-explanation-balloon/pyqt-explanation-balloon-0.0.21.tar.gz/pyqt-explanation-balloon-0.0.21/setup.py
@@ -0,0 +1,26 @@
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+setup(
+    name='pyqt-explanation-balloon',
+    version='0.0.21',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    package_data={'pyqt_explanation_balloon.ico': ['close.svg']},
+    description='PyQt explanation balloon',
+    url='https://github.com/yjg30737/pyqt-explanation-balloon.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    install_requires=[
+        'PyQt5>=5.8'
+    ]
+)
