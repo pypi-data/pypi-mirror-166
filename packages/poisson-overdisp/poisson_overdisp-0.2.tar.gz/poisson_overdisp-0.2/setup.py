572f7bc271da5f1aef16f06b3c24c52c75bd066e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:40 +0000

    Add package poisson-overdisp/0.2/poisson_overdisp-0.2.tar.gz

---
+++ packages/poisson-overdisp/poisson_overdisp-0.2.tar.gz/poisson_overdisp-0.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(name='poisson_overdisp',
+      packages=['poisson_overdisp'],
+      version='0.2',
+      description='Overdispersion test for statsmodels poisson model',
+      url='https://github.com/helderprado/overdisp.git',
+      download_url='https://github.com/helderprado/overdisp/archive/refs/tags/0.1.tar.gz',
+      author='Luiz Paulo Fávero',
+      author_email='lpfavero@usp.br',
+      keywords=['statsmodels', 'overdisp'],
+      license='MIT',  # YOUR LICENSE HERE!
+
+      install_requires=['statsmodels'],  # YOUR DEPENDENCIES HERE
+
+
+      classifiers=[
+          # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+          'License :: OSI Approved :: MIT License',  # Your License Here
+          # List Python versions that you support Here
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+      ],
+      )
