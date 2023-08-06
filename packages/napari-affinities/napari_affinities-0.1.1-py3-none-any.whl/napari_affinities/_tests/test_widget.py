e288c3a3dd27c02cba7adc705f6fdd3ae6a45704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:05 +0000

    Add package napari-affinities/0.1.1/napari_affinities-0.1.1-py3-none-any.whl

---
+++ packages/napari-affinities/napari_affinities-0.1.1-py3-none-any.whl/napari_affinities/_tests/test_widget.py
@@ -0,0 +1,18 @@
+# from napari_affinities.widgets.affinities import ModelWidget
+# from napari_affinities.widgets.mutex_watershed import mutex_watershed_widget
+
+
+def test_train_affinities_widget(make_napari_viewer, capsys):
+    pass
+    # viewer = make_napari_viewer()
+    # model_widget = ModelWidget(viewer)
+    # my_widget = model_widget.create_train_widget(viewer)
+
+
+def test_mutex_watershed_widget(make_napari_viewer, capsys):
+    pass
+    # viewer = make_napari_viewer()
+    # offsets = [[1, 0, 0]] * 5
+    # layer = viewer.add_image(np.random.random((5, 100, 100)), metadata={"offsets": offsets})
+    # my_widget = mutex_watershed_widget()
+    # my_widget(viewer.layers[0])
---
+++ packages/napari-affinities/napari_affinities-0.1.1-py3-none-any.whl/napari_affinities/gp/nodes/__init__.py
@@ -0,0 +1,6 @@
+from .napari_image_source import NapariImageSource
+from .napari_label_source import NapariLabelsSource
+from .ones_source import OnesSource
+from .binarize import Binarize
+from .np_source import NpArraySource
+from .add_local_shape_descriptor import AddLocalShapeDescriptor
