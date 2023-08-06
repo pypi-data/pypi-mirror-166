4d3bd0c287eee4f207f2667e8151fafe51162b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:02 +0000

    Add package systemathics.apis/0.10.27rc0/systemathics.apis-0.10.27rc0.tar.gz

---
+++ packages/systemathics.apis/systemathics.apis-0.10.27rc0.tar.gz/systemathics.apis-0.10.27rc0/setup.py
@@ -0,0 +1,26 @@
+import os
+import setuptools
+
+from setuptools import setup, find_packages
+
+setup(
+    name="systemathics.apis",
+    version="0.10.27-pre",
+    author="Systemathics",
+    author_email="contact@systemathics.com",
+    description="Python grpc stub for Systemathics APIs.",
+    url="https://github.com/systemathics/sdk-python",
+    packages=find_packages(),
+    namespace_packages=['systemathics'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research"
+    ],
+    license='MIT',
+    python_requires='>=3.6',
+    install_requires=['googleapis-common-protos', 'protobuf', 'grpcio'],
+)
