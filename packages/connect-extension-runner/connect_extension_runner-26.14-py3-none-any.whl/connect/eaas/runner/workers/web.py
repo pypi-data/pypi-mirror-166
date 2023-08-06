98848d90c6bb0c0ec3ae6e00200280265a6c0fdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:55 +0000

    Add package connect-extension-runner/26.14/connect_extension_runner-26.14-py3-none-any.whl

---
+++ packages/connect-extension-runner/connect_extension_runner-26.14-py3-none-any.whl/connect/eaas/runner/workers/web.py
@@ -0,0 +1,82 @@
+#
+# This file is part of the Ingram Micro CloudBlue Connect EaaS Extension Runner.
+#
+# Copyright (c) 2022 Ingram Micro. All Rights Reserved.
+#
+import asyncio
+import signal
+
+from connect.eaas.core.proto import (
+    Message,
+    MessageType,
+    SetupRequest,
+)
+from connect.eaas.runner.asgi import RequestResponseCycle
+from connect.eaas.runner.workers.base import WorkerBase
+from connect.eaas.runner.helpers import get_version
+
+
+class WebWorker(WorkerBase):
+    """
+    The EventsWorker is responsible to handle the websocket connection
+    with the server. It will send the extension capabilities to
+    the server and wait for tasks that need to be processed using
+    the tasks manager.
+    """
+
+    def get_url(self):
+        return self.config.get_webapp_ws_url()
+
+    def get_setup_request(self):
+        return Message(
+            version=2,
+            message_type=MessageType.SETUP_REQUEST,
+            data=SetupRequest(
+                ui_modules=self.handler.ui_modules,
+                variables=self.handler.variables,
+                repository={
+                    'readme_url': self.handler.readme,
+                    'changelog_url': self.handler.changelog,
+                },
+                icon=self.handler.icon,
+                runner_version=get_version(),
+            ),
+        ).dict()
+
+    async def stopping(self):
+        pass
+
+    async def process_message(self, data):
+        message = Message.deserialize(data)
+        if message.message_type == MessageType.SETUP_RESPONSE:
+            self.process_setup_response(message.data)
+        elif message.message_type == MessageType.WEB_TASK:
+            await self.process_task(message.data)
+        elif message.message_type == MessageType.SHUTDOWN:
+            await self.shutdown()
+
+    async def shutdown(self):
+        await super().shutdown()
+
+    async def process_task(self, task):
+        cycle = RequestResponseCycle(
+            self.config,
+            self.handler.app,
+            task,
+            self.send,
+        )
+        asyncio.create_task(cycle())
+
+
+def start_webapp_worker_process(handler):
+    worker = WebWorker(handler)
+    loop = asyncio.get_event_loop()
+    loop.add_signal_handler(
+        signal.SIGINT,
+        worker.handle_signal,
+    )
+    loop.add_signal_handler(
+        signal.SIGTERM,
+        worker.handle_signal,
+    )
+    loop.run_until_complete(worker.start())
