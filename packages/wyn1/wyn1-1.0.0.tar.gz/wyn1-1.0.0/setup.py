236acc4a91a24527a60fd0b4441c0b7f3f36563d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:52 +0000

    Add package wyn1/1.0.0/wyn1-1.0.0.tar.gz

---
+++ packages/wyn1/wyn1-1.0.0.tar.gz/wyn1-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='wyn1',  # 对外模块的名字
+    version='1.0.0',  # 版本号
+    description='sssssss',  # 描述
+    author='dgw',  # 作者
+    author_email='535646343@qq.com',
+    py_modules=['wyn1.wyn'],  # 要发布的模块
+)
