e9c9b25d221869c0d9f3d10986fefe664a096c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:24 +0000

    Add package cardano-explorer/0.5.0b0/cardano_explorer-0.5.0b0-py2-none-any.whl

---
+++ packages/cardano-explorer/cardano_explorer-0.5.0b0-py2-none-any.whl/cardano_explorer/blockfrost/config.py
@@ -0,0 +1,9 @@
+bf_osenv_testnet_apiKey = "BLOCKFROST_TESTNET_API_KEY"
+bf_osenv_mainnet_apiKey = "BLOCKFROST_MAINNET_API_KEY"
+bf_osenv_preview_apiKey = "BLOCKFROST_PREVIEW_API_KEY"
+bf_osenv_preprod_apiKey = "BLOCKFROST_PREPROD_API_KEY"
+
+bf_osenv = {"testnet":bf_osenv_testnet_apiKey,
+           "mainnet":bf_osenv_mainnet_apiKey,
+           "preview":bf_osenv_preview_apiKey,
+           "preprod":bf_osenv_preprod_apiKey} 
