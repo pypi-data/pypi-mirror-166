4d2b011ae3b511f98055bef4920710ec687593a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:11 +0000

    Add package kuzukiri/0.1.3/kuzukiri-0.1.3-cp37-cp37m-macosx_12_0_x86_64.whl

---
+++ packages/kuzukiri/kuzukiri-0.1.3-cp37-cp37m-macosx_12_0_x86_64.whl/kuzukiri/__init__.py
@@ -0,0 +1,22 @@
+"""
+kuzukiri is a rule-based text segmenter for Japanese.
+
+
+## Example
+```
+import kuzukiri
+
+segmenter = kuzukiri.Segmenter()
+text = "これはテストです。文分割します。"
+sentences = segmenter.split(text)
+print(sentences)  # => ['これはテストです。', '文分割します。']
+```
+"""
+
+from .kuzukiri import *  # noqa
+
+__copyright__ = 'Copyright (C) ALINEAR Corporation'
+__version__ = '0.1.3'
+__license__ = 'MIT'
+__author__ = "ALINEAR Corporation"
+__url__ = "https://github.com/alinear-corp/kuzukiri"
