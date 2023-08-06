fb720a4a2fe5d31129d6bf148fd3b4ddc4794dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:39 +0000

    Add package polars-lts-cpu/0.14.9/polars_lts_cpu-0.14.9.tar.gz

---
+++ packages/polars-lts-cpu/polars_lts_cpu-0.14.9.tar.gz/polars_lts_cpu-0.14.9/local_dependencies/polars-lazy/src/physical_plan/executors/slice.rs
@@ -0,0 +1,23 @@
+use polars_core::prelude::*;
+
+use crate::physical_plan::state::ExecutionState;
+use crate::prelude::*;
+
+pub struct SliceExec {
+    pub input: Box<dyn Executor>,
+    pub offset: i64,
+    pub len: IdxSize,
+}
+
+impl Executor for SliceExec {
+    fn execute(&mut self, state: &mut ExecutionState) -> Result<DataFrame> {
+        #[cfg(debug_assertions)]
+        {
+            if state.verbose() {
+                println!("run SciceExec")
+            }
+        }
+        let df = self.input.execute(state)?;
+        Ok(df.slice(self.offset, self.len as usize))
+    }
+}
