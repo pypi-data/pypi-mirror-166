3fd82e52bb41a5d6379d1658b62423415997ef9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:05 +0000

    Add package octomizer-sdk/0.10.0/octomizer_sdk-0.10.0-py3-none-any.whl

---
+++ packages/octomizer-sdk/octomizer_sdk-0.10.0-py3-none-any.whl/octomizer/package_type.py
@@ -0,0 +1,9 @@
+import enum
+
+
+class PackageType(enum.Enum):
+    """An enum representing the types of possible packages."""
+
+    PYTHON_PACKAGE = "PYTHON_PACKAGE"
+    LINUX_SHARED_OBJECT = "LINUX_SHARED_OBJECT"
+    DOCKER_BUILD_TRITON = "DOCKER_BUILD_TRITON"
