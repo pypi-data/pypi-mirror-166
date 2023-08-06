2fd35652d28504dd45d3973c29d01cb1c1abdfd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:28 +0000

    Add package greetings-from-k4sdo/0.1.0/greetings_from_k4sdo-0.1.0.tar.gz

---
+++ packages/greetings-from-k4sdo/greetings_from_k4sdo-0.1.0.tar.gz/greetings_from_k4sdo-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="greetings_from_k4sdo",
+    version='0.1.0',
+    author="DominikaK",
+    author_email='dominika.kasel@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    description="to we can greet you"
+)
+
+
