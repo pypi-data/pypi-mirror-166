256e00153ab40874139cfc1ebbbc5850cc49a538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:47 +0000

    Add package lnschema_wetlab/0.3.6/lnschema_wetlab-0.3.6-py2.py3-none-any.whl

---
+++ packages/lnschema_wetlab/lnschema_wetlab-0.3.6-py2.py3-none-any.whl/lnschema_wetlab/_techsample.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+from sqlmodel import Field, SQLModel
+
+
+class techsample(SQLModel, table=True):  # type: ignore
+    """Tech samples that are generated due to instrument units."""
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    external_id: str = Field(default=None, index=True)
+    name: str = Field(default=None, index=True)
+    batch: Optional[str] = None
