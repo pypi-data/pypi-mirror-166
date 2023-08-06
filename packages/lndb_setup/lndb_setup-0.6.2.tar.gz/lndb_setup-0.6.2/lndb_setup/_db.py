ffcd7470e1cbdaaa097dd86d61b60411e0924e6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:25 +0000

    Add package lndb_setup/0.6.2/lndb_setup-0.6.2.tar.gz

---
+++ packages/lndb_setup/lndb_setup-0.6.2.tar.gz/lndb_setup-0.6.2/lndb_setup/_db.py
@@ -0,0 +1,94 @@
+import lnschema_core as schema_core
+import sqlmodel as sqm
+
+from ._settings_load import load_or_create_instance_settings
+
+
+class insert_if_not_exists:
+    """Insert data if it does not yet exist.
+
+    A wrapper around the `insert` class below.
+    """
+
+    @classmethod
+    def user(cls, email, user_id, handle):
+        settings = load_or_create_instance_settings()
+        engine = settings.db_engine()
+        with sqm.Session(engine) as session:
+            user = session.get(schema_core.user, user_id)
+        if user is None:
+            user_id = insert.user(email, user_id, handle)  # type: ignore
+            # do not update sqlite on the cloud as this happens within
+            # insert.user
+
+        return user_id
+
+    @classmethod
+    def storage(cls, root, region):
+        root = str(root)
+        settings = load_or_create_instance_settings()
+        engine = settings.db_engine()
+        with sqm.Session(engine) as session:
+            storage = session.exec(
+                sqm.select(schema_core.storage).where(schema_core.storage.root == root)
+            ).first()
+        if storage is None:
+            root = insert.storage(root, region)  # type: ignore
+
+        return root
+
+
+class insert:
+    """Insert data."""
+
+    @classmethod
+    def version_yvzi(cls, version, user_id):
+        """User."""
+        settings = load_or_create_instance_settings()
+        engine = settings.db_engine()
+
+        with sqm.Session(engine) as session:
+            row = schema_core.version_yvzi(v=version, user_id=user_id)
+            session.add(row)
+            session.commit()
+
+        settings._update_cloud_sqlite_file()
+
+    @classmethod
+    def user(cls, email, user_id, handle):
+        """User."""
+        settings = load_or_create_instance_settings()
+        engine = settings.db_engine()
+
+        with sqm.Session(engine) as session:
+            user = schema_core.user(id=user_id, email=email, handle=handle)
+            session.add(user)
+            session.commit()
+            session.refresh(user)
+
+        settings._update_cloud_sqlite_file()
+
+        return user.id
+
+    @classmethod
+    def storage(cls, root, region):
+        """Storage."""
+        settings = load_or_create_instance_settings()
+        engine = settings.db_engine()
+
+        if str(root).startswith("s3://"):
+            storage_type = "s3"
+        elif str(root).startswith("gs://"):
+            storage_type = "gs"
+        else:
+            storage_type = "local"
+
+        with sqm.Session(engine) as session:
+            storage = schema_core.storage(root=root, region=region, type=storage_type)
+            session.add(storage)
+            session.commit()
+            session.refresh(storage)
+
+        settings._update_cloud_sqlite_file()
+
+        return storage.root
