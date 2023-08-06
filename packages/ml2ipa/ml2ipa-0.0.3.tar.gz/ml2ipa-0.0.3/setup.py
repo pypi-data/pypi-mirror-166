ef61faebba92385ca7d840d3660e7087f452e58c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:03 +0000

    Add package ml2ipa/0.0.3/ml2ipa-0.0.3.tar.gz

---
+++ packages/ml2ipa/ml2ipa-0.0.3.tar.gz/ml2ipa-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="ml2ipa",
+	version="0.0.3",
+	description="A linguistic tool that generates IPA pronounciation for Malayalam words",
+	author="Elaine Mary Rose",
+	author_email="ammuelaine@gmail.com",
+	url="https://github.com/erose311/ml2ipa",
+	packages=find_packages(),
+	install_requires=['mlphon', 'ml-to-en'],
+	download_url="https://github.com/erose311/ml2ipa",
+	license="MIT",
+	classifiers=[
+		"Intended Audience :: Developers",
+		"Programming Language :: Python :: 3.9",
+	]
+)
