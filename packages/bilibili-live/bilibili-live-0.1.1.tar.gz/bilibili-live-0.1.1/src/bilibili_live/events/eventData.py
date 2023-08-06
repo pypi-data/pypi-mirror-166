70f70612ef94c84675221973374047251efbf9ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:58 +0000

    Add package bilibili-live/0.1.1/bilibili-live-0.1.1.tar.gz

---
+++ packages/bilibili-live/bilibili-live-0.1.1.tar.gz/bilibili-live-0.1.1/src/bilibili_live/events/eventData.py
@@ -0,0 +1,48 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class BaseData:
+    timestamp: int
+
+
+@dataclass
+class FansMedal:
+    name: str
+    level: int
+
+
+@dataclass
+class User:
+    uid: int
+    uname: str
+    uface: str = None
+    fans_medal: FansMedal = None
+    guard_level: int = None
+
+
+@dataclass
+class Danmu(BaseData):
+    # 用户名
+    user: User
+    msg: str
+
+
+@dataclass
+class Gift(BaseData):
+    user: User
+    gift_id: int
+    gift_name: str
+    gift_num: int
+    price: int
+    coin_type: str
+
+
+@dataclass
+class SuperChat(BaseData):
+    user: User
+    msg_id: int
+    msg: str
+    price: int
+    time: int
+    msg_jpn: str = ""
