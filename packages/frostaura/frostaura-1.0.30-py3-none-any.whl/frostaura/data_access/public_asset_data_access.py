b6bfa0911e02a0f388f1ac20a9deb0b3c1a3cf39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:55:47 +0000

    Add package frostaura/1.0.30/frostaura-1.0.30-py3-none-any.whl

---
+++ packages/frostaura/frostaura-1.0.30-py3-none-any.whl/frostaura/data_access/public_asset_data_access.py
@@ -0,0 +1,15 @@
+'''This module defines public asset data access components.'''
+import pandas as pd
+
+class IPublicAssetDataAccess:
+    '''Component to perform functions related to public assets.'''
+
+    def get_symbol_history(self, symbol: str) -> pd.DataFrame:
+        '''Get historical price movements for a given symbol.'''
+
+        raise NotImplementedError()
+
+    def augment_symbols_info(self, symbols: pd.DataFrame) -> pd.DataFrame:
+        '''Add info to a daraframe containing symbols.'''
+
+        raise NotImplementedError()
