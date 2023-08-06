eb9d1a2e049f33da719499d226bed859dbbc4c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:43 +0000

    Add package flask-bigapp/1.0.7/Flask-BigApp-1.0.7.tar.gz

---
+++ packages/flask-bigapp/Flask-BigApp-1.0.7.tar.gz/Flask-BigApp-1.0.7/src/flask_bigapp/__init__.py
@@ -0,0 +1,9 @@
+from .BigApp import BigApp as BigApp
+from .Blueprint import BigAppBlueprint as Blueprint
+from .Security import BigAppSecurity as Security
+from .Auth import Auth as Auth
+from .Auth import PasswordGeneration as PasswordGeneration
+
+"""
+See docs.md for documentation
+"""
