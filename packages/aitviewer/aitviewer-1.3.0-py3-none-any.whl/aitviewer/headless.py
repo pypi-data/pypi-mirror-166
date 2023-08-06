fe6ce6fa6c4c70f95aecab624fe6881bdae2ed3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:58 +0000

    Add package aitviewer/1.3.0/aitviewer-1.3.0-py3-none-any.whl

---
+++ packages/aitviewer/aitviewer-1.3.0-py3-none-any.whl/aitviewer/headless.py
@@ -0,0 +1,54 @@
+"""
+Copyright (C) 2022  ETH Zurich, Manuel Kaufmann, Velko Vechev
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from aitviewer.viewer import Viewer
+
+
+class HeadlessRenderer(Viewer):
+    gl_version = (4, 0)
+    samples = 0  # Headless rendering does not like super sampling.
+    window_type = 'headless'
+
+    def __init__(self, **kwargs):
+        """
+        Initializer.
+        :param frame_dir: Where to save the frames to.
+        :param kwargs: kwargs.
+        """
+        super().__init__(**kwargs)
+
+    def run(self, frame_dir=None, video_dir=None, output_fps=60):
+        """
+        Convenience method to run the headless rendering.
+        :param frame_dir: Where to store the individual frames or None if you don't care.
+        :param video_dir: If set will automatically generate a video from the images found in `frame_dir`. Must
+          be specified if `frame_dir` is None.
+        :param output_fps: Fps of the output video, if None uses 60fps as default
+        """
+        if frame_dir is None and video_dir is None:
+            raise ValueError("You should either specify a path where to render the images to or where to "
+                             "save the video to.")
+
+        if video_dir is not None:
+            assert video_dir.endswith(".mp4")
+
+        self._init_scene()
+        self.export_video(
+            output_path=video_dir, 
+            frame_dir=frame_dir,
+            animation=True,
+            output_fps=output_fps,
+        )
---
+++ packages/aitviewer/aitviewer-1.3.0-py3-none-any.whl/aitviewer/shaders/chessboard.glsl
@@ -0,0 +1,73 @@
+#version 400
+
+#include directional_lights.glsl
+
+#if defined VERTEX_SHADER
+
+    uniform mat4 mvp;
+
+    in vec3 in_position;
+    in vec3 in_normal;
+    in vec2 in_uv;
+
+    out vec3 v_vert;
+    out vec3 v_norm;
+    out vec2 v_uv;
+    out vec4 v_vert_light[NR_DIR_LIGHTS];
+
+    void main() {
+        v_vert = in_position;
+        v_norm = in_normal;
+        v_uv = in_uv;
+        gl_Position = mvp * vec4(in_position, 1.0);
+        for(int i = 0; i < NR_DIR_LIGHTS; i++) {
+            v_vert_light[i] = dirLights[i].matrix * vec4(in_position, 1.0);
+        }
+    }
+
+
+
+#elif defined FRAGMENT_SHADER
+
+    uniform vec4 color_1;
+    uniform vec4 color_2;
+    uniform float n_tiles;
+    uniform bool tiling_enabled;
+
+    in vec3 v_vert;
+    in vec3 v_norm;
+    in vec2 v_uv;
+    in vec4 v_vert_light[NR_DIR_LIGHTS];
+
+    out vec4 f_color;
+
+    // Box filtered checkerboard by Inigo Quilez: 
+    // https://iquilezles.org/articles/filterableprocedurals/
+    vec4 checkerboard(vec4 c1, vec4 c2, vec2 p) {
+        vec2 w = max(abs(dFdx(p)), abs(dFdy(p))) + 0.01; 
+        vec2 i = 2.0 * (abs(fract((p - 0.5 * w) / 2.0) - 0.5) - abs(fract((p + 0.5 * w) / 2.0) - 0.5)) / w;
+        float weight = 0.5 - 0.5 * i.x * i.y;
+        return mix(c1, c2, weight);
+    }
+
+    void main() {
+        // Compute normal
+        vec3 normal = normalize(v_norm);
+
+        // Compute base color from UVs and chessboard parameters
+        vec4 t_color;
+        if(tiling_enabled) {
+            t_color = checkerboard(color_1, color_2, v_uv * n_tiles);   
+        } else {
+            t_color = color_1;
+        }
+
+        // Compute shadow value
+        vec3 color = compute_lighting(t_color.rgb, v_vert, normal, v_vert_light);
+        
+        // Output resulting color with the original alpha value
+        f_color = vec4(color, t_color.a);
+    }
+
+
+#endif
