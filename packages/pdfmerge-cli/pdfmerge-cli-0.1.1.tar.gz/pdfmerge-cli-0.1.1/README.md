623ba2f94d0f6b566756eed6b3dff390b935d644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:44 +0000

    Add package pdfmerge-cli/0.1.1/pdfmerge-cli-0.1.1.tar.gz

---
+++ packages/pdfmerge-cli/pdfmerge-cli-0.1.1.tar.gz/pdfmerge-cli-0.1.1/README.md
@@ -0,0 +1,24 @@
+# PDF Merge Tool
+
+## Build
+
+```bash
+$ poetry build
+$ pip install dist/pdfmerg-x.x.x.tar.gz
+```
+
+## Install
+
+```bash
+$ pip install pdfmerge-cli
+```
+
+## Usage
+
+```bash
+$ pdfmerge  --input-dir .
+```
+
+## Refs
+
+- [py-pdf/PyPDF2](https://github.com/py-pdf/PyPDF2)
---
+++ packages/pdfmerge-cli/pdfmerge-cli-0.1.1.tar.gz/pdfmerge-cli-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pdfmerge']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyPDF2>=2.10.4,<3.0.0', 'click>=8.1.3,<9.0.0']
+
+entry_points = \
+{'console_scripts': ['pdfmerge = pdfmerge.main:pdf_merge']}
+
+setup_kwargs = {
+    'name': 'pdfmerge-cli',
+    'version': '0.1.1',
+    'description': 'pdf merge command line',
+    'long_description': '# PDF Merge Tool\n\n## Build\n\n```bash\n$ poetry build\n$ pip install dist/pdfmerg-x.x.x.tar.gz\n```\n\n## Install\n\n```bash\n$ pip install pdfmerge-cli\n```\n\n## Usage\n\n```bash\n$ pdfmerge  --input-dir .\n```\n\n## Refs\n\n- [py-pdf/PyPDF2](https://github.com/py-pdf/PyPDF2)\n',
+    'author': 'maguowei',
+    'author_email': 'i.maguowei@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/maguowei/pdfmerge',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
