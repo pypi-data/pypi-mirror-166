567c0feeb80e0808b6019bc3304c2208bf135265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:01 +0000

    Add package my-bmi-project/0.0.0.1/my_bmi_project-0.0.0.1.tar.gz

---
+++ packages/my-bmi-project/my_bmi_project-0.0.0.1.tar.gz/my_bmi_project-0.0.0.1/my_bmi_project/calculate_bmi.py
@@ -0,0 +1,4 @@
+def calculate_bmi(centimeter, kilogram) :
+    height = centimeter * 0.01
+    weight = kilogram
+    return weight / (height * height)
---
+++ packages/my-bmi-project/my_bmi_project-0.0.0.1.tar.gz/my_bmi_project-0.0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'my_bmi_project',
+    version = '0.0.0.1',
+    description = 'My bmi project',
+    author = 'far_away',
+    author_email = 'atker14@gmail.com',
+    url = "https://www.naver.com",
+    license = 'MIT',
+    py_modules = ['calculate_bmi'],
+    python_requires = '>=3',
+    packages = ['my_bmi_project']
+)
