c34d82f7f2079b9182317d06dd26c4936ca71286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:50 +0000

    Add package kani-fork-pynetbox/6.6.2/kani-fork-pynetbox-6.6.2.tar.gz

---
+++ packages/kani-fork-pynetbox/kani-fork-pynetbox-6.6.2.tar.gz/kani-fork-pynetbox-6.6.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    version='6.6.2',
+    name="kani-fork-pynetbox",
+    description="NetBox API client library",
+    url="https://github.com/kani999/pynetbox",
+    author="Zach Moody",
+    author_email="zmoody@do.co",
+    license="Apache2",
+    include_package_data=True,
+    #use_scm_version=True,
+    #setup_requires=["setuptools_scm"],
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    install_requires=[
+        "requests>=2.20.0,<3.0",
+        "six==1.*",
+    ],
+    zip_safe=False,
+    keywords=["netbox"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
