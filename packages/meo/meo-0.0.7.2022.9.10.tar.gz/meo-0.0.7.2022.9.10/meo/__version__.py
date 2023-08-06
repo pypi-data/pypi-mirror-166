bfca45ef64411a37620f27452f0ff072a4954345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:53 +0000

    Add package meo/0.0.7.2022.9.10/meo-0.0.7.2022.9.10.tar.gz

---
+++ packages/meo/meo-0.0.7.2022.9.10.tar.gz/meo-0.0.7.2022.9.10/meo/__version__.py
@@ -0,0 +1,11 @@
+"""
+package declaration
+"""
+__title__ = 'meo'
+__description__ = 'frequently-used function library'
+__version__ = '0.0.7.2022.9.10'
+__url__ = "http://github.com/miaobuao/meo"
+__author__ = 'miaobuao'
+__author_email__ = '617802189@qq.com'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2022 miaobuao'
