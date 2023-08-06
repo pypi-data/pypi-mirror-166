27a04e830f88a22b44ed6059f9c46deff779ec4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:11 +0000

    Add package librapid/0.5.5/librapid-0.5.5-pp37-pypy37_pp73-macosx_10_9_x86_64.whl

---
+++ packages/librapid/librapid-0.5.5-pp37-pypy37_pp73-macosx_10_9_x86_64.whl/librapid/array/functors/functors.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "binary/bitwise.hpp"
+#include "binary/scalarArithmetic.hpp"
+#include "unary/negate.hpp"
+#include "matrix/transpose.hpp"
+#include "misc/fill.hpp"
+#include "assignOp.hpp"
