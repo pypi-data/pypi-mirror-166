80385a88579dac18b68c18c74d5f5d5b048c29e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:34 +0000

    Add package Federated-Learning-Client/0.0.5/Federated-Learning-Client-0.0.5.tar.gz

---
+++ packages/Federated-Learning-Client/Federated-Learning-Client-0.0.5.tar.gz/Federated-Learning-Client-0.0.5/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="Federated-Learning-Client",
+    version="0.0.5",
+    author="Bichebi",
+    author_email="mgach18@freeuni.edu.ge",
+    description="Client package for Collaborative learning framework for deep neural network",
+    packages=["Client"],
+    url="https://github.com/sHOTa-23/Collaborative-Learning-Framework-for-Deep-Neural-Network"
+)
