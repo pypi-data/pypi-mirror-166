33f2ccc96cd8d79e6b377f394099042c2b5b4105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:43 +0000

    Add package modin/0.15.3/modin-0.15.3.tar.gz

---
+++ packages/modin/modin-0.15.3.tar.gz/modin-0.15.3/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import versioneer
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+dask_deps = ["dask>=2.22.0,<2022.2.0", "distributed>=2.22.0,<2022.2.0"]
+# TODO: remove redis dependency when resolving GH#4398
+# Pin ray to < 1.13.0 to work around GH#4564
+# TODO(https://github.com/modin-project/modin/issues/4564): let ray go past 1.13.0.
+# Following https://github.com/ray-project/ray/pull/25648, pin protobuf < 4,
+# because versions >= 4.0.0 are incompatible with ray<1.13.0.
+ray_deps = [
+    "ray[default]>=1.4.0,<1.13.0",
+    "pyarrow>=4.0.1",
+    "redis>=3.5.0,<4.0.0",
+    "protobuf<4.0.0",
+]
+remote_deps = ["rpyc==4.1.5", "cloudpickle", "boto3"]
+spreadsheet_deps = ["modin-spreadsheet>=0.1.0"]
+sql_deps = ["dfsql>=0.4.2", "pyparsing<=2.4.7"]
+all_deps = dask_deps + ray_deps + remote_deps + spreadsheet_deps
+
+setup(
+    name="modin",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Modin: Make your pandas code run faster by changing one line of code.",
+    packages=find_packages(exclude=["scripts", "scripts.*"]),
+    include_package_data=True,
+    license="Apache 2",
+    url="https://github.com/modin-project/modin",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["pandas==1.4.4", "packaging", "numpy>=1.18.5", "fsspec", "psutil"],
+    extras_require={
+        # can be installed by pip install modin[dask]
+        "dask": dask_deps,
+        "ray": ray_deps,
+        "remote": remote_deps,
+        "spreadsheet": spreadsheet_deps,
+        "sql": sql_deps,
+        "all": all_deps,
+    },
+    python_requires=">=3.8",
+)
