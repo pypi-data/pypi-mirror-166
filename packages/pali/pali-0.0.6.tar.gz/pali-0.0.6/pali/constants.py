2282b4a650ccfd8f30d76e360c4d671b29a1da74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:53 +0000

    Add package pali/0.0.6/pali-0.0.6.tar.gz

---
+++ packages/pali/pali-0.0.6.tar.gz/pali-0.0.6/pali/constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+'''
+This module holds package level constants for Pali.
+'''
+
+
+# Config module constants
+CONFIG_FILE_PATH = '/etc/pali/pali.cfg'
+CONFIG_DEFAULT_SECTION = 'DEFAULT'
+CONFIG_COMMON_SECTION = 'COMMON'
+CONFIG_USE_PARAMS = True
+
+# Logger constants
+LOG_FILE='pali.log'
