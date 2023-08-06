14c6d85331722367a4d65c33ded3013e64a0bdd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:18 +0000

    Add package mtworker/0.0.6/mtworker-0.0.6-py3-none-any.whl

---
+++ packages/mtworker/mtworker-0.0.6-py3-none-any.whl/mtworker/tasks/helloworld.py
@@ -0,0 +1,16 @@
+from celery import shared_task,chord, group, signature, uuid
+import requests
+import logging
+import time
+from ..taskUtil import help1
+from celery.utils.log import get_task_logger
+logger = get_task_logger(__name__)
+
+@shared_task
+def helloworld(hello="hello1", world="word1"):
+    return {
+        "hello": hello,
+        "world":world
+    }  
+    
+    
