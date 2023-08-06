38f8d94dfd8251b2ca854894f7dcaa6bd71bd2c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:11 +0000

    Add package treedb/2.4.4/treedb-2.4.4-py3-none-any.whl

---
+++ packages/treedb/treedb-2.4.4-py3-none-any.whl/treedb/backend/__init__.py
@@ -0,0 +1,17 @@
+"""SQLite3 database engine."""
+
+try:
+    import sqlparse
+except ImportError:  # pragma: no cover
+    sqlparse = None
+
+from ._basics import (print_versions,
+                      set_engine, connect,
+                      scalar, iterrows,
+                      expression_compile)
+
+__all__ = ['sqlparse',
+           'print_versions',
+           'set_engine', 'connect',
+           'scalar', 'iterrows',
+           'expression_compile']
