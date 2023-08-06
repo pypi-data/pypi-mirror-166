080bf15a6bc0f1db44ad7767b5b05cc0b2dcfdd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:03 +0000

    Add package punpy/0.33/punpy-0.33.tar.gz

---
+++ packages/punpy/punpy-0.33.tar.gz/punpy-0.33/punpy/__init__.py
@@ -0,0 +1,16 @@
+from punpy.digital_effects_table.measurement_function import MeasurementFunction
+from punpy.lpu.lpu_propagation import LPUPropagation
+from punpy.mc.mc_propagation import MCPropagation
+from ._version import get_versions
+
+# load functionality of comet_maths as this used to be in punpy, and is generally useful as part of punpy
+
+"""___Authorship___"""
+__author__ = "Pieter De Vis"
+__created__ = "01/03/2020"
+__maintainer__ = "Pieter De Vis"
+__email__ = "pieter.de.vis@npl.co.uk"
+__status__ = "Development"
+
+__version__ = get_versions()["version"]
+del get_versions
