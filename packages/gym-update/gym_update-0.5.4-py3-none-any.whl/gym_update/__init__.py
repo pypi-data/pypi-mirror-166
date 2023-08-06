0cbc8cb9ad9ee31f0044a1e4f608b23459371ed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:08 +0000

    Add package gym-update/0.5.4/gym_update-0.5.4-py3-none-any.whl

---
+++ packages/gym-update/gym_update-0.5.4-py3-none-any.whl/gym_update/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from gym.envs.registration import register
+
+register(
+    id='update-v0',
+    entry_point='gym_update.envs:UpdateEnv',
+)
+
