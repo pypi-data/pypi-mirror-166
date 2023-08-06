c51fd5924518dfa5ad87e4253b6009443ea1a0d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:25 +0000

    Add package xj-location/1.1.0/xj_location-1.1.0.tar.gz

---
+++ packages/xj-location/xj_location-1.1.0.tar.gz/xj_location-1.1.0/xj_location/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class LocationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'xj_location'
+    verbose_name = u'定位模块'
