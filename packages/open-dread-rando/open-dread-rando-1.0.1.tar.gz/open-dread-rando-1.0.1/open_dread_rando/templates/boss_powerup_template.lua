2894a3b477bb9a4c9a7475b9b9fb46108598946f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:29 +0000

    Add package open-dread-rando/1.0.1/open-dread-rando-1.0.1.tar.gz

---
+++ packages/open-dread-rando/open-dread-rando-1.0.1.tar.gz/open-dread-rando-1.0.1/open_dread_rando/templates/boss_powerup_template.lua
@@ -0,0 +1,10 @@
+
+local original_TEMPLATE("funcname") = TEMPLATE("scenario").TEMPLATE("funcname")
+function TEMPLATE("scenario").TEMPLATE("funcname")(TEMPLATE("args"))
+    Game.LogWarn(0, "Starting boss callback")
+    original_TEMPLATE("funcname")(TEMPLATE("args"))
+    Game.LogWarn(0, "Granting item")
+    TEMPLATE("pickup_class").OnPickedUp(nil)
+    TEMPLATE("extra_code")
+    Game.LogWarn(0, "Item granted")
+end
