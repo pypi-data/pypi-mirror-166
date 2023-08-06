e3074e41b64c1b56fd39509d726ca911e379a2a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:47 +0000

    Add package BraketLab/1.68/BraketLab-1.68-py3-none-any.whl

---
+++ packages/BraketLab/BraketLab-1.68-py3-none-any.whl/braketlab/__init__.py
@@ -0,0 +1,14 @@
+from .braketlab import * #ket, kinetic_operator,onebody_coulomb_operator, twobody_coulomb_operator, operator
+#import basisbank 
+
+__all__ = [
+    'basisfunction',
+    'ket',
+    'kinetic_operator',
+    'onebody_coulomb_operator',
+    'twobody_coulomb_operator',
+    'operator'
+]
+
+__version__ = '0.0.1-alpha'
+
