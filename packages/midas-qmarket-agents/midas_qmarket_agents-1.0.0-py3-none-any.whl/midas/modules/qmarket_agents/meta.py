1d1a136d51aa74a2878dec345dbc00f3a9f2c0de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:29 +0000

    Add package midas-qmarket-agents/1.0.0/midas_qmarket_agents-1.0.0-py3-none-any.whl

---
+++ packages/midas-qmarket-agents/midas_qmarket_agents-1.0.0-py3-none-any.whl/midas/modules/qmarket_agents/meta.py
@@ -0,0 +1,15 @@
+META = {
+    "type": "time-based",
+    "models": {
+        "MarketAgentModel": {
+            "public": True,
+            "params": ["eid", "unit_type", "s_max", "start_date", "step_size"],
+            "attrs": [
+                "schedule",
+                "reactive_power_offer",
+                "q_set",
+                "set_q_schedule",
+            ],
+        },
+    },
+}
