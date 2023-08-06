5a7bd64ba5315cd2716f7e1e2a4b5127cfba4703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:36 +0000

    Add package minimono/0.2.1/minimono-0.2.1.tar.gz

---
+++ packages/minimono/minimono-0.2.1.tar.gz/minimono-0.2.1/minimono/__init__.py
@@ -0,0 +1,12 @@
+from .API import Client as Client
+from .API import MonoCaller as MonoCaller
+from .API import Account as Account
+from .API import Transaction as Transaction
+from .API import UserInfoResp as UserInfoResp
+from .API import StatementResp as StatementResp
+from .API import CurrencyInfo as CurrencyInfo
+from .API import HeadersPrivate as HeadersPrivate
+from .API import StatementReq as StatementReq
+from .API import UserInfoReq as UserInfoReq
+from .API import CurrRateReq as CurrRateReq
+from .API import construct_bucket_list as construct_bucket_list
---
+++ packages/minimono/minimono-0.2.1.tar.gz/minimono-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['minimono', 'minimono.API']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.10.1,<2.0.0', 'requests>=2.28.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'minimono',
+    'version': '0.2.1',
+    'description': 'A wrapper for monobanks API. With caching and serialization.',
+    'long_description': None,
+    'author': 'Arthur Ryzhak',
+    'author_email': 'ryzhakar@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
