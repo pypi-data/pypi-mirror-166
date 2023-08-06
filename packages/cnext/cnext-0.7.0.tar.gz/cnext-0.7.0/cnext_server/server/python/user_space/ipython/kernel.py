65833691a83066c1fa055b752d10c64c773dae56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:56 +0000

    Add package cnext/0.7.0/cnext-0.7.0.tar.gz

---
+++ packages/cnext/cnext-0.7.0.tar.gz/cnext-0.7.0/cnext_server/server/python/user_space/ipython/kernel.py
@@ -0,0 +1,165 @@
+import threading
+import time
+import traceback
+import jupyter_client
+import queue
+from user_space.ipython.constants import IPythonConstants
+from libs import logs
+log = logs.get_logger(__name__)
+
+MESSSAGE_TIMEOUT = 1
+
+class IPythonKernel():
+    def __init__(self):
+        self.km = jupyter_client.KernelManager()
+        self.km.start_kernel()
+        self.kc = self.km.blocking_client()
+        self.wait_for_ready()
+
+        self.stop_stream_thread = False
+        self.shell_msg_thread = threading.Thread(
+            target=self.handle_ipython_stream, args=(IPythonConstants.StreamType.SHELL,), daemon=True)
+        self.iobuf_msg_thread = threading.Thread(
+            target=self.handle_ipython_stream, args=(IPythonConstants.StreamType.IOBUF,), daemon=True)
+        self.shell_msg_thread.start()
+        self.iobuf_msg_thread.start()
+
+        self.message_handler_callback = None
+        ## This lock is used to make sure only one execution is being executed at any moment in time #
+        self.execute_lock = threading.Lock()
+        self._set_execution_complete_condition(False)
+
+    def shutdown_kernel(self):
+        try:
+            if self.km.is_alive():
+                log.info('Kernel shutting down')
+                self.kc.stop_channels()
+                self.km.shutdown_kernel(now=True)
+                log.info('Kernel shutdown')
+        except:
+            trace = traceback.format_exc()
+            log.info("Exception %s" % (trace))
+
+    def restart_kernel(self):
+        try:
+            # if self.km.is_alive():
+            log.info('Kernel restarting')
+            self.km.restart_kernel()
+            self.stop_stream_thread = True
+            self.kc = self.km.blocking_client()
+            self.wait_for_ready()
+            # wait to make sure the stream threads will stop before proceeding
+            while self.shell_msg_thread.is_alive() or self.shell_msg_thread.is_alive():
+                time.sleep(1)
+            self.shell_msg_thread = threading.Thread(
+                target=self.handle_ipython_stream, args=(IPythonConstants.StreamType.SHELL,), daemon=True)
+            self.iobuf_msg_thread = threading.Thread(
+                target=self.handle_ipython_stream, args=(IPythonConstants.StreamType.IOBUF,), daemon=True)
+            self.stop_stream_thread = False
+            self.shell_msg_thread.start()
+            self.iobuf_msg_thread.start()
+            log.info('Kernel restarted')
+
+            # release execution lock which might be locked during an execution
+            if self.execute_lock.locked():
+                self.execute_lock.release()
+                log.info('Kernel execution lock released')
+                self._set_execution_complete_condition(True)
+                
+            if self.km.is_alive():
+                return True
+            else:
+                return False
+        except:
+            trace = traceback.format_exc()
+            log.info("Exception %s" % (trace))
+        return False
+
+    def interrupt_kernel(self):
+        try:
+            if self.km.is_alive():
+                self.km.interrupt_kernel()
+                log.info('Interupt kernel')
+            return True
+        except:
+            trace = traceback.format_exc()
+            log.info("Exception %s" % (trace))
+        return False
+
+    def is_alive(self):
+        return self.kc.is_alive()
+
+    def wait_for_ready(self):
+        try:
+            self.kc.wait_for_ready(timeout=50)
+        except RuntimeError:
+            self.shutdown_kernel()
+
+    def _is_status_message(self, message):
+        return message['header']['msg_type'] == 'status'
+
+    def _set_execution_complete_condition(self, condition: bool):
+        self.shell_cond = condition
+        self.iobuf_cond = condition
+
+    def _set_execution_complete_condition_from_message(self, stream_type, message):
+        if stream_type == IPythonConstants.StreamType.SHELL:
+            self.shell_cond = message['header']['msg_type'] == 'execute_reply' and 'status' in message['content']
+        if stream_type == IPythonConstants.StreamType.IOBUF:
+            self.iobuf_cond = message['header']['msg_type'] == 'status' and message['content']['execution_state'] == 'idle'
+
+    def _is_execution_complete(self, stream_type, message) -> bool:
+        ## Look at the shell stream and check the status #
+        return self.shell_cond and self.iobuf_cond
+
+    def handle_ipython_stream(self, stream_type: IPythonConstants.StreamType):
+        try:
+            log.info('Start stream %s' % stream_type)
+            while not self.stop_stream_thread:
+                try:
+                    ipython_message = None
+                    if stream_type == IPythonConstants.StreamType.SHELL:
+                        ipython_message = self.kc.get_shell_msg(
+                            timeout=MESSSAGE_TIMEOUT)
+                    elif stream_type == IPythonConstants.StreamType.IOBUF:
+                        ipython_message = self.kc.get_iopub_msg(
+                            timeout=MESSSAGE_TIMEOUT)
+
+                    if 'status' in ipython_message['content']:
+                        log.info('%s msg: msg_type = %s, status = %s' % (
+                            stream_type, ipython_message['header']['msg_type'], ipython_message['content']['status']))
+                    elif ipython_message['header']['msg_type'] == 'status' or ipython_message['header']['msg_type'] == 'error':
+                        log.info('%s msg: msg_type = %s, content = %s' % (
+                            stream_type, ipython_message['header']['msg_type'], ipython_message['content']))
+                    else:
+                        log.info('%s msg: msg_type = %s' % (
+                            stream_type, ipython_message['header']['msg_type']))
+                        # log.info('%s msg: msg_type = %s msg_content = %s' % (
+                        #     stream_type, ipython_message['header']['msg_type'], ipython_message['content']))
+
+                    if ipython_message is not None and self.message_handler_callback is not None:
+                        self.message_handler_callback(
+                            ipython_message, stream_type, self.client_message)
+
+                    ## unlock execute lock only after upstream has processed the data if messge is status #
+                    # if self._is_status_message(ipython_message) and ipython_message['content']['execution_state'] != 'busy' and self.execute_lock.locked():
+                    #     self.execute_lock.release()
+                    self._set_execution_complete_condition_from_message(
+                        stream_type, ipython_message)
+                    if self.execute_lock.locked() and self._is_execution_complete(stream_type, ipython_message):
+                        self.execute_lock.release()
+                        log.info('Kernel execution lock released')
+                except queue.Empty:
+                    pass
+            log.info('Stop stream %s' % stream_type)
+        except:
+            trace = traceback.format_exc()
+            log.info("Exception %s" % (trace))
+
+    def execute(self, code, exec_mode=None, message_handler_callback=None, client_message=None):
+        self.execute_lock.acquire()
+        log.info('Kernel execution lock acquired')
+        self._set_execution_complete_condition(False)
+        self.message_handler_callback = message_handler_callback
+        self.client_message = client_message
+        self.kc.execute(code)
---
+++ packages/cnext/cnext-0.7.0.tar.gz/cnext-0.7.0/setup.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cnext_server',
+ 'cnext_server.server',
+ 'cnext_server.server.python',
+ 'cnext_server.server.python.cassist',
+ 'cnext_server.server.python.code_editor',
+ 'cnext_server.server.python.dataframe_manager',
+ 'cnext_server.server.python.executor_manager',
+ 'cnext_server.server.python.experiment_manager',
+ 'cnext_server.server.python.file_explorer',
+ 'cnext_server.server.python.file_manager',
+ 'cnext_server.server.python.jupyter_server_manager',
+ 'cnext_server.server.python.libs',
+ 'cnext_server.server.python.logs_manager',
+ 'cnext_server.server.python.model_manager',
+ 'cnext_server.server.python.project_manager',
+ 'cnext_server.server.python.user_space',
+ 'cnext_server.server.python.user_space.ipython',
+ 'cnext_server.server.tests',
+ 'cnext_server.server.tests.test_servers']
+
+package_data = \
+{'': ['*'],
+ 'cnext_server': ['public/*',
+                  'public/_next/static/chunks/*',
+                  'public/_next/static/chunks/pages/*',
+                  'public/_next/static/css/*',
+                  'public/_next/static/nuX8IgQlSErnkXxk2rrl4/*',
+                  'public/icons/*'],
+ 'cnext_server.server': ['build/*', 'ls/*', 'routes/*']}
+
+install_requires = \
+['cnextlib>=0.5.0,<0.6.0',
+ 'jupyter-client>=7.2.2,<7.3.0',
+ 'jupyter-resource-usage>=0.6.1,<0.7.0',
+ 'jupyterlab>=3.3.4,<3.4.0',
+ 'matplotlib>=3.5.1,<3.6.0',
+ 'mlflow>=1.25.1,<2.0.0',
+ 'multipledispatch>=0.6.0,<0.7.0',
+ 'netron>=6.0.0,<7.0.0',
+ 'pandas>=1.3.5,<1.4.0',
+ 'plotly>=5.7.0,<5.8.0',
+ 'protobuf==3.20.1',
+ 'pyreadline>=2.1,<3.0',
+ 'python-language-server>=0.36.2,<0.37.0',
+ 'pyyaml>=5.1,<6.0',
+ 'pyzmq>=23.2.0,<24.0.0',
+ 'requests>=2.27.1,<3.0.0',
+ 'send2trash>=1.8.0,<2.0.0',
+ 'sentry-sdk>=1.5.12,<2.0.0',
+ 'simplejson>=3.17.6,<4.0.0']
+
+entry_points = \
+{'console_scripts': ['cnext = cnext_server.__main__:main']}
+
+setup_kwargs = {
+    'name': 'cnext',
+    'version': '0.7.0',
+    'description': 'The data-centric workspace for AI & DS',
+    'long_description': '# CNext Instructions\n\n[Website] - [Documentation] - [Docker Image] - [Overview Video]\n\n## Setup and run the CNext workspace\n\nCNext is a data-centric workspace for DS and AI. Our workspace is meant to consolidate the most common tasks performed by data scientists and ML engineers. At a high level our workspace allows for:\n\n-   Data exploration & transformation\n-   Model development / exploration\n-   Production code generation\n-   Dashboard & App Generation\n-   Experiment Management\n\n## Features\n\n-   Interactive Python coding envrionment with native Python output (think Jupyter replacement)\n-   Smart code suggestion (categorical values and column names)\n-   Interactive data exploration\n-   Automative visualizations\n-   Experiment and model management\n-   Instant dashboarding\n\n## Installation via Docker\n\ncnext is also available via pre-built Docker images. To get started, you can simply run the following command:\n\n```bash\ndocker run --rm -it -p 4000:4000 -p 5000:5000 -p 5011:5011 -p 5008:5008 -p 5005:5005 cycai/cnext\n```\n\nThe web application will launch at: `http://localhost:4000` or `http://127.0.0.1:4000/`\n\n## Installation via Pip\n\nPLEASE NOTE: CNext requires npm >= 18.4 and Python >= 3.9.7 . Please ensure your environment meets the minimum requirements before beginning the installation. \n\nStep 1: Make sure `Nodejs` is available in your computer (try `npm --version` and make sure it work)\n\nStep 2: `run` command `pip install -U cnext`\n\nStep 3: `run` command `cnext-init`\n\n-   Input `Enter path to the cnext sample project created in Step 1` and hit `Enter` (Example `C:/Skywalker`)\n    \u200b\n\nStep 4 `run` command `cnext-run`\n\n-   Web application will launch at : `http://localhost:CLIENT_PORT` or `http://127.0.0.1:CLIENT_PORT/` (CLIENT_PORT default is 4000)\n-   Stop application: `Ctrl + c | Command + c`\n-   Note: Pay attention at `CLIENT_PORT`, and `SERVER_PORT` in `.env` file (you will have to change these ports if you already use them on your machine)\n\n\n## License\n\nCopyright 2022 CycAI Inc.\n\u200b\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\u200b\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\u200b\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n\n[website]: https://www.cnext.io/\n[docker image]: https://hub.docker.com/r/cycai/cnext\n[documentation]: https://docs.cnext.io/\n[overview video]: https://youtu.be/5eWPkQIUfZw\n[cnext]: https://drive.google.com/file/d/1ft4PmFclylOtEAQSPBqn9nUSyAkMs5R-\n[docker]: https://www.docker.com/products/docker-desktop/\n',
+    'author': 'CycAI Inc',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://cyc-ai.com/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9',
+}
+
+
+setup(**setup_kwargs)
