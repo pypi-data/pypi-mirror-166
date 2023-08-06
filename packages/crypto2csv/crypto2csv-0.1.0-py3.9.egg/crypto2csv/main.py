c4946d03c8f65c600046a72c84e170a6607ee990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:48 +0000

    Add package crypto2csv/0.1.0/crypto2csv-0.1.0-py3.9.egg

---
+++ packages/crypto2csv/crypto2csv-0.1.0-py3.9.egg/crypto2csv/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+import pandas
+import sys
+
+from .utils.detailed_data import detailed_data
+from .utils.historical_data import historical_data
+
+def main():
+
+    if(len(sys.argv)==2):
+        coin=str(sys.argv[1])
+        output=detailed_data(coin)
+        output.to_csv("data.csv")
+
+    if(len(sys.argv)==4):
+        coin=str(sys.argv[1])
+        start=str(sys.argv[2])
+        end=str(sys.argv[3])
+        output=historical_data(coin,start,end)
+        output.to_csv("data.csv")
