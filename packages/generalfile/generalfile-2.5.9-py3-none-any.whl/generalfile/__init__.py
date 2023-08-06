2a49c0c5c53a20e1c0631700c16d44956bbd1ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:02 +0000

    Add package generalfile/2.5.9/generalfile-2.5.9-py3-none-any.whl

---
+++ packages/generalfile/generalfile-2.5.9-py3-none-any.whl/generalfile/__init__.py
@@ -0,0 +1,6 @@
+
+from generalfile.path import Path
+from generalfile.configfile import ConfigFile
+from generalfile.errors import CaseSensitivityError, InvalidCharacterError
+
+Path.get_lock_dir().create_folder()
