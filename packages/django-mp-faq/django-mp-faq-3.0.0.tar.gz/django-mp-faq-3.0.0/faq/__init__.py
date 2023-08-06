70daa599828a0badb7e2c4a70c6d88e0b4fbf154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:34 +0000

    Add package django-mp-faq/3.0.0/django-mp-faq-3.0.0.tar.gz

---
+++ packages/django-mp-faq/django-mp-faq-3.0.0.tar.gz/django-mp-faq-3.0.0/faq/__init__.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+def setup_settings(settings, is_prod, **kwargs):
+
+    settings['INSTALLED_APPS'] += [
+        app for app in [
+            'faq',
+            'ordered_model'
+        ] if app not in settings['INSTALLED_APPS']
+    ]
+
+
+class FAQConfig(AppConfig):
+    name = 'faq'
+    verbose_name = _("FAQ")
+
+
+default_app_config = 'faq.FAQConfig'
