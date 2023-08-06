92b3b9f323d8d181bf98b4d0b8c3999dce0afa7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:09 +0000

    Add package ikologikapi/1.1.53/ikologikapi-1.1.53-py3-none-any.whl

---
+++ packages/ikologikapi/ikologikapi-1.1.53-py3-none-any.whl/ikologikapi/domain/DataImport.py
@@ -0,0 +1,12 @@
+from ikologikapi.domain.AbstractIkologikInstallationObject import AbstractIkologikInstallationObject
+
+
+class DataImport(AbstractIkologikInstallationObject):
+
+    def __init__(self, customer: str, installation: str):
+        super().__init__(customer, installation)
+
+        self.name = None
+        self.status = None
+        self.active = True
+        self.parameters = []
