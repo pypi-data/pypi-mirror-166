a468e8da4955bfb37e95b7ae65ca0fb83a67f2d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:51 +0000

    Add package rimseval/2.0.1/rimseval-2.0.1-py3-none-any.whl

---
+++ packages/rimseval/rimseval-2.0.1-py3-none-any.whl/rimseval/data_io/__init__.py
@@ -0,0 +1,13 @@
+"""Routine to pre-process list files.
+
+This routine turns list files into crd files. The crd file format is as specified in
+the `docs` folder and adheres currently to v1.0 of the format.
+"""
+
+from . import crd_utils
+from . import export
+from . import lst_utils
+from .crd_reader import CRDReader
+from .lst_to_crd import LST2CRD
+
+__all__ = ["export", "crd_utils", "lst_utils", "CRDReader", "LST2CRD"]
