682ccd8252994126ed66ba0390dfd50780bdc6fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:37 +0000

    Add package my-geometry-lib/0.1/my_geometry_lib-0.1-py3-none-any.whl

---
+++ packages/my-geometry-lib/my_geometry_lib-0.1-py3-none-any.whl/geometry/point.py
@@ -0,0 +1,12 @@
+class Point:
+    def __init__(self, x=0, y=0):
+        self.__x = x
+        self.__y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
