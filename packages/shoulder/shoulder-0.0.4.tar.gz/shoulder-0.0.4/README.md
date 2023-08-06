74e79710a8ebdf2a6738181cf8157c0b4ba1c1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:26 +0000

    Add package shoulder/0.0.4/shoulder-0.0.4.tar.gz

---
+++ packages/shoulder/shoulder-0.0.4.tar.gz/shoulder-0.0.4/README.md
@@ -0,0 +1,5 @@
+# Shoulder
+
+Shoulder is a patient specific anatomic coordinate system generator for shoulder bones.
+
+Still a work in progress.
---
+++ packages/shoulder/shoulder-0.0.4.tar.gz/shoulder-0.0.4/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['shoulder', 'shoulder.glenoid', 'shoulder.humerus']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Rtree>=1.0.0,<2.0.0',
+ 'Shapely>=1.8.4,<2.0.0',
+ 'circle-fit>=0.1.3,<0.2.0',
+ 'networkx>=2.8.6,<3.0.0',
+ 'numpy-stl>=2.17.1,<3.0.0',
+ 'pandas>=1.4.4,<2.0.0',
+ 'plotly>=5.10.0,<6.0.0',
+ 'scikit-learn>=1.1.2,<2.0.0',
+ 'scikit-spatial>=6.5.0,<7.0.0',
+ 'scipy>=1.9.1,<2.0.0',
+ 'trimesh>=3.14.1,<4.0.0']
+
+setup_kwargs = {
+    'name': 'shoulder',
+    'version': '0.0.4',
+    'description': 'patient specific anatomic coordinate system generation for shoulder bones',
+    'long_description': '# Shoulder\n\nShoulder is a patient specific anatomic coordinate system generator for shoulder bones.\n\nStill a work in progress.\n',
+    'author': 'Gregory W Spangenberg',
+    'author_email': 'gspangen@westerneng.ca',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/gregspangenberg/shoulder',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<3.11',
+}
+
+
+setup(**setup_kwargs)
