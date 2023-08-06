bab972f4020ab4fa8501816dd25b0f01dd5d4ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:34 +0000

    Add package pyscrabbler/1.0.3/pyscrabbler-1.0.3.tar.gz

---
+++ packages/pyscrabbler/pyscrabbler-1.0.3.tar.gz/pyscrabbler-1.0.3/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='pyscrabbler',
+      version='1.0.2',
+      description='Provide a set of letters and retrieve a list of potential worsds and point values for Scrabble.',
+      author='Michael Warmbier',
+      author_email='business@michaelwarmbier.com',
+      url='http://michaelwarmbier.com',
+      include_package_data=True,
+     )
