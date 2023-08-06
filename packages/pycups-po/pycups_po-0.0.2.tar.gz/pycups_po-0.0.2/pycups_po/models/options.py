316eb7260bc2759e1a719356de3bdc8c88a77b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:26 +0000

    Add package pycups-po/0.0.2/pycups_po-0.0.2.tar.gz

---
+++ packages/pycups-po/pycups_po-0.0.2.tar.gz/pycups_po-0.0.2/pycups_po/models/options.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from typing import Literal, List
+
+
+@dataclass
+class OptionValue:
+    value: str
+    pretty_value: str
+
+
+@dataclass
+class PrinterOption:
+    name: str
+    type: Literal["PickOne"]
+    default_value: str
+    values: List[OptionValue]
