e24ee38b48920604b3311d36ae368fa5def8761e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:07 +0000

    Add package git-logs/0.0.3/git-logs-0.0.3.tar.gz

---
+++ packages/git-logs/git-logs-0.0.3.tar.gz/git-logs-0.0.3/gitlogs/parser.py
@@ -0,0 +1,24 @@
+import argparse
+
+args_parser = argparse.ArgumentParser(description="Display information from .git")
+
+args_parser.add_argument("-f", "--frequency", action="store", dest="frequency",
+                   type=str, required=False, default="month")
+
+args_parser.add_argument("-u", "--author", action="store", dest="author",
+                   type=str, required=False, default="",
+                   help="filter by author's e-mail (substring)")
+
+args_parser.add_argument("-a", "--after", action="store", dest="after",
+                   type=str, required=False, default="",
+                   help="after date (yyyy-mm-dd hh:mm)")
+
+args_parser.add_argument("-b", "--before", action="store", dest="before",
+                   type=str, required=False, default="",
+                   help="before date (yyyy-mm-dd hh:mm)")
+
+args_parser.add_argument("-r", "--reverse", action="store", dest="reverse",
+                   type=bool, required=False, default=False,choices=[True,False],
+                   help="reverse date order")
+
+args = args_parser.parse_args()
