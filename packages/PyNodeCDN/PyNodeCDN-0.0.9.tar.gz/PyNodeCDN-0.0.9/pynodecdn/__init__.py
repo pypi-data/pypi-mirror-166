677ee7bf5241e7f301b8003f51409ee6670f590e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:48 +0000

    Add package PyNodeCDN/0.0.9/PyNodeCDN-0.0.9.tar.gz

---
+++ packages/PyNodeCDN/PyNodeCDN-0.0.9.tar.gz/PyNodeCDN-0.0.9/pynodecdn/__init__.py
@@ -0,0 +1,21 @@
+import requests
+import random
+import os
+import sys
+import re
+import time
+import json
+
+class Flask():
+    def __init__(self, app):
+        self.app = app
+        self.route = '/nodecdn.js'
+        
+    def change_path(self, path):
+        self.route = path
+        Flask.apply(self)
+
+    def apply(self):
+        @self.app.route(self.route)
+        def nodecdnjsurl():
+            return "Works!"
