f6d8aaddf8fa6879ec9f3fa62a1b3eff22a8048f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:50 +0000

    Add package YetAnotherSMSBomber/3.0.0/YetAnotherSMSBomber-3.0.0.tar.gz

---
+++ packages/YetAnotherSMSBomber/YetAnotherSMSBomber-3.0.0.tar.gz/YetAnotherSMSBomber-3.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="YetAnotherSMSBomber",
+    version="3.0.0",
+    url="https://t.me/RobbyBakum",
+    license="GNU",
+    author="Robby Ivanov",
+    author_email="robbyivash@teml.net",
+    description="Юзербот для Телеграм")
