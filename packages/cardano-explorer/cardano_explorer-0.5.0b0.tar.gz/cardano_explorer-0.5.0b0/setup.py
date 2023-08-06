41b6d148653a637d978a874abb403759bf7fa76a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:24 +0000

    Add package cardano-explorer/0.5.0b0/cardano_explorer-0.5.0b0.tar.gz

---
+++ packages/cardano-explorer/cardano_explorer-0.5.0b0.tar.gz/cardano_explorer-0.5.0b0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setuptools
+
+try:
+    with open('PYPI_README.md') as f:
+        long_description = f.read()
+except Exception as e:
+    long_description=""
+
+setuptools.setup(
+    name='cardano_explorer',
+    packages=find_packages(include=['cardano_explorer', 'cardano_explorer.blockfrost', 'cardano_explorer.cnft']),
+    include_package_data = True,
+    version='0.5.0-beta.0',
+    description='Python wrapper for accessing and processing information stored on the Cardano blockchain using Blockfrost API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='ATTA Djessy',
+    author_email = 'djessyatta@live.fr',
+    url = 'https://github.com/djessy-atta/py-cardano-explorer',
+    download_url = 'https://github.com/attadje/py-cardano-explorer/archive/refs/tags/v0.5-beta.0.tar.gz',
+    keywords = ['CARDANO', 'API', 'WRAPPER', 'BLOCKCHAIN', 'BLOCKFROST'],
+    license='MIT',
+    install_requires=['pandas>=1.3.2', 'requests>=2.26.0', 'typing>=3.7.4.3', 'numpy==1.21.2', 'tqdm>=4.62.2'],
+    tests_require=['pytest>=6.2.5', 'pytest-runner>=5.3.1', 'tqdm>=4.62.2'],
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 3 - Alpha',  
+        'Intended Audience :: Developers',      
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License', 
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',      
+  ],
+)
+
