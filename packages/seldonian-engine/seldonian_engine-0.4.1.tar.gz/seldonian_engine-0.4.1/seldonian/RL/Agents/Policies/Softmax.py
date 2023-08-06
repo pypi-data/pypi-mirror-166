2b0fb3c95078609af7fd02173940818dc8a996ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:31 +0000

    Add package seldonian-engine/0.4.1/seldonian_engine-0.4.1.tar.gz

---
+++ packages/seldonian-engine/seldonian_engine-0.4.1.tar.gz/seldonian_engine-0.4.1/seldonian/RL/Agents/Policies/Softmax.py
@@ -0,0 +1,60 @@
+from seldonian.RL.Agents.Policies.Policy import *
+import autograd.numpy as np
+from seldonian.utils.RL_utils import *
+
+class Softmax(Discrete_Action_Policy):
+    def __init__(self, hyperparam_and_setting_dict, env_description):
+        super().__init__(hyperparam_and_setting_dict, env_description)
+
+    def choose_action(self, obs):
+        """ Select an action given a observation
+
+        :param obs: The current observation of the agent, type depends on environment.
+
+        :return: array of actions
+        """
+        action_values = self.get_action_values_given_state(obs)
+        return self.choose_action_from_action_values(action_values)
+
+    def choose_action_from_action_values(self, action_values):
+        if len(action_values) != self.num_actions:
+            error(f"should have {self.num_actions} actions, but got {len(action_values)} action values")
+
+        action_probs = self.get_action_probs_from_action_values(action_values)
+
+        roulette_wheel_start = 0.0
+        stop_value = np.random.rand()
+        for action_num_zero_indexed in range(self.num_actions):
+            roulette_wheel_start += action_probs[action_num_zero_indexed]
+            if roulette_wheel_start >= stop_value:
+                return self.from_0_indexed_action_to_environment_action(action_num_zero_indexed)
+
+        print(stop_value)
+        print(roulette_wheel_start)
+        print(action_probs)
+        error("reached the end of SoftMax.choose_action(), this should never happen")
+
+    def get_action_probs_from_action_values(self, action_values):
+        e_to_the_something_terms = self.get_e_to_the_something_terms(action_values)
+        denom = sum(e_to_the_something_terms)
+        return e_to_the_something_terms / denom
+
+    def get_e_to_the_something_terms(self, action_values):
+        max_value = np.max(action_values)
+        e_to_the_something_terms = np.exp(action_values - max_value) #subtract max for numerical stability
+        return e_to_the_something_terms
+
+    def get_prob_this_action(self, observation, action):
+        """ Get the probability of a selected action in a given obs
+
+        :param observation: The current obs of the agent, type depends on environment.
+
+        :param action: The action selected, type depends on environment
+
+        :return: probability of action
+        :rtype: float
+        """
+        action_values = self.get_action_values_given_state(observation)
+        action_probs = self.get_action_probs_from_action_values(action_values)
+        this_action = self.from_environment_action_to_0_indexed_action(action)
+        return action_probs[this_action]
