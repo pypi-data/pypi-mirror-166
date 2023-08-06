40b1f03b68ab1329d36935ee733ce6401a56f2a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:52 +0000

    Add package gto/0.2.2/gto-0.2.2.tar.gz

---
+++ packages/gto/gto-0.2.2.tar.gz/gto-0.2.2/gto/constants.py
@@ -0,0 +1,49 @@
+import re
+from enum import Enum
+
+COMMIT = "commit"
+REF = "ref"
+TAG = "tag"
+
+ARTIFACT = "artifact"
+ACTION = "action"
+TYPE = "type"
+NAME = "name"
+PATH = "path"
+VERSION = "version"
+STAGE = "stage"
+COUNTER = "counter"
+
+
+class Action(Enum):
+    CREATE = "create"
+    DEPRECATE = "deprecate"
+    DEREGISTER = "deregister"
+    REGISTER = "register"
+    ASSIGN = "assign"
+    UNASSIGN = "unassign"
+
+
+name = "[a-z][a-z0-9-/]*[a-z0-9]"
+semver = r"(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?"
+counter = "?P<counter>[0-9]+"
+name_regexp = re.compile(f"^{name}$")
+tag_regexp = re.compile(
+    f"^(?P<artifact>{name})(((#(?P<stage>{name})|@(?P<version>v{semver}))(?P<cancel>!?))|@((?P<deprecated>deprecated)|(?P<created>created)))(#({counter}))?$"
+)
+shortcut_regexp = re.compile(
+    f"^(?P<artifact>{name})(((#(?P<stage>{name})|@(?P<latest>latest)|@(?P<greatest>greatest))))$"
+)
+
+
+def mark_artifact_unregistered(artifact_name):
+    return f"*{artifact_name}"
+
+
+class VersionSort(Enum):
+    SemVer = "semver"
+    Timestamp = "timestamp"
+
+
+ASSIGNMENTS_PER_VERSION = -1
+VERSIONS_PER_STAGE = 1
