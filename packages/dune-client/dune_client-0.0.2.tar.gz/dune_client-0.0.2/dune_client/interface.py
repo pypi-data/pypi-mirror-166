6c4f2a9b08d962b676d359775979a499394b0f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:03 +0000

    Add package dune-client/0.0.2/dune_client-0.0.2.tar.gz

---
+++ packages/dune-client/dune_client-0.0.2.tar.gz/dune_client-0.0.2/dune_client/interface.py
@@ -0,0 +1,20 @@
+"""
+Abstract class for a basic Dune Interface with refresh method used by Query Runner.
+"""
+from abc import ABC
+
+from dune_client.query import Query
+from dune_client.types import DuneRecord
+
+
+# pylint: disable=too-few-public-methods
+class DuneInterface(ABC):
+    """
+    User Facing Methods for a Dune Client
+    """
+
+    def refresh(self, query: Query) -> list[DuneRecord]:
+        """
+        Executes a Dune query, waits till query execution completes,
+        fetches and returns the results.
+        """
