be121f2049fbcfbb730fad7019864b5115678d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:26 +0000

    Add package atelier-ai/0.1.2/atelier-ai-0.1.2.tar.gz

---
+++ packages/atelier-ai/atelier-ai-0.1.2.tar.gz/atelier-ai-0.1.2/atelier/workflow/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# ================================================================================================ #
+# Project    : Atelier AI: Studio for AI Designers                                                 #
+# Version    : 0.1.2                                                                               #
+# Python     : 3.9.12                                                                              #
+# Filename   : \__init__.py                                                                        #
+# ------------------------------------------------------------------------------------------------ #
+# Author     : John James                                                                          #
+# Email      : john.james.ai.studio@gmail.com                                                      #
+# URL        : https://github.com/john-james-ai/atelier-ai                                         #
+# ------------------------------------------------------------------------------------------------ #
+# Created    : Thursday August 11th 2022 07:38:11 pm                                               #
+# Modified   : Tuesday September 6th 2022 02:34:12 pm                                              #
+# ------------------------------------------------------------------------------------------------ #
+# License    : BSD 3-clause "New" or "Revised" License                                             #
+# Copyright  : (c) 2022 John James                                                                 #
+# ================================================================================================ #
