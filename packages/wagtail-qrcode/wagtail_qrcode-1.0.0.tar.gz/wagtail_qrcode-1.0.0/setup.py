595e940ef3f62e63e812c031aea7fd6ce67e712e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:51 +0000

    Add package wagtail-qrcode/1.0.0/wagtail_qrcode-1.0.0.tar.gz

---
+++ packages/wagtail-qrcode/wagtail_qrcode-1.0.0.tar.gz/wagtail_qrcode-1.0.0/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['wagtail_qrcode',
+ 'wagtail_qrcode.test',
+ 'wagtail_qrcode.test.migrations',
+ 'wagtail_qrcode.test.tests']
+
+package_data = \
+{'': ['*'], 'wagtail_qrcode': ['templates/wagtail_qrcode/admin/*']}
+
+install_requires = \
+['PyQRCode>=1.2.1,<2.0.0', 'wagtail>=2.15']
+
+entry_points = \
+{'console_scripts': ['clean = scripts:clean',
+                     'develop = scripts:develop',
+                     'test-scripts = scripts:test_scripts']}
+
+setup_kwargs = {
+    'name': 'wagtail-qrcode',
+    'version': '1.0.0',
+    'description': 'Create a QR code that can be used to link to a wagtail page',
+    'long_description': None,
+    'author': 'Nick Moreton',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/nickmoreton/wagtail-qrcode',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
