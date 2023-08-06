090d54e55e11d5ae9d77643cd7cc08623dd6bf37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:08 +0000

    Add package rowantree.service.sdk/0.6.0/rowantree.service.sdk-0.6.0.tar.gz

---
+++ packages/rowantree.service.sdk/rowantree.service.sdk-0.6.0.tar.gz/rowantree.service.sdk-0.6.0/src/rowantree/service/sdk/commands/user/delete.py
@@ -0,0 +1,29 @@
+""" User Delete Command Definition """
+
+import requests
+
+from ..abstract_command import AbstractCommand
+
+
+class UserDeleteCommand(AbstractCommand):
+    """
+    User Delete Command
+    Deletes a user.
+
+    Methods
+    -------
+    execute(self, user_guid: str) -> None
+        Executes the command.
+    """
+
+    def execute(self, user_guid: str, headers: dict[str, str]) -> None:
+        """
+        Executes the command.
+
+        Parameters
+        ----------
+        user_guid: str
+            The target user guid.
+        """
+
+        requests.delete(url=f"{self.config.endpoint}/v1/user/{user_guid}", headers=headers, timeout=self.config.timeout)
