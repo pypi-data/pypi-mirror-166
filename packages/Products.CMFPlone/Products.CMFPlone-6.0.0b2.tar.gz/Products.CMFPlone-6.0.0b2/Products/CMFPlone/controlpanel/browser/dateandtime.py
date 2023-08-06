ac7a4b87c76d0be6349414b2a48a44b15dd8519f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:40 +0000

    Add package Products.CMFPlone/6.0.0b2/Products.CMFPlone-6.0.0b2.tar.gz

---
+++ packages/Products.CMFPlone/Products.CMFPlone-6.0.0b2.tar.gz/Products.CMFPlone-6.0.0b2/Products/CMFPlone/controlpanel/browser/dateandtime.py
@@ -0,0 +1,21 @@
+from Products.CMFPlone import PloneMessageFactory as _
+from plone.base.interfaces import IDateAndTimeSchema
+from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+from plone.app.registry.browser.controlpanel import RegistryEditForm
+
+
+class DateAndTimeControlPanelForm(RegistryEditForm):
+
+    id = "DateAndTimeControlPanel"
+    schema = IDateAndTimeSchema
+    schema_prefix = "plone"
+
+    label = _("label_dateandtime_settings", default="Date and Time Settings")
+    description = _(
+        "help_event_settings",
+        default="Date and Time related settings like timezone(s), etc."
+    )
+
+
+class DateAndTimeControlPanel(ControlPanelFormWrapper):
+    form = DateAndTimeControlPanelForm
