16936bf50fa8b345da71eb3173f75209b4f4bb23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:18 +0000

    Add package pytest-ekstazi/1.0.0/pytest-ekstazi-1.0.0.tar.gz

---
+++ packages/pytest-ekstazi/pytest-ekstazi-1.0.0.tar.gz/pytest-ekstazi-1.0.0/setup.py
@@ -0,0 +1,31 @@
+__author__ = 'Igorxp5, vsla, Nielx20'
+
+__license__ = 'MIT'
+__version__ = '1.0.0'
+
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name='pytest-ekstazi',
+    version=__version__,
+    author=__author__,
+    description='Pytest plugin to select test using Ekstazi algorithm',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Igorxp5/pytest-ekstazi',
+    packages=['pytest_ekstazi'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: Pytest'
+    ],
+    entry_points={
+        'pytest11': ['pytest_ekstazi = pytest_ekstazi.plugin']
+    },
+    install_requires=['pytest'],
+    python_requires='>=3.7',
+)
