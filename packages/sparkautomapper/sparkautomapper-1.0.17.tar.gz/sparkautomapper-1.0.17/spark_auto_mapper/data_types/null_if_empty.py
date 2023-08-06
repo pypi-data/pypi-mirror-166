b42d9f3c59dc5f881f16882ce0bc45534fa3c70c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:57 +0000

    Add package sparkautomapper/1.0.17/sparkautomapper-1.0.17.tar.gz

---
+++ packages/sparkautomapper/sparkautomapper-1.0.17.tar.gz/sparkautomapper-1.0.17/spark_auto_mapper/data_types/null_if_empty.py
@@ -0,0 +1,62 @@
+from typing import Generic, List, Optional, TypeVar, Union
+
+from pyspark.sql import Column, DataFrame
+from pyspark.sql.functions import when, lit
+
+from spark_auto_mapper.data_types.data_type_base import AutoMapperDataTypeBase
+from spark_auto_mapper.type_definitions.native_types import AutoMapperNativeSimpleType
+
+_TAutoMapperDataType = TypeVar(
+    "_TAutoMapperDataType",
+    bound=Union[AutoMapperNativeSimpleType, AutoMapperDataTypeBase],
+)
+
+
+class AutoMapperNullIfEmptyDataType(
+    AutoMapperDataTypeBase, Generic[_TAutoMapperDataType]
+):
+    """
+    If value returns empty then return null else return value
+    """
+
+    def __init__(
+        self,
+        value: _TAutoMapperDataType,
+    ):
+        super().__init__()
+
+        self.value: _TAutoMapperDataType = value
+
+    def include_null_properties(self, include_null_properties: bool) -> None:
+        if isinstance(self.value, AutoMapperDataTypeBase):
+            self.value.include_null_properties(
+                include_null_properties=include_null_properties
+            )
+
+    def get_column_spec(
+        self,
+        source_df: Optional[DataFrame],
+        current_column: Optional[Column],
+        parent_columns: Optional[List[Column]],
+    ) -> Column:
+        value_spec = (
+            self.value.get_column_spec(
+                source_df=source_df,
+                current_column=current_column,
+                parent_columns=parent_columns,
+            )
+            if isinstance(self.value, AutoMapperDataTypeBase)
+            else lit(self.value)
+        )
+        column_spec = when(
+            value_spec.eqNullSafe(""),
+            lit(None),
+        ).otherwise(value_spec)
+
+        return column_spec
+
+    @property
+    def children(
+        self,
+    ) -> Union[AutoMapperDataTypeBase, List[AutoMapperDataTypeBase]]:
+        return self.value  # type: ignore
