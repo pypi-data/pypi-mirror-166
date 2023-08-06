551430f465f7c338f2d1dc6b27601379d16225a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:48 +0000

    Add package jdv-funcutils/1.0.2a4/jdv_funcutils-1.0.2a4.tar.gz

---
+++ packages/jdv-funcutils/jdv_funcutils-1.0.2a4.tar.gz/jdv_funcutils-1.0.2a4/jdv_funcutils/utils/__init__.py
@@ -0,0 +1,6 @@
+#  Copyright (c) 2022 Justin Vrana. All Rights Reserved.
+#  You may use, distribute, and modify this code under the terms of the MIT license.
+from .null import Null
+from .null import null
+
+__all__ = ["null", "Null"]
