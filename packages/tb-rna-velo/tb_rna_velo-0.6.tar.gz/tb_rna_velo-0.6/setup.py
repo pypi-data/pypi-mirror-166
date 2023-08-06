06302fca0e0e36f19c387198d0a71bb48cfd520e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:08 +0000

    Add package tb-rna-velo/0.6/tb_rna_velo-0.6.tar.gz

---
+++ packages/tb-rna-velo/tb_rna_velo-0.6.tar.gz/tb_rna_velo-0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tb_rna_velo',
+    version='0.6',
+    license='TEST',
+    author="RijeshShrestha",
+    author_email='craaabby@gmail.com.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    keywords='example project',
+    install_requires=[
+        'tensorflow',
+        'numpy==1.20',
+        'anndata',
+        'matplotlib',
+        'pandas',
+        'scikit_learn',
+        'scipy',
+        'scvelo',
+        'seaborn'        
+    ]
+)
