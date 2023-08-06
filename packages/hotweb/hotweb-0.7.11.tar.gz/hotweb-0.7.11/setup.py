90621636a9b61cedb40a6065083730fd780ff510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:19 +0000

    Add package hotweb/0.7.11/hotweb-0.7.11.tar.gz

---
+++ packages/hotweb/hotweb-0.7.11.tar.gz/hotweb-0.7.11/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="hotweb",
+    version="0.7.11",
+    license="MIT",
+    author = "Real Manlow,aka ManlowCharumbira",
+    author_email="realmanlow20@gmail.com",
+    #packages=["hotweb"],
+    package_dir = {'':'hotweb'},
+    keywords = "hotweb python-web-framework python web framework fast light secure",
+    install_requires = [
+        "parse","waitress","webob","requests",
+    ]
+)
