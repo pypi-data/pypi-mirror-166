78d4fb296d654ec137cb54cfe1ad2eff3bdef0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:36 +0000

    Add package zwave-me-ws/0.2.5.2/zwave_me_ws-0.2.5.2-py3-none-any.whl

---
+++ packages/zwave-me-ws/zwave_me_ws-0.2.5.2-py3-none-any.whl/zwave_me_ws/helpers.py
@@ -0,0 +1,55 @@
+from dataclasses import dataclass, field
+from typing import Union
+
+FIELDS = [
+    "id",
+    "deviceType",
+    "probeType",
+    "locationName",
+    "manufacturer",
+    "firmware",
+    "tags",
+]
+METRICS_SCALE = ["title", "level", "scaleTitle", "min", "max", "color", "isFailed"]
+TYPE_LIGHT = "type-light"
+
+
+@dataclass
+class ZWaveMeData:
+    id: str
+    deviceType: str
+    title: str
+    level: Union[str, int, float]
+    probeType: str = ""
+    scaleTitle: str = ""
+    min: str = ""
+    max: str = ""
+    color: dict = field(default_factory=dict)
+    isFailed: bool = False
+    locationName: str = ""
+    manufacturer: str = ""
+    firmware: str = ""
+    tags: list[str] = field(default_factory=list)
+
+
+def prepare_devices(devices: list[dict]) -> list[ZWaveMeData]:
+    prepared_devices = []
+    for device in devices:
+        prepared_device = {
+            **{key: device[key] for key in FIELDS if key in device},
+            **{
+                key: device["metrics"][key]
+                for key in METRICS_SCALE
+                if key in device["metrics"]
+            },
+        }
+        if prepared_device["probeType"] == "siren":
+            prepared_device["deviceType"] = "siren"
+        elif TYPE_LIGHT in prepared_device["tags"]:
+            prepared_device["deviceType"] = "lightMultilevel"
+        elif prepared_device["probeType"] == "motor":
+            prepared_device["deviceType"] = "motor"
+        elif prepared_device["probeType"] == "fan":
+            prepared_device["deviceType"] = "fan"
+        prepared_devices.append(prepared_device)
+    return [ZWaveMeData(**d) for d in prepared_devices]
