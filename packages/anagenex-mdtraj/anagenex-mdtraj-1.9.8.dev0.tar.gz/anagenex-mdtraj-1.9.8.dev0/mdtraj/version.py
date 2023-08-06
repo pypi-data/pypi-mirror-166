e1fca7fa26c719ea4f5c725f6a4454877962f1a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:52 +0000

    Add package anagenex-mdtraj/1.9.8.dev0/anagenex-mdtraj-1.9.8.dev0.tar.gz

---
+++ packages/anagenex-mdtraj/anagenex-mdtraj-1.9.8.dev0.tar.gz/anagenex-mdtraj-1.9.8.dev0/mdtraj/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '1.9.8.dev0'
+short_version = '1.9.8.dev0'
+full_version = '1.9.8.dev0+ae67b65dd2127e31379317547df031c0347c9577'
+git_revision = 'ae67b65dd2127e31379317547df031c0347c9577'
+release = False
