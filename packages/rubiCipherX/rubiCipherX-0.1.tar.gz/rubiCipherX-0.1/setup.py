76744efda315bab0b2e9c668588aecbad7d9f9eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:08 +0000

    Add package rubiCipherX/0.1/rubiCipherX-0.1.tar.gz

---
+++ packages/rubiCipherX/rubiCipherX-0.1.tar.gz/rubiCipherX-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='rubiCipherX',
+    version='0.1',
+    url='https://github.com/CipherX-B4ar3/rubikaLib',
+    description='Rubika Lib python3',
+    python_requires='>=3.9',
+    packages=find_packages(exclude=['rubika*']),
+    install_requires=['aiohttp', 'pycryptodome'],
+)
