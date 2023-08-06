25cfdbf2e0890fb8425fb3c78d8ea826dd2cf08f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:47 +0000

    Add package panapi/0.0.1/panapi-0.0.1.tar.gz

---
+++ packages/panapi/panapi-0.0.1.tar.gz/panapi-0.0.1/panapi/config/network.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+
+from panapi.config import PanObject
+
+class BandwidthAllocation(PanObject):
+    'A bandwidth allocation'
+    _endpoint = '/sse/config/v1/bandwidth-allocations'
+
+
+class IKECryptoProfile(PanObject):
+    'An IKE crypto profile'
+    _endpoint = '/sse/config/v1/ike-crypto-profiles'
+
+
+class IKEGateway(PanObject):
+    'An IKE gateway'
+    _endpoint = '/sse/config/v1/ike-gateways'
+
+
+class IPSecCryptoProfile(PanObject):
+    'An IPSec crypto profile'
+    _endpoint = '/sse/config/v1/ipsec-crypto-profiles'
+
+
+class IPSecTunnel(PanObject):
+    'An IPSec tunnel'
+    _endpoint = '/sse/config/v1/ipsec-tunnels'
+
+
+class Location(PanObject):
+    'A SASE location'
+    _endpoint = '/sse/config/v1/locations'
+    # TODO Remove this once ADI-13944 is fixed
+    folder = 'Shared'
+    name = 'dummy'
+    def list(self, session):
+        if session.is_expired:
+            session.reauthenticate()
+        url = self._base_url + self._endpoint
+        params = {'folder': self.folder}
+        try:
+            session.response = session.get(
+                url = url,
+                params = params
+            )
+        except Exception as err:
+            print(err)
+        else:   
+            if session.response.status_code == 200:
+                result = session.response.json()
+                return result
+
+
+class QoSPolicyRule(PanObject):
+    'A QoS policy rule'
+    _endpoint = '/sse/config/v1/qos-policy-rules'
+
+
+class QoSProfile(PanObject):
+    'A QoS profile'
+    _endpoint = '/sse/config/v1/qos-profiles'
+
+
+class RemoteNetwork(PanObject):
+    'A SASE remote network'
+    _endpoint = '/sse/config/v1/remote-networks'
+
+
+class SharedInfrastructureSetting(PanObject):
+    'Shared Infrastructure Settings'
+    _endpoint = '/sse/config/v1/shared-infrastructure-settings'
+    # TODO Fix this!
+    folder = 'Shared'
+    name = 'dummy'
+    def list(self, session):
+        if session.is_expired:
+            session.reauthenticate()
+        url = self._base_url + self._endpoint
+        params = {'folder': self.folder}
+        try:
+            session.response = session.get(
+                url = url,
+                params = params
+            )
+        except Exception as err:
+            print(err)
+        else:   
+            if session.response.status_code == 200:
+                result = session.response.json()
+                return result
+
+
+class ServiceConnection(PanObject):
+    'Service Connections'
+    _endpoint = '/sse/config/v1/service-connections'
+
+
+class InternalDNSServer(PanObject):
+    'Internal DNS Servers'
+    _endpoint = '/sse/config/v1/ingternal-dns-servers'
+
+
+class TrafficSteeringRule(PanObject):
+    'Traffic steering rules'
+    _endpoint = '/sse/config/v1/traffic-steering-rules'
---
+++ packages/panapi/panapi-0.0.1.tar.gz/panapi-0.0.1/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="panapi",
+    version="0.0.1",
+    author="Palo Alto Networks",
+    author_email="devrel@paloaltonetworks.com",
+    description="A lightweight SDK for the Panorama Cloud API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PaloAltoNetworks/panapi",
+    packages=find_packages(),
+    install_requires=[
+        "certifi==2022.6.15",
+        "cffi==1.15.1",
+        "charset-normalizer==2.1.1",
+        "cryptography==37.0.4",
+        "idna==3.3",
+        "oauthlib==3.2.0",
+        "pycparser==2.21",
+        "PyJWT==2.4.0",
+        "PyYAML==6.0",
+        "requests==2.28.1",
+        "requests-oauthlib==1.3.1",
+        "urllib3==1.26.12"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Natural Language :: English"
+    ],
+    python_requires=">=3.6"
+)
