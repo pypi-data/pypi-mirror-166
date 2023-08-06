12fceb8cc3338249fa6f3e3815d44c9b09b54fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:33 +0000

    Add package docloudstuff/0.1.4/doCloudStuff-0.1.4.tar.gz

---
+++ packages/docloudstuff/doCloudStuff-0.1.4.tar.gz/doCloudStuff-0.1.4/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['docloudstuff', 'docloudstuff.events']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'docloudstuff',
+    'version': '0.1.4',
+    'description': '',
+    'long_description': '',
+    'author': 'Stephen Bawks',
+    'author_email': 'stephen@bawks.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/stephenbawks/docloudstuff',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
