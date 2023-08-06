8f3a7ef594c51cb278e0ff5f26f2a382a99ea99e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:39 +0000

    Add package resens/0.4.1/resens-0.4.1.tar.gz

---
+++ packages/resens/resens-0.4.1.tar.gz/resens-0.4.1/setup.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="resens",
+    version="0.4.1",
+    description="Raster Processing package for Remote Sensing and Earth Observation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.nargyrop.com",
+    author="Nikos Argyropoulos",
+    author_email="n.argiropgeo@gmail.com",
+    license="MIT",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    python_requires=">=3.7",
+    zip_safe=False,
+)
