5ffc94664dcbdd6734f3240c6ce4639262243284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:34 +0000

    Add package st-tokenization-vis-component/0.0.8/st_tokenization_vis_component-0.0.8-py3-none-any.whl

---
+++ packages/st-tokenization-vis-component/st_tokenization_vis_component-0.0.8-py3-none-any.whl/viz_tokens_component/frontend/build/precache-manifest.849ec3e56e8895b5eff7683f7815bdad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf0e36c07e1aea00e1bc94e4c763ebb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2aec7f1a25cc75162af7",
+    "url": "./static/js/2.1f7660f1.chunk.js"
+  },
+  {
+    "revision": "90d85258a7227bbb926dec29dcb29a18",
+    "url": "./static/js/2.1f7660f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d609ab5f06e559bfc7b2",
+    "url": "./static/js/main.2552536f.chunk.js"
+  },
+  {
+    "revision": "1534b57579d070af0362",
+    "url": "./static/js/runtime-main.d58b2606.js"
+  }
+]);
---
+++ packages/st-tokenization-vis-component/st_tokenization_vis_component-0.0.8-py3-none-any.whl/viz_tokens_component/frontend/build/precache-manifest.c0f9a1dd46d6298b4acf6b328674d36c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733d1a69be789fc5316f2c53b2757649",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2aec7f1a25cc75162af7",
+    "url": "./static/js/2.1f7660f1.chunk.js"
+  },
+  {
+    "revision": "90d85258a7227bbb926dec29dcb29a18",
+    "url": "./static/js/2.1f7660f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fb8f1ff2b8a152c0ff9",
+    "url": "./static/js/main.00b97b63.chunk.js"
+  },
+  {
+    "revision": "1534b57579d070af0362",
+    "url": "./static/js/runtime-main.d58b2606.js"
+  }
+]);
