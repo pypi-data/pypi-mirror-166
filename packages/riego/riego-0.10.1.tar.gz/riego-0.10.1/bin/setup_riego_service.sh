c1d6b33568a3a01dfe975ebf6005da74c1d314a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:31 +0000

    Add package riego/0.10.1/riego-0.10.1.tar.gz

---
+++ packages/riego/riego-0.10.1.tar.gz/riego-0.10.1/bin/setup_riego_service.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+sudo bash -c "cat > /etc/systemd/system/riego.service" <<'EOT'
+[Unit]
+Description=Riego Rain-System
+After=mnt-usb1.mount mosquitto.service
+
+[Service]
+Environment="PYTHONUNBUFFERED=1"
+Type=simple
+User=riego
+WorkingDirectory=/mnt/usb1/riego
+ExecStart=/mnt/usb1/riego/.venv/bin/riego
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+systemctl daemon-reload
+systemctl enable riego
+systemctl restart riego
