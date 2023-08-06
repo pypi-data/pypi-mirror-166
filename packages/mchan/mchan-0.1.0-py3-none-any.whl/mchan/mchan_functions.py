0c086d7050608b4cc08321f996b418eed64a8ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:12 +0000

    Add package mchan/0.1.0/mchan-0.1.0-py3-none-any.whl

---
+++ packages/mchan/mchan-0.1.0-py3-none-any.whl/mchan/mchan_functions.py
@@ -0,0 +1,8 @@
+def print_bday():
+    print("My birthday is on Dec 24, 1991")
+    
+def print_name():
+    print("My name is Mendelsohn Chan") 
+
+def print_custom():
+    print("This is a message to test my custom library")
