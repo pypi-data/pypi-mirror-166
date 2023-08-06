a55a41733b8c5ff57e6dfcf63bbbc2cf2908595c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:12 +0000

    Add package gym-update/0.5.0/gym_update-0.5.0.tar.gz

---
+++ packages/gym-update/gym_update-0.5.0.tar.gz/gym_update-0.5.0/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# -*- coding: utf-8 -*-
+
+
+import setuptools
+from pathlib import Path
+#from setuptools import setup
+setuptools.setup(name='gym_update',
+      version='0.5.0',
+      description="A OpenAI Gym Env for continuous control",
+      long_description=Path("README.md").read_text(encoding="utf-8"),
+      long_description_content_type="text/markdown",
+                 author="Claudia Viaro",
+                 license="MIT",
+      packages=setuptools.find_packages(include="gym_update*"),
+      install_requires=['gym']  # And any other dependencies needed
+)
+
