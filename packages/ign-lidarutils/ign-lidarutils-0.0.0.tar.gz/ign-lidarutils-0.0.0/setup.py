b1b9e1dcb7f96880111721ea8c3288b1dcb9fa15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:56 +0000

    Add package ign-lidarutils/0.0.0/ign-lidarutils-0.0.0.tar.gz

---
+++ packages/ign-lidarutils/ign-lidarutils-0.0.0.tar.gz/ign-lidarutils-0.0.0/setup.py
@@ -0,0 +1,14 @@
+import pathlib
+from setuptools import setup
+
+setup(
+    name="ign-lidarutils",
+    version="0.0.0",
+    description="Lidar utils",
+    long_description_content_type="text/markdown",
+    author="Guillaume Liégard",
+    author_email="guillaume.liegard@ign.fr",
+    classifiers=[],
+    packages=["lidarutils"],
+    include_package_data=True,
+)
