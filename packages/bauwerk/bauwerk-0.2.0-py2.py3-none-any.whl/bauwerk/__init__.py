658a762cb5dc60c99985c01fc4239f4431663e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:58 +0000

    Add package bauwerk/0.2.0/bauwerk-0.2.0-py2.py3-none-any.whl

---
+++ packages/bauwerk/bauwerk-0.2.0-py2.py3-none-any.whl/bauwerk/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for bauwerk."""
+
+__author__ = """rdnfn"""
+__email__ = ""
+__version__ = "0.2.0"
+
+import bauwerk.envs.registration
+
+bauwerk.envs.registration.register_all()
