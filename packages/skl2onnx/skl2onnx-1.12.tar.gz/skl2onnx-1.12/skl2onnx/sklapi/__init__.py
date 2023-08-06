df93ffad8dfdcb21a60e6a54f5cd21c5e3f424bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:15 +0000

    Add package skl2onnx/1.12/skl2onnx-1.12.tar.gz

---
+++ packages/skl2onnx/skl2onnx-1.12.tar.gz/skl2onnx-1.12/skl2onnx/sklapi/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+
+
+from .cast_transformer import CastTransformer  # noqa
+from .cast_regressor import CastRegressor  # noqa
+from .replace_transformer import ReplaceTransformer  # noqa
+from .woe_transformer import WOETransformer  # noqa
