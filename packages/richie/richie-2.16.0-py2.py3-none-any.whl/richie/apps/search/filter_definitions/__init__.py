c08d7e3a8727e6550c088122ba7e186337171031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:44 +0000

    Add package richie/2.16.0/richie-2.16.0-py2.py3-none-any.whl

---
+++ packages/richie/richie-2.16.0-py2.py3-none-any.whl/richie/apps/search/filter_definitions/__init__.py
@@ -0,0 +1,21 @@
+"""Make all filter definitions available from richie.apps.search.filter_definitions."""
+from django.conf import settings
+from django.utils.module_loading import import_string
+
+# pylint: disable=unused-import
+from ..defaults import FILTERS_CONFIGURATION
+from .base import BaseFilterDefinition, NestingWrapper  # noqa
+from .courses import (  # noqa
+    AvailabilityFilterDefinition,
+    IndexableFilterDefinition,
+    IndexableHierarchicalFilterDefinition,
+    LanguagesFilterDefinition,
+    StaticChoicesFilterDefinition,
+)
+
+FILTERS = {
+    params["name"]: import_string(path)(**params)
+    for path, params in getattr(
+        settings, "RICHIE_FILTERS_CONFIGURATION", FILTERS_CONFIGURATION
+    )
+}
