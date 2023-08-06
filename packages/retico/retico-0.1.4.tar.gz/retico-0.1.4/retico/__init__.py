b9df083ffd145e389be893db881934333e7a5b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:18 +0000

    Add package retico/0.1.4/retico-0.1.4.tar.gz

---
+++ packages/retico/retico-0.1.4.tar.gz/retico-0.1.4/retico/__init__.py
@@ -0,0 +1,8 @@
+import retico.ius as ius
+import retico.modules as modules
+import retico.helper as aux
+
+from retico_core.network import *
+from retico_core.abstract import *
+
+from retico.version import __version__
