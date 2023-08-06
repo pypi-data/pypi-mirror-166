556cc463ba1284700ad29d68d06274c019953418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:13 +0000

    Add package honeybee-ph/1.0.5/honeybee_ph-1.0.5-py2.py3-none-any.whl

---
+++ packages/honeybee-ph/honeybee_ph-1.0.5-py2.py3-none-any.whl/tests/test_honeybee_ph/test_site/test_site_climate_monthly.py
@@ -0,0 +1,37 @@
+from honeybee_ph import site
+
+
+def test_basic_climate_monthly_collection():
+    monthly_climate_collection = site.Climate_MonthlyValueSet()
+
+    assert 'january' in vars(monthly_climate_collection).keys()
+    assert 'july' in vars(monthly_climate_collection).keys()
+    assert 'december' in vars(monthly_climate_collection).keys()
+
+    assert monthly_climate_collection
+
+
+def test_set_climate_monthly_collection_values():
+    monthly_climate_collection = site.Climate_MonthlyValueSet()
+
+    monthly_climate_collection.values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    assert monthly_climate_collection.january == 1
+    assert monthly_climate_collection.july == 7
+    assert monthly_climate_collection.december == 12
+
+
+def test_climate_monthly_collection_serialization_empty():
+    monthly_climate_collection = site.Climate_MonthlyValueSet()
+    d = monthly_climate_collection.to_dict()
+    new_obj = site.Climate_MonthlyValueSet.from_dict(d)
+
+    assert new_obj.to_dict() == d
+
+
+def test_climate_monthly_collection_serialization_with_values():
+    monthly_climate_collection = site.Climate_MonthlyValueSet()
+    monthly_climate_collection.values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    d = monthly_climate_collection.to_dict()
+    new_obj = site.Climate_MonthlyValueSet.from_dict(d)
+
+    assert new_obj.to_dict() == d
