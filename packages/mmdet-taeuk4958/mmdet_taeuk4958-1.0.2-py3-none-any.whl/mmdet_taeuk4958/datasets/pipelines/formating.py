867ca718d1c3106a09efb9e6307dc5a0448584f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:32 +0000

    Add package mmdet-taeuk4958/1.0.2/mmdet_taeuk4958-1.0.2-py3-none-any.whl

---
+++ packages/mmdet-taeuk4958/mmdet_taeuk4958-1.0.2-py3-none-any.whl/mmdet_taeuk4958/datasets/pipelines/formating.py
@@ -0,0 +1,9 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+# flake8: noqa
+import warnings
+
+from .formatting import *
+
+warnings.warn('DeprecationWarning: mmdet_taeuk4958.datasets.pipelines.formating will be '
+              'deprecated, please replace it with '
+              'mmdet_taeuk4958.datasets.pipelines.formatting.')
