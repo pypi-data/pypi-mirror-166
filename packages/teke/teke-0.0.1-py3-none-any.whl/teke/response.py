6e3d3e99bef9b2becf86d517268302a0baf5af1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:15 +0000

    Add package teke/0.0.1/teke-0.0.1-py3-none-any.whl

---
+++ packages/teke/teke-0.0.1-py3-none-any.whl/teke/response.py
@@ -0,0 +1,41 @@
+import json
+from multiprocessing import connection
+from typing import Union, Optional, Mapping, Any
+
+from .connection import Connection
+
+
+class HttpResponse:
+    def __init__(
+        self,
+        body: Optional[Union[bytes, str]] = b"",
+        connection: Optional[Connection] = None,
+        *,
+        status_code: int = 200,
+        headers: Optional[Mapping[str, str]] = None
+    ) -> None:
+        self.body = body
+        self.connection = connection
+        self.status_code = status_code
+        self.headers = headers
+
+    def __await__(self):
+        if not self.connection:
+            raise ValueError("No connection")
+        self.connection.response_status_code = self.status_code
+        if self.headers:
+            for key, value in self.headers.items():
+                self.connection.insert_response_header(key, value)
+        return self.connection.send(self.body, finish=True).__await__()
+
+
+class JsonResponse(HttpResponse):
+    def __init__(
+        self, data: Any, connection: Optional[Connection] = None, *args, **kwargs
+    ):
+        body = json.dumps(data)
+        headers = kwargs.get("headers")
+        if headers is None:
+            headers = {}
+        headers["content-type"] = "application/json"
+        super().__init__(body, connection, *args, **kwargs)
