7c19cc9e9593f694078d8c02d34bdd372e81b27f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:29 +0000

    Add package potc-typing/0.0.2/potc_typing-0.0.2-py3-none-any.whl

---
+++ packages/potc-typing/potc_typing-0.0.2-py3-none-any.whl/potc_typing/config/meta.py
@@ -0,0 +1,19 @@
+"""
+Overview:
+    Meta information for potc package.
+"""
+
+#: Title of this project (should be `potc-typing`).
+__TITLE__ = "potc-typing"
+
+#: Version of this project.
+__VERSION__ = "0.0.2"
+
+#: Short description of the project, will be included in ``setup.py``.
+__DESCRIPTION__ = 'Pretty dict generating plugin for potc.'
+
+#: Author of this project.
+__AUTHOR__ = "HansBug"
+
+#: Email of the authors'.
+__AUTHOR_EMAIL__ = "hansbug@buaa.edu.cn"
---
+++ packages/potc-typing/potc_typing-0.0.2-py3-none-any.whl/potc_typing/plugin.py
@@ -0,0 +1,7 @@
+from .types_ import types_all
+from .typing import typing_all
+
+__rules__ = [
+    typing_all,
+    types_all,
+]
