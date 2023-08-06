82094431cec8123b1ba13ee19b785e678cb76159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:10 +0000

    Add package firebmail/0.1.1/firebmail-0.1.1.tar.gz

---
+++ packages/firebmail/firebmail-0.1.1.tar.gz/firebmail-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='firebmail',
+    version='0.1.1',
+    license='MIT',
+    url='https://github.com/firebreather-heart/firebmail',
+    author="firebreather",
+    author_email='dtenny95@gmail.com',
+    install_requires=[
+          'python-dotenv',
+      ],
+
+)
