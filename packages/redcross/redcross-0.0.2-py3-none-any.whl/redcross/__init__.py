080314192fe5fdfcaa41d6f3fda7ad4364de477a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:33 +0000

    Add package redcross/0.0.2/redcross-0.0.2-py3-none-any.whl

---
+++ packages/redcross/redcross-0.0.2-py3-none-any.whl/redcross/__init__.py
@@ -0,0 +1,9 @@
+from .datacube import Datacube
+from .cross_correlation import CCF, KpV, Template
+from .read import read_harpsn, read_giano
+from .pipeline import Pipeline
+__author__ = 'Darío González Picos'
+__license__ = 'GPLv3'
+__version__ = '0.1'
+__email__ = 'dariogonzalezpicos@gmail.com'
+__status__ = 'Development'
