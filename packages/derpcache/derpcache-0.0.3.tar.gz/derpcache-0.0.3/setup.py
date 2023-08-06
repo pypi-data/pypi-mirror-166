d7ce28cfca77760efb59fc2e2ecd2ca9a699095d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:40 +0000

    Add package derpcache/0.0.3/derpcache-0.0.3.tar.gz

---
+++ packages/derpcache/derpcache-0.0.3.tar.gz/derpcache-0.0.3/setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='derpcache',
+    version='0.0.3',
+    description='Simple pickle-based caching utility.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='GPL-3',
+    author='Ben Johnson',
+    author_email='bkitej@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'pickle5',
+    ],
+    extras_require={
+        'dev': [
+            'commitizen',
+            'pre-commit',
+            'twine',
+        ],
+        'lint': [
+            'black',
+            'flakeheaven',
+            'mypy',
+            'types-setuptools',
+        ],
+        'test': [
+            'faker',
+            'pytest',
+            'pytest-freezegun',
+        ],
+    },
+    zip_safe=False,  # for mypy to detect typing stubs
+)
