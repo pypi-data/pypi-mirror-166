538a0f72323ec9613008f136e458a94da33b6f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:20:01 +0000

    Add package tkitRegex/0.0.0.116624588/tkitRegex-0.0.0.116624588-py2.py3-none-any.whl

---
+++ packages/tkitRegex/tkitRegex-0.0.0.116624588-py2.py3-none-any.whl/tkitRegex/__init__.py
@@ -0,0 +1,5 @@
+# from .api import auto_request,SentenceClassification,AutoReWrite,dff_text,get_markdown_images,get_markdown_images_format
+# from .diff import dff_text
+# # from rewriter import *
+from .regex_plus import auto_regex_replace,auto_regex
+
