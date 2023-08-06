af44dc9150514167edaee145e8a7617c34e87f5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:59 +0000

    Add package ipylabel/0.1.1/ipylabel-0.1.1.tar.gz

---
+++ packages/ipylabel/ipylabel-0.1.1.tar.gz/ipylabel-0.1.1/src/TextWidget.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { useModelState, WidgetModelContext } from "./hooks/widget-model";
+import { WidgetProps } from "./types";
+import { Button, Checkbox, DropDown, Selectable } from "./components";
+
+// widget state, don't forget to update `ipylabel/text.py`
+export const defaultModelProperties: {
+  text: string;
+  labels: string[];
+  colors: string[]; // should be color
+  result: { start: number; end: number; label: string }[];
+  finished: boolean;
+} = {
+  // use as inputs
+  text: "",
+  labels: [],
+  colors: [],
+  // set as outputs
+  result: [],
+  finished: false,
+};
+
+const TextWidget = (props: WidgetProps) => {
+  // use as input
+  const [text] = useModelState("text");
+  const [labels] = useModelState("labels");
+  const [colors] = useModelState("colors");
+
+  // set as output
+  // const [result, setResult] = useModelState("result");
+  // const [result] = useModelState("result");
+  const [finished, setFinished] = useModelState("finished");
+
+  const [selectedLabel, setSelectedLabel] = useState(labels[0]);
+  const [selectedColor, setSelectedColor] = useState(colors[0]);
+
+  return (
+    <div className="TextWidget">
+      <div className="flex-column">
+        <div className="flex-row">
+          {/* alligned left */}
+          <DropDown
+            label="dropdown"
+            options={labels.map((label, index) => ({
+              text: label,
+              color: colors[index],
+            }))}
+            onChange={({ text, color }) => {
+              setSelectedLabel(text);
+              setSelectedColor(color);
+            }}
+            disabled={finished}
+          />
+
+          <Button
+            label={`Label selected as ${selectedLabel}`}
+            disabled={finished}
+            background={selectedColor}
+          />
+          <Checkbox
+            value={finished}
+            label="Nothing to label"
+            onChange={() => setFinished(!finished)}
+            disabled={finished}
+          />
+
+          {/* spacer */}
+          <div className="m-auto" />
+
+          {/* alligned right */}
+          <Button label="Remove selected" disabled={finished} />
+          <Button label="Reset" disabled={finished} />
+        </div>
+        <div>
+          <Selectable text={text} disabled={finished} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function withModelContext(Component: (props: WidgetProps) => JSX.Element) {
+  return (props: WidgetProps) => (
+    <WidgetModelContext.Provider value={props.model}>
+      <Component {...props} />
+    </WidgetModelContext.Provider>
+  );
+}
+
+export default withModelContext(TextWidget);
---
+++ packages/ipylabel/ipylabel-0.1.1.tar.gz/ipylabel-0.1.1/src/components/DropDown.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+
+export interface DropDownProps {
+  label: string;
+  options: { text: string; color: string }[];
+  disabled: boolean;
+  onChange: ({ text, color }: { text: string; color: string }) => void;
+}
+
+const DropDown = ({
+  label,
+  options,
+  disabled = false,
+  onChange,
+}: DropDownProps): JSX.Element => {
+  const [selectedText, setSelectedText] = useState(options[0].text);
+  const [selectedColor, setSelectedColor] = useState(options[0].color);
+
+  useEffect(
+    () => onChange({ text: selectedText, color: selectedColor }),
+    [selectedColor, selectedText],
+  );
+
+  return (
+    <div>
+      <label>
+        {label}
+        <select
+          onChange={(event) => {
+            const option = options[parseInt(event.target.value)];
+            setSelectedText(option.text);
+            setSelectedColor(option.color);
+          }}
+        >
+          {options.map((option, index) => (
+            <option value={index}>{option.text}</option>
+          ))}
+        </select>
+      </label>
+    </div>
+  );
+};
+
+export default DropDown;
