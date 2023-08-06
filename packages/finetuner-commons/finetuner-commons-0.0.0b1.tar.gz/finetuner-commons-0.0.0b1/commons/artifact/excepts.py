831f001b913e1d58cd7603cf51f93630bebb8202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:45 +0000

    Add package finetuner-commons/0.0.0b1/finetuner-commons-0.0.0b1.tar.gz

---
+++ packages/finetuner-commons/finetuner-commons-0.0.0b1.tar.gz/finetuner-commons-0.0.0b1/commons/artifact/excepts.py
@@ -0,0 +1,18 @@
+class ArtifactNotFound(Exception):
+    ...
+
+
+class CorruptedArtifact(Exception):
+    ...
+
+
+class CorruptedMetadata(Exception):
+    ...
+
+
+class NoSuchModel(Exception):
+    ...
+
+
+class SelectModelRequired(Exception):
+    ...
