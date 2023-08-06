eebcff2f22cb68fa922338c8e2b92f3b40d97aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:20 +0000

    Add package gym-sf/0.0.1/gym-sf-0.0.1.tar.gz

---
+++ packages/gym-sf/gym-sf-0.0.1.tar.gz/gym-sf-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='gym-sf',
+      version='0.0.1',
+      description='Environments adapted to the use of successor features',
+      author="Rafael F Cunha",
+      author_email="rafaelcunha2013@gmail.com",
+      url="https://github.com/rafaelcunha2013/gym-sf",
+      license='MIT',
+      python_requires=">=3.8",
+      install_requires=['gym==0.25.2',
+                        'numpy',
+                        'pygame==2.1.2']
+      )
+
+# https://www.gymlibrary.dev/content/environment_creation/
+# https://towardsdatascience.com/create-your-own-python-package-and-publish-it-into-pypi-9306a29bc116
+
