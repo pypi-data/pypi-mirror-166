ad7649e6333c10b2e1601142527cb91886f93093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:59 +0000

    Add package cdisc-rules-engine/0.3.6.2/cdisc-rules-engine-0.3.6.2.tar.gz

---
+++ packages/cdisc-rules-engine/cdisc-rules-engine-0.3.6.2.tar.gz/cdisc-rules-engine-0.3.6.2/cdisc_rules_engine/models/dictionaries/__init__.py
@@ -0,0 +1,11 @@
+"""
+This module contains DB models related
+to dictionaries like WhoDrug, MedDra etc.
+"""
+
+
+from .dictionary_types import DictionaryTypes
+
+__all__ = [
+    "DictionaryTypes",
+]
