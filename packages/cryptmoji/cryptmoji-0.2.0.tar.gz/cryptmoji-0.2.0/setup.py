a183b6824b8e7c46a7ae75cb3349d70c67705bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:42 +0000

    Add package cryptmoji/0.2.0/cryptmoji-0.2.0.tar.gz

---
+++ packages/cryptmoji/cryptmoji-0.2.0.tar.gz/cryptmoji-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cryptmoji']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'cryptmoji',
+    'version': '0.2.0',
+    'description': 'Encrypt Text using emojis!',
+    'long_description': '# Cryptmoji\n\nA simple emoji-based encryption algorithm.\n_______________________\n\n## Installation\n\npip install the library:\n\n```sh\npip install cryptmoji\n```\n\n## Usage\n\n```python\nfrom cryptmoji import Cryptmoji\n\ntext = "Hello World!"\nkey = "random_key" # makes the encryption stronger (optional)\na = Cryptmoji(text, key=key)\nencrypted = a.encrypt()\nprint(encrypted)\ndecrypted = a.decrypt()\nprint(decrypted)\n```\n',
+    'author': 'Siddhesh Agarwal',
+    'author_email': 'siddhesh.agarwal@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Siddhesh-Agarwal/cryptmoji',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
