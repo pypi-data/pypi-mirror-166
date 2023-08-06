2779252623508b4a65f9da150d58da63af871071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:09 +0000

    Add package astrocalculator/0.1.4/astrocalculator-0.1.4.tar.gz

---
+++ packages/astrocalculator/astrocalculator-0.1.4.tar.gz/astrocalculator-0.1.4/setup.py
@@ -0,0 +1,25 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='astrocalculator',
+    version='0.1.4',
+    description='AstroCalculator, a Calculator for Astronomers and Physicists',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/chongchonghe/acap.git',
+    author='Chong-Chong He',
+    author_email='che1234@umd.edu',
+    license="MIT",
+    # packages=find_packages(),
+    packages=["calc"],
+    # entry_points={'console_scripts': ['calc=calc:main']},
+    entry_points={'console_scripts': ['calc=calc.__init__:main']},
+    install_requires=['sympy>=1.6', 'astropy>=4.0'],
+)
