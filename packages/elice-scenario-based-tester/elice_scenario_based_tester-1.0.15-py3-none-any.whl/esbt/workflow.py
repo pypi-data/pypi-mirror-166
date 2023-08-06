fd0663190dcf98d3182d3c66a2d1d574dddd9a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:09 +0000

    Add package elice-scenario-based-tester/1.0.15/elice_scenario_based_tester-1.0.15-py3-none-any.whl

---
+++ packages/elice-scenario-based-tester/elice_scenario_based_tester-1.0.15-py3-none-any.whl/esbt/workflow.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+import hashlib
+from functools import cached_property
+from typing import Dict, Union
+
+import aiohttp
+import msgpack
+from esbt.model import ScenarioBase
+from pydantic import BaseModel
+
+
+class Workflow(BaseModel):
+    dependency: Dict[tuple[str, ScenarioBase], list[tuple[str, ScenarioBase]]]
+    context: Dict[str, Union[str, bool]]
+    session: aiohttp.ClientSession
+
+    @cached_property
+    def ident(self) -> str:
+        return hashlib.sha256(msgpack.packb(self.dict())).hexdigest()
+
+    class Config:
+        keep_untouched = (cached_property,)
+        arbitrary_types_allowed = True
+
+
+def init_dependency(
+    dependency: Dict[tuple[str, ScenarioBase], list[tuple[str, ScenarioBase]]] | None = {},
+):
+    for k, v in dependency.items():
+        workflow.dependency[k] = v
+
+
+workflow = Workflow(
+    dependency={},
+    context={},
+    session=aiohttp.ClientSession(
+        connector=aiohttp.TCPConnector(ssl=False)),
+)
