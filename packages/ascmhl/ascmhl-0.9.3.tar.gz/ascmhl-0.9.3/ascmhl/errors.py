c9b00f08a84204d03477f50b2407134b627067bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:11 +0000

    Add package ascmhl/0.9.3/ascmhl-0.9.3.tar.gz

---
+++ packages/ascmhl/ascmhl-0.9.3.tar.gz/ascmhl-0.9.3/ascmhl/errors.py
@@ -0,0 +1,59 @@
+"""
+__author__ = "Jon Waggoner, Patrick Renner"
+__copyright__ = "Copyright 2020, Pomfort GmbH"
+
+__license__ = "MIT"
+__maintainer__ = "Patrick Renner"
+__email__ = "opensource@pomfort.com"
+"""
+
+import click
+
+
+class CompletenessCheckFailedException(click.ClickException):
+    exit_code = 15
+
+    def __init__(self):
+        super().__init__("Files referenced in the ASC MHL history are missing")
+
+
+class NoMHLHistoryException(click.ClickException):
+    exit_code = 11
+
+    def __init__(self, path):
+        super().__init__(f"Missing ASC MHL history at path {path}")
+
+
+class VerificationFailedException(click.ClickException):
+    exit_code = 12
+
+    def __init__(self):
+        super().__init__("Verification of files referenced in the ASC MHL history failed")
+
+
+class VerificationDirectoriesFailedException(click.ClickException):
+    exit_code = 15
+
+    def __init__(self):
+        super().__init__("Verification of directories referenced in the ASC MHL history failed")
+
+
+class NewFilesFoundException(click.ClickException):
+    exit_code = 13
+
+    def __init__(self):
+        super().__init__("New files not referenced in the ASC MHL history have been found")
+
+
+class NoMHLHistoryExceptionForPath(click.ClickException):
+    exit_code = 14
+
+    def __init__(self, path):
+        super().__init__(f"Missing ASC MHL history for path {path}")
+
+
+class SingelFileNotFoundException(click.ClickException):
+    exit_code = 15
+
+    def __init__(self):
+        super().__init__("Could not find single file.")
