62c523201390044b0ec755c6628afb30444201e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:31 +0000

    Add package riego/0.10.1/riego-0.10.1-py3-none-any.whl

---
+++ packages/riego/riego-0.10.1-py3-none-any.whl/riego/migrations/_tmp_yoyonewi8m817hk.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+
+from yoyo import step
+
+__depends__ = {'20210214_01_WyOHg'}
+
+steps = [
+    step("ALTER TABLE boxes ADD COLUMN rssi (int)
+                            ADD COLUMN link_count int
+                            ADD COLUMN down_time varchar(255)")
+]
---
+++ packages/riego/riego-0.10.1-py3-none-any.whl/riego/migrations/_tmp_yoyonewwkavt8qd.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+
+from yoyo import step
+
+__depends__ = {'20210214_01_WyOHg'}
+
+steps = [
+    step("ALTER TABLE boxes ADD COLUMN rssi INT")
+]
+    
