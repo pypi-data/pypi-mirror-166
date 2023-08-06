052328e8f7e575921d4968e123722dd4fbe86299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:53 +0000

    Add package mc920/0.1/mc920-0.1.tar.gz

---
+++ packages/mc920/mc920-0.1.tar.gz/mc920-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mc920',
+      version='0.1',
+      description='Assignments of Image Processing Course (MC920)',
+      url='https://github.com/mauricio-lorenzetti/mc920',
+      author='Mauricio Lorenzetti',
+      author_email='mauricio.lorenzetti.bezerra@gmail.com',
+      license='MIT',
+      packages=['mc920'],
+      zip_safe=False)
