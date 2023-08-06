4052420c36b94bc03d524de2d7a6db9b277a5caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:24 +0000

    Add package abork-config-initiator/1.1.7/abork_config_initiator-1.1.7.tar.gz

---
+++ packages/abork-config-initiator/abork_config_initiator-1.1.7.tar.gz/abork_config_initiator-1.1.7/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='abork_config_initiator',
+    version='1.1.7',
+    author='Alex Bork',
+    packages=['py_config_initiator'],
+    description='Configuration File Initiator'
+    )
