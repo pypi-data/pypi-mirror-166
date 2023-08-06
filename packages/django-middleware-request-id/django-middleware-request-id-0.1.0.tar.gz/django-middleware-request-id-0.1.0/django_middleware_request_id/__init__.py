44dbc3f1ac1b1c6b4b8024715a69373853aa5bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:46 +0000

    Add package django-middleware-request-id/0.1.0/django-middleware-request-id-0.1.0.tar.gz

---
+++ packages/django-middleware-request-id/django-middleware-request-id-0.1.0.tar.gz/django-middleware-request-id-0.1.0/django_middleware_request_id/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+from __future__ import absolute_import, division, generators, nested_scopes, print_function, unicode_literals, with_statement
+
+app_requires = [
+    "django_middleware_global_request",
+]
+
+app_middleware_requires = [
+    "django_middleware_global_request.middleware.GlobalRequestMiddleware",
+    "django_middleware_request_id.middlewares.DjangoMiddlewareRequestId",
+]
+
+app_setting_callbacks = [
+    "django_middleware_request_id.log_filters.add_request_id_log_filter",
+]
