8e5e102af6ba21e6deccc284f4481c686205fe08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:08 +0000

    Add package wanna-ml/0.2.9/wanna-ml-0.2.9.tar.gz

---
+++ packages/wanna-ml/wanna-ml-0.2.9.tar.gz/wanna-ml-0.2.9/src/wanna/core/models/docker.py
@@ -0,0 +1,68 @@
+import sys
+from enum import Enum
+from pathlib import Path
+from typing import Dict, List, Optional, Union
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+from pydantic import BaseModel, Extra, Field
+
+
+class DockerBuildConfigModel(BaseModel, extra=Extra.forbid):
+    # Docu for more info: https://gabrieldemarmiesse.github.io/python-on-whales/sub-commands/buildx/#build
+    build_args: Dict[str, str] = {}
+    add_hosts: Dict[str, str] = {}
+    labels: Dict[str, str] = {}
+    network: Optional[str]
+    platforms: Optional[List[str]]
+    secrets: Union[str, List[str]] = []
+    ssh: Optional[str]
+    target: Optional[str]
+
+
+class ImageBuildType(str, Enum):
+    local_build_image = "local_build_image"
+    provided_image = "provided_image"
+    notebook_ready_image = "notebook_ready_image"
+
+
+class BaseDockerImageModel(BaseModel, extra=Extra.forbid, validate_assignment=True):
+    name: str = Field(min_length=3, max_length=128)
+
+
+class LocalBuildImageModel(BaseDockerImageModel):
+    build_type: Literal[ImageBuildType.local_build_image]
+    build_args: Optional[Dict[str, str]]
+    context_dir: Path
+    dockerfile: Path
+
+
+class ProvidedImageModel(BaseDockerImageModel):
+    build_type: Literal[ImageBuildType.provided_image]
+    image_url: str
+
+
+class NotebookReadyImageModel(BaseDockerImageModel):
+    build_type: Literal[ImageBuildType.notebook_ready_image]
+    build_args: Optional[Dict[str, str]]
+    base_image: str = "gcr.io/deeplearning-platform-release/base-cpu"
+    requirements_txt: Path
+
+
+DockerImageModel = Union[LocalBuildImageModel, ProvidedImageModel, NotebookReadyImageModel]
+
+
+class DockerModel(BaseModel, extra=Extra.forbid, validate_assignment=True):
+    images: List[DockerImageModel] = []
+    repository: Optional[str]
+    registry: Optional[str]
+    cloud_build: bool = False
+
+
+class DockerBuildResult(BaseModel, extra=Extra.forbid):
+    name: str
+    tags: List[str]
+    build_type: ImageBuildType
---
+++ packages/wanna-ml/wanna-ml-0.2.9.tar.gz/wanna-ml-0.2.9/src/wanna/core/models/pipeline.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Union
+
+from pydantic import Field
+
+from wanna.core.models.base_instance import BaseInstanceModel
+from wanna.core.models.cloud_scheduler import CloudSchedulerModel
+
+
+class PipelineModel(BaseInstanceModel):
+    name: str = Field(min_length=3, max_length=63, to_lower=True, regex="^[a-z][a-z0-9-]*[a-z0-9]$")
+    zone: str
+    pipeline_file: str
+    pipeline_function: Optional[str]
+    pipeline_params: Union[Path, Dict[str, Any], None]
+    docker_image_ref: List[str] = []
+    schedule: Optional[CloudSchedulerModel]
+    tensorboard_ref: Optional[str]
+    network: Optional[str]
+    notification_channels_ref: List[str] = []
+    sla_hours: Optional[float]
