7104e959adf190bfb40ee1850a955db6b0d8058c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:26 +0000

    Add package manasafiles/146/manasafiles-146.tar.gz

---
+++ packages/manasafiles/manasafiles-146.tar.gz/manasafiles-146/manasafiles/__init__.py
@@ -0,0 +1,5 @@
+# This is so that you can import ppack or import average from ppack
+# in stead of from ppack.functions import average...........
+
+from .decorators import singleton
+from .functions import listChunker, report, weirdCase
