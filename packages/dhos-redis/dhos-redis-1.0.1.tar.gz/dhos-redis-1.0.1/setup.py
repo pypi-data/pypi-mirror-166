6871f2c6e746352f9a2ca0ae23fccea37679d219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:07 +0000

    Add package dhos-redis/1.0.1/dhos-redis-1.0.1.tar.gz

---
+++ packages/dhos-redis/dhos-redis-1.0.1.tar.gz/dhos-redis-1.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dhosredis']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['environs>=9.0.0,<10.0.0', 'redis>=3.0.0,<4.0.0', 'she-logging>=1.0.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'dhos-redis',
+    'version': '1.0.1',
+    'description': 'Redis functionality for Polaris',
+    'long_description': None,
+    'author': 'Jon Daly',
+    'author_email': 'jonathan.daly@sensynehealth.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/draysontechnologies/dhos-redis',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
