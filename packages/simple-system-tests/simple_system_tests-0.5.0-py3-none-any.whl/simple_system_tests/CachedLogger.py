742052f02c512051e2d3ef3912045b40860954f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:59 +0000

    Add package simple-system-tests/0.5.0/simple_system_tests-0.5.0-py3-none-any.whl

---
+++ packages/simple-system-tests/simple_system_tests-0.5.0-py3-none-any.whl/simple_system_tests/CachedLogger.py
@@ -0,0 +1,50 @@
+import logging
+import io
+import sys
+
+class LoggerWriter:
+    def __init__(self, logfct):
+        self.__logfct = logfct
+        self.__buf = []
+
+    def write(self, msg):
+        if msg.endswith('\n'):
+            self.__buf.append(msg.strip('\n'))
+            self.__logfct(''.join(self.__buf))
+            self.__buf = []
+        else:
+            self.__buf.append(msg)
+
+    def flush(self):
+        pass
+
+class CachedLogger(object):
+    def __init__(self):
+        self.__log_capture_string = None
+    def start_logging(self):
+        self.__old_stdout = sys.stdout
+        self.__old_stderr = sys.stderr
+        self.__log_capture_string = io.StringIO()
+        ch = logging.StreamHandler(self.__log_capture_string)
+        ch.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+
+        self.__logger = logging.getLogger('basic_logger')
+        sys.stdout = LoggerWriter(self.__logger.info)
+        sys.stderr = LoggerWriter(self.__logger.error)
+        self.__logger .setLevel(logging.DEBUG)
+        self.__logger .addHandler(ch)
+        return self.__logger
+
+    def stop_logging(self):
+        log_contents = self.__log_capture_string.getvalue()
+        self.__log_capture_string.flush()
+        sys.stdout = self.__old_stdout
+        sys.stderr = self.__old_stderr
+        print(log_contents)
+        return log_contents
+
+    def __del__(self):
+        if self.__log_capture_string:
+            self.__log_capture_string.close()
