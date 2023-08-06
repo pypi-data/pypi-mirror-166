a06ab67e8e7ed4e514f0cba19f5d1f14e03e49ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:08 +0000

    Add package multi-prophet/1.1.0/multi_prophet-1.1.0-py3-none-any.whl

---
+++ packages/multi-prophet/multi_prophet-1.1.0-py3-none-any.whl/multi_prophet/plots.py
@@ -0,0 +1,8 @@
+from prophet.plot import plot_plotly, plot_components_plotly
+
+
+def plotly_plot(model, forecast, **kwargs):
+    return plot_plotly(model, forecast, **kwargs)
+
+def plotly_components_plot(model, forecast, **kwargs):
+    return plot_components_plotly(model, forecast, **kwargs)
