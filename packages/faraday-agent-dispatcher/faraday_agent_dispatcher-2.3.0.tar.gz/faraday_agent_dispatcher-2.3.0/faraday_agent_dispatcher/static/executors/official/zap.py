db266386f54151721d7f788d76e19bd0e7522516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:32 +0000

    Add package faraday-agent-dispatcher/2.3.0/faraday_agent_dispatcher-2.3.0.tar.gz

---
+++ packages/faraday-agent-dispatcher/faraday_agent_dispatcher-2.3.0.tar.gz/faraday_agent_dispatcher-2.3.0/faraday_agent_dispatcher/static/executors/official/zap.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import os
+import sys
+import time
+import subprocess
+from zapv2 import ZAPv2
+from faraday_plugins.plugins.repo.zap.plugin import ZapPlugin
+
+
+def main():
+    # If the script is run outside the dispatcher the environment variables
+    # are checked.
+    # ['ZAP_API_KEY', 'EXECUTOR_CONFIG_TARGET_URL']
+    ignore_info = os.getenv("AGENT_CONFIG_IGNORE_INFO", "False").lower() == "true"
+    hostname_resolution = os.getenv("AGENT_CONFIG_RESOLVE_HOSTNAME", "True").lower() == "true"
+    vuln_tag = os.getenv("AGENT_CONFIG_VULN_TAG", None)
+    if vuln_tag:
+        vuln_tag = vuln_tag.split(",")
+    service_tag = os.getenv("AGENT_CONFIG_SERVICE_TAG", None)
+    if service_tag:
+        service_tag = service_tag.split(",")
+    host_tag = os.getenv("AGENT_CONFIG_HOSTNAME_TAG", None)
+    if host_tag:
+        host_tag = host_tag.split(",")
+    try:
+        target = os.environ["EXECUTOR_CONFIG_TARGET_URL"]
+        api_key = os.environ["ZAP_API_KEY"]
+    except KeyError:
+        print("environment variable not found", file=sys.stderr)
+        sys.exit()
+
+    # zap is required to be started
+    zap_run = subprocess.check_output("pgrep -f zap", shell=True)
+
+    if len(zap_run.decode("utf-8").split("\n")) > 3:
+        # the apikey from ZAP->Tools->Options-API
+        zap = ZAPv2(apikey=api_key)
+        # it passes the url to scan and starts
+        scanID = zap.spider.scan(target)
+        # Wait for the scan to finish
+        while int(zap.spider.status(scanID)) < 100:
+            time.sleep(1)
+        # If finish the scan and the xml is generated
+        zap_result = zap.core.xmlreport()
+        plugin = ZapPlugin(
+            ignore_info=ignore_info,
+            hostname_resolution=hostname_resolution,
+            host_tag=host_tag,
+            service_tag=service_tag,
+            vuln_tag=vuln_tag,
+        )
+        plugin.parseOutputString(zap_result)
+        print(plugin.get_json())
+
+    else:
+        print("ZAP not running", file=sys.stderr)
+        sys.exit()
+
+
+if __name__ == "__main__":
+    main()
