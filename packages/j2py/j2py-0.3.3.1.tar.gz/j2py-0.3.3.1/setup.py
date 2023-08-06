d79368fad3cc6e8c409ea070dc5f42564c5c826a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:54 +0000

    Add package j2py/0.3.3.1/j2py-0.3.3.1.tar.gz

---
+++ packages/j2py/j2py-0.3.3.1.tar.gz/j2py-0.3.3.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='j2py',
+    version='0.3.3.1',
+    license='gpl-3.0',
+    author="Alberto Brandolini",
+    author_email='brando1411@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/ArcticBlueOrange/jupyterconverter',
+    keywords='j2py jupyter-converter',
+    # install_requires=[ 'pathlib', ],
+)
