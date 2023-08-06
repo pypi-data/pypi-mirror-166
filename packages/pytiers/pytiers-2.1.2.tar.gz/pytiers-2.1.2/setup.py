8620ab8313f1df013e7e306a1f5bc079a6d59d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:01 +0000

    Add package pytiers/2.1.2/pytiers-2.1.2.tar.gz

---
+++ packages/pytiers/pytiers-2.1.2.tar.gz/pytiers-2.1.2/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name='pytiers',
+    version='2.1.2',
+    author='Huang Bo-Xuan',
+    author_email='r09142003@gmail.com',
+    install_requires=[
+        'seaborn',
+        'matplotlib',
+        'pandas',
+        'plotly',
+        'nbformat>=4.2.0'
+    ]
+)
