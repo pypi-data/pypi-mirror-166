1c243504444f93167a7d335233e604c3c2f6d425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:13 +0000

    Add package JaesonTest/0.0.0.2/JaesonTest-0.0.0.2-py3-none-any.whl

---
+++ packages/JaesonTest/JaesonTest-0.0.0.2-py3-none-any.whl/JaesonTest/test1.py
@@ -0,0 +1,5 @@
+
+def printTest1():
+    '''테스트 문장 출력'''
+    print("Hi, test1")
+    print("This is Mark2")
---
+++ packages/JaesonTest/JaesonTest-0.0.0.2-py3-none-any.whl/JaesonTest/test2.py
@@ -0,0 +1,5 @@
+
+def printTest2():
+    '''테스트 문장 출력'''
+    print("안녕, test2")
+    print("마크2 버전")
