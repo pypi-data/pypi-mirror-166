d8766f2fa9bbf873ad1ca43483c2556c71e13b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:24 +0000

    Add package jsonparse/0.4.1/jsonparse-0.4.1.tar.gz

---
+++ packages/jsonparse/jsonparse-0.4.1.tar.gz/jsonparse-0.4.1/setup.py
@@ -0,0 +1,30 @@
+# local imports
+import jsonparse
+
+# python imports
+from setuptools import setup
+import os
+
+# read from the VERSION file
+with open(os.path.join(os.path.dirname(jsonparse.__file__), 'VERSION')) as version_file:
+    version = version_file.read().strip()
+
+# Package meta-data.
+NAME = 'jsonparse'
+DESCRIPTION = 'Search through JSON data for what you need'
+URL = url = 'https://github.com/ctomkow/jsonparse'
+EMAIL = 'ctomkow@gmail.com'
+AUTHOR = 'Craig Tomkow'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = version
+
+setup(
+        name=NAME,
+        version=VERSION,
+        description=DESCRIPTION,
+        url=URL,
+        author=AUTHOR,
+        author_email=EMAIL,
+        license='MIT',
+        packages=['jsonparse']
+    )
