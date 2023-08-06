f336bcdb6c0fd9bc70c48babd922a35d7c992fde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:33 +0000

    Add package evnex/0.1.0/evnex-0.1.0.tar.gz

---
+++ packages/evnex/evnex-0.1.0.tar.gz/evnex-0.1.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['evnex']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.23.0,<0.24.0',
+ 'pycognito>=2022.8.0,<2023.0.0',
+ 'pydantic>=1.10.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'evnex',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Brian Thorne',
+    'author_email': 'brian@hardbyte.nz',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
