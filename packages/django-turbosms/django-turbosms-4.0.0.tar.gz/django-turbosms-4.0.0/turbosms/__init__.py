749ae0dc23873d5342d61948ff37faaaf7bbd79d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:02 +0000

    Add package django-turbosms/4.0.0/django-turbosms-4.0.0.tar.gz

---
+++ packages/django-turbosms/django-turbosms-4.0.0.tar.gz/django-turbosms-4.0.0/turbosms/__init__.py
@@ -0,0 +1,29 @@
+
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+def setup_settings(settings, is_prod, **kwargs):
+
+    settings['DATABASE_ROUTERS'] = (
+        settings.get('DATABASE_ROUTERS', []) +
+        ['turbosms.routers.TurboSMSRouter']
+    )
+
+    if settings.get('IS_SMS_ENABLED'):
+        settings['DATABASES']['turbosms'] = {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'users',
+            'USER': settings.get('SMS_USERNAME'),
+            'PASSWORD': settings.get('SMS_PASSWORD'),
+            'HOST': '94.249.146.189',
+            'PORT': ''
+        }
+
+
+class TurboSMSConfig(AppConfig):
+    name = 'turbosms'
+    verbose_name = _("Turbosms")
+
+
+default_app_config = 'turbosms.TurboSMSConfig'
