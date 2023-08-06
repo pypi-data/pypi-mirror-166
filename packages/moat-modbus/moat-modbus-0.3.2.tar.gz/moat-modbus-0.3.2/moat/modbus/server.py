453c35c5f37afa58182776169b97d93e42071003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:00 +0000

    Add package moat-modbus/0.3.2/moat-modbus-0.3.2.tar.gz

---
+++ packages/moat-modbus/moat-modbus-0.3.2.tar.gz/moat-modbus-0.3.2/moat/modbus/server.py
@@ -0,0 +1,263 @@
+#!/usr/bin/python3
+
+import anyio
+import socket
+from collections.abc import Mapping, Iterable
+from typing import Dict
+
+from pymodbus.constants import Defaults
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+from pymodbus.datastore.store import BaseModbusDataBlock
+from pymodbus.device import ModbusControlBlock
+from pymodbus.device import ModbusDeviceIdentification
+from pymodbus.exceptions import NoSuchSlaveException
+from pymodbus.factory import ServerDecoder
+from pymodbus.pdu import ModbusExceptions as merror
+from pymodbus.utilities import hexlify_packets
+
+from moat.modbus.types import DataBlock
+
+from binascii import b2a_hex
+import traceback
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class BaseModbusServer:
+    def __init__(self, identity=None, response_manipulator=None):
+        self.context = ModbusServerContext(single=False)
+        self.context._slaves = self.units = {}
+        self.control = ModbusControlBlock()
+        self.broadcast_enable = False
+        self.response_manipulator = response_manipulator
+
+        if isinstance(identity, ModbusDeviceIdentification):
+            self.control.Identity.update(identity)
+
+        if identity is None:
+            identity = ModbusDeviceIdentification()
+            identity.VendorName = "Matthias Urlichs"
+            identity.ProductCode = "MoaT.modbus"
+            identity.VendorUrl = "http://M-o-a-T.org/"
+            identity.ProductName = "MoaT-Modbus Test"
+            identity.ModelName = "MoaT-Modbus Test"
+            identity.MajorMinorRevision = "1.0"
+        self.identity = identity
+
+    def add_unit(self, unit):
+        if unit in self.units:
+            raise RuntimeError(f"Unit {unit} already exists")
+        self.units[unit] = s = ModbusSlaveContext(
+            di = DataBlock(),
+            co = DataBlock(),
+            ir = DataBlock(),
+            hr = DataBlock(),
+            zero_mode=True,
+        )
+        return s
+
+    async def serve(self, opened=None):
+        raise RuntimeError("You need to override .serve")
+
+    async def process_request(self, request):
+        context = self.context[request.unit_id]
+        response = request.execute(context)
+        return response
+
+
+
+class SerialModbusServer(BaseModbusServer):
+    def __init__(self, identity=None, **args):
+        super().__init__(identity=identity)
+        self.args = args
+
+        from pymodbus.framer.rtu_framer import ModbusRtuFramer
+
+        self.decoder = ServerDecoder()
+        self.framer = ModbusRtuFramer
+
+    async def serve(self, opened=None):
+        from anyio_serial import Serial
+
+        self._serial = ser = serial.open(**args)
+        while True:
+            data = await ser.read()
+            msgs = []
+            self.framer.processIncomingPacket(
+                data=data,
+                callback=lambda x: append(msgs, x),
+                unit=self.units,
+                single=False,
+            )
+            for msg in msgs:
+                await self._process(msg)
+
+    async def _process(self, msg):
+        broadcast = False
+
+        try:
+            if self.broadcast_enable and not request.unit_id:
+                broadcast = True
+                # if broadcasting then execute on all slave contexts,
+                # note response will be ignored
+                for unit_id in self.context.slaves():
+                    response = request.execute(self.context[unit_id])
+            else:
+                context = self.context[request.unit_id]
+                response = request.execute(context)
+        except NoSuchSlaveException:
+            txt = f"requested slave does not exist: {request.unit_id}"
+            _logger.error(txt)
+            if self.ignore_missing_slaves:
+                return  # the client will simply timeout waiting for a response
+            response = request.doException(merror.GatewayNoResponse)
+        except Exception as exc:  # pylint: disable=broad-except
+            txt = (
+                f"Datastore unable to fulfill request: {exc}; {traceback.format_exc()}"
+            )
+            _logger.error(txt)
+            response = request.doException(merror.SlaveFailure)
+        # no response when broadcasting
+        if message.should_respond and not broadcast:
+            response.transaction_id = request.transaction_id
+            response.unit_id = request.unit_id
+            skip_encoding = False
+            if self.response_manipulator:
+                response, skip_encoding = self.response_manipulator(response)
+            self.send(response, *addr, skip_encoding=skip_encoding)
+
+
+class ModbusServer(BaseModbusServer):
+    def __init__(self, identity=None, address=None, port=None):
+        """Initializer for the Modbus server.
+
+        If the identify structure is not passed in, the ModbusControlBlock
+        uses its own empty structure.
+
+        :param identity: An optional identify structure
+        :param address: An optional address to bind to.
+        :param port: the TCP port to listen on.
+        """
+        super().__init__(identity=identity)
+
+        from pymodbus.framer.socket_framer import ModbusSocketFramer
+
+        self.decoder = ServerDecoder()
+        self.framer = ModbusSocketFramer
+        self.address = address or "localhost"
+        self.port = port if port is not None else Defaults.Port
+
+    async def serve(self, opened=None):
+        """Run this server.
+        Sets the `opened` event, if given, as soon as the server port is open.
+        """
+        try:
+            async with anyio.create_task_group() as tg:
+                self.taskgroup = tg
+                async with await anyio.create_tcp_listener(
+                    local_port=self.port, local_host=self.address,
+                    reuse_port=True,
+                ) as server:
+                    if self.port == 0:
+                        self.port = server.port
+                    if opened is not None:
+                        await opened.set()
+
+                    await server.serve(self._serve_one)
+        except socket.gaierror as exc:
+            _logger.error(f"Trying to look up {self.address}")
+        finally:
+            self.taskgroup = None
+
+    async def _serve_one(self, conn):
+        reset_frame = False
+        framer = self.framer(decoder=self.decoder)
+
+        while True:
+            try:
+                data = await conn.receive(4096)
+                if data == b"":
+                    break
+                _logger.debug("Handling data: " + hexlify_packets(data))
+
+                reqs = []
+                # TODO fix pymodbus
+                framer.processIncomingPacket(data, lambda req: reqs.append(req), list(self.units.keys()), single=False)
+
+                for request in reqs:
+                    try:
+                        response = await self.process_request(request)
+                    except NoSuchSlaveException as ex:
+                        _logger.debug(
+                            f"requested slave does not exist: {request.unit_id}"
+                        )
+                        response = request.doException(merror.GatewayNoResponse)
+                    except Exception as ex:
+                        _logger.debug(
+                            f"Datastore unable to fulfill request: {ex}; {traceback.format_exc()}"
+                        )
+                        response = request.doException(merror.SlaveFailure)
+                    if response.should_respond:
+                        response.transaction_id = request.transaction_id
+                        response.unit_id = request.unit_id
+                        # self.server.control.Counter.BusMessage += 1
+                        pdu = framer.buildPacket(response)
+                        _logger.debug(f"send: {b2a_hex(pdu)}")
+                        await conn.send(pdu)
+
+            except socket.timeout as msg:
+                _logger.debug(f"Socket timeout occurred {msg}")
+                reset_frame = True
+            except socket.error as msg:
+                _logger.error(f"Socket error occurred {msg}")
+                return
+            except anyio.get_cancelled_exc_class():
+                raise
+            except anyio.BrokenResourceError:
+                return
+            except Exception as exc:
+                _logger.error(f"Socket exception occurred {traceback.format_exc()}")
+                return
+            finally:
+                if reset_frame:
+                    framer.resetFrame()
+                    reset_frame = False
+
+
+class MockAioModbusServer(ModbusServer):
+    """A test modbus server with static data"""
+    pass
+
+
+class ForwardingAioModbusServer:
+    """A modbus server mix-in that forwards requests to a modbus client"""
+
+    def add_unit(self, unit):
+        raise RuntimeError("Use 'set_forward' with a forwarding server")
+
+    def set_forward(self, id, unit:ModbusSlaveContext = None):
+        """Arrange for requests to the given ID to be forwarded
+        to this client.
+
+        Use unit=None to stop forwarding.
+        """
+        if unit is None:
+            del self.units[id]
+        else:
+            self.units[id] = unit
+
+    async def process_request(self, request):
+        """Process this request by forwarding it to the client."""
+        old_unit = request.unit_id
+        old_tid = request.transaction_id
+
+        unit = self.units[request.unit_id]
+        request.unit_id = unit.unit
+        try:
+            async with unit.concurrent_requests:
+                return await unit.host.execute(request)
+        finally:
+            request.unit_id = old_unit
+            request.transaction_id = old_tid
---
+++ packages/moat-modbus/moat-modbus-0.3.2.tar.gz/moat-modbus-0.3.2/moat/modbus/types.py
@@ -0,0 +1,481 @@
+import struct
+from typing import List
+import anyio
+
+try:
+    from pymodbus.client.common import (
+        ReadDiscreteInputsRequest,
+        ReadDiscreteInputsResponse,
+        ReadCoilsRequest,
+        ReadCoilsResponse,
+        ReadHoldingRegistersRequest,
+        ReadHoldingRegistersResponse,
+        ReadInputRegistersRequest,
+        ReadInputRegistersResponse,
+        WriteSingleCoilRequest,
+        WriteSingleCoilResponse,
+        WriteMultipleCoilsRequest,
+        WriteMultipleCoilsResponse,
+        WriteSingleRegisterRequest,
+        WriteSingleRegisterResponse,
+        WriteMultipleRegistersRequest,
+        WriteMultipleRegistersResponse,
+    )
+except ImportError:
+    from pymodbus.factory import (
+        ReadDiscreteInputsRequest,
+        ReadDiscreteInputsResponse,
+        ReadCoilsRequest,
+        ReadCoilsResponse,
+        ReadHoldingRegistersRequest,
+        ReadHoldingRegistersResponse,
+        ReadInputRegistersRequest,
+        ReadInputRegistersResponse,
+        WriteSingleCoilRequest,
+        WriteSingleCoilResponse,
+        WriteMultipleCoilsRequest,
+        WriteMultipleCoilsResponse,
+        WriteSingleRegisterRequest,
+        WriteSingleRegisterResponse,
+        WriteMultipleRegistersRequest,
+        WriteMultipleRegistersResponse,
+    )
+from pymodbus.datastore.store import BaseModbusDataBlock
+
+
+def _singleton(x):
+    x = x()
+    return x
+
+
+class BaseValue:
+    """Base class for a single value.
+
+    Do not instantiate directly.
+
+    Set @idem if setting the item shall trigger iterators even if it
+    doesn't change.
+    """
+
+    len = 0
+    value = None
+    gen = 0
+
+    def __init__(self, value=None, idem=False):
+        self.changed = anyio.Event()
+        self.value = value
+        self.idem = idem
+
+        if value is not None:
+            self.gen = 1
+            self.changed = anyio.Event()
+
+    def _decode(self, regs):
+        raise NotImplementedError
+
+    def _encode(self, value):
+        raise NotImplementedError
+
+    def decode(self, regs):
+        val = self._decode(regs)
+        if not self.idem and val == self.value:
+            return
+        self.value = self._decode(regs)
+        self.changed.set()
+        self.changed = anyio.Event()
+        self.gen += 1
+
+    def clear(self):
+        self.value = None
+        self.changed.set()
+        self.changed = anyio.Event()
+
+    def encode(self):
+        return self._encode(self.value)
+
+    def __str__(self):
+        return f'‹{self.value}›'
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__}:{self.value}>'
+
+    def __aiter__(self):
+        return ValueIterator(self)
+
+
+class _Signed:
+    """A mix-in for signed integers."""
+    def _decode(self, regs):
+        res = super()._decode(regs)
+        if res & 1<<(self.len*16-1):
+            res -= 1<<(self.len*16)
+        return res
+
+    def _encode(self, value):
+        if value < 0:
+            value += 1<<(self.len*16)
+        return super()._encode(value)
+
+class _Swapped:
+    """A mix-in to byteswap the Modbus data."""
+    def _decode(self, regs):
+        regs.reverse()
+        return super()._decode(regs)
+
+    def _encode(self, value):
+        regs = super()._encode(value)
+        regs.reverse()
+        return regs
+
+
+class InaccessibleValue(BaseValue):  # duck-types but does NOT interit BaseValue
+    """This register range must not be accessed.
+
+    Use an instance of this type (with appropriate length)
+    to force splitting a request into multiple parts.
+
+    :param len: The length of the block that may not be accessed.
+    """
+
+    def __init__(self, len):
+        self.len = len
+
+
+class IntValue(BaseValue):
+    """Simplest-possible value, one register.
+    """
+    len = 1
+
+    def _decode(self, regs):
+        return regs[0]
+
+    def _encode(self, value):
+        return (value,)
+
+
+class LongValue(BaseValue):
+    """32-bit integer, two registers, standard (big-endian) word order.
+    """
+    len = 2
+
+    def _decode(self, regs):
+        return regs[0] * 65536 + regs[1]
+
+    def _encode(self, value):
+        return (value & 0xFFFF, value >> 16)
+
+class QuadValue(BaseValue):
+    """64-bit integer, four registers, standard (big-endian) word order.
+
+    This is a BaseValue instance.
+    """
+    len = 4
+
+    def _decode(self, regs):
+        return (((((regs[0] << 16) + regs[1]) << 16) + regs[2]) << 16) + regs[3]
+
+    def _encode(self, value):
+        return (
+            value & 0xFFFF,
+            (value >> 16) & 0xFFFF,
+            (value >> 32) & 0xFFFF,
+            (value >> 48) & 0xFFFF,
+        )
+
+
+class FloatValue(BaseValue):
+    """network-ordered floating point.
+    """
+    len = 2
+
+    def _decode(self, regs):
+        return struct.unpack(">f", struct.pack(">2H", *regs))[0]
+
+    def _encode(self, value):
+        return struct.unpack(">2H", struct.pack(">f", value))
+
+class DoubleValue(BaseValue):
+    """network-ordered accurate floating point.
+    """
+    len = 4
+
+    def _decode(self, regs):
+        return struct.unpack(">d", struct.pack(">4H", *regs))[0]
+
+    def _encode(self, value):
+        return struct.unpack(">4H", struct.pack(">d", value))
+
+
+class SwappedLongValue(_Swapped, LongValue):
+    """32-bit integer, two registers, little-endian word order.
+    """
+    pass
+
+class SwappedQuadValue(_Swapped, QuadValue):
+    """64-bit integer, four registers, little-endian word order.
+    """
+    pass
+
+
+class SignedIntValue(_Signed, IntValue):
+    """one register, signed.
+    """
+    pass
+
+class SignedLongValue(_Signed, LongValue):
+    """two registers, signed.
+    """
+    pass
+
+class SwappedSignedLongValue(_Signed, _Swapped, BaseValue):
+    """two registers, signed, swapped.
+    """
+    pass
+
+SignedSwappedLongValue = SwappedSignedLongValue
+
+
+class SignedQuadValue(_Signed, QuadValue):
+    """four registers, signed.
+    """
+    pass
+
+
+class SwappedSignedQuadValue(_Signed, _Swapped, QuadValue):
+    """four registers, signed, swapped.
+    """
+    pass
+
+SignedSwappedQuadValue = SwappedSignedQuadValue
+
+
+class SwappedFloatValue(_Swapped, FloatValue):
+    """broken-ordered floating point.
+    """
+    pass
+
+
+class SwappedDoubleValue(_Swapped, DoubleValue):
+    """broken-ordered accurate floating point.
+    """
+    pass
+
+
+class TypeCodec:
+    """Base class for access types. Do not instantiate."""
+
+    typ = None
+    acc = None
+
+    def __repr__(self):
+        return self.__class__.__name__
+
+    def __eq__(self, typ):
+        if isinstance(typ, TypeCodec):
+            typ = typ.typ
+        return self.typ == typ
+
+    def __hash__(self):
+        return self.typ
+
+
+@_singleton
+class Coils(TypeCodec):
+    """Modbus 'coils' data.
+    This is a TypeCodec.
+    """
+
+    typ = 0
+    key = 'c'
+    encoder = ReadCoilsRequest
+    decoder = ReadCoilsResponse
+    encoder_s = WriteSingleCoilRequest
+    decoder_s = WriteSingleCoilResponse
+    encoder_m = WriteMultipleCoilsRequest
+    decoder_m = WriteMultipleCoilsResponse
+
+
+@_singleton
+class DiscreteInputs(TypeCodec):
+    """Modbus 'discrete input' data.
+    This is a TypeCodec.
+    """
+
+    typ = 1
+    key = 'd'
+    encoder = ReadDiscreteInputsRequest
+    decoder = ReadDiscreteInputsResponse
+
+
+@_singleton
+class HoldingRegisters(TypeCodec):
+    """Modbus 'holding register' data.
+    This is a TypeCodec.
+    """
+
+    typ = 2
+    key = 'h'
+    encoder = ReadHoldingRegistersRequest
+    decoder = ReadHoldingRegistersResponse
+    encoder_s = WriteSingleRegisterRequest
+    decoder_s = WriteSingleRegisterResponse
+    encoder_m = WriteMultipleRegistersRequest
+    decoder_m = WriteMultipleRegistersResponse
+
+
+@_singleton
+class InputRegisters(TypeCodec):
+    """Modbus 'input register' data.
+    This is a TypeCodec.
+    """
+
+    typ = 3
+    key = 'i'
+    encoder = ReadInputRegistersRequest
+    decoder = ReadInputRegistersResponse
+
+
+class DataBlock(dict, BaseModbusDataBlock):
+    """Your basic sparse data block.
+
+    The @changed attribute is an event that triggers when a write request
+    succeeds.
+    """
+
+    def __init__(self, max_len=30):
+        super().__init__()
+        self.max_len = max_len
+        self.changed = anyio.Event()
+
+    def default(self, count, value=False):
+        raise NotImplementedError
+
+    def reset(self):
+        for val in self.values():
+            val.value = None
+
+    def add(self, offset:int, val: BaseValue): 
+        if offset in self:
+            raise ValueError("Already known", i)
+        for n in range(1,8):
+            try:
+                if self[offset-n].len > n:
+                    raise ValueError(f"Overlap with {self[offset-n]} @{offset-n}")
+                break
+            except KeyError:
+                pass
+        for n in range(1,val.len):
+            try:
+                if offset+n in self:
+                    raise ValueError(f"Overlap with {self[offset+n]} @{offset+n}")
+                break
+            except KeyError:
+                pass
+        self[offset] = val
+
+    def validate(self, address:int, count:int = 1):
+        if not count:
+            return False
+        while count:
+            try:
+                val = self[address]
+                if val.len <= 0:
+                    raise RuntimeError("invalid")
+            except (KeyError,RuntimeError):
+                return False
+            address += val.len
+            count -= val.len
+        return True
+
+    def ranges(self):
+        """Iterate over to-be-retrieved range(s)."""
+        start, cur = None, None
+        for offset, val in sorted(self.items()):
+            if isinstance(val, InaccessibleValue):
+                if start is not None:
+                    yield (start, cur - start)
+                    start = None
+            elif start is None: 
+                start = offset  
+                cur = start + val.len
+            elif cur == offset and (cur + val.len - start) <= self.max_len:
+                cur += val.len
+            else:
+                yield (start, cur - start)
+                start = offset
+                cur = start + val.len
+                
+        if cur is not None:
+            yield (start, cur - start)
+
+    def getValues(self, address:int, count=1):
+        """Get the array of Modbus values for the @address:+@count range"""
+        res = []
+        while count > 0:
+            try:
+                val = self[address]
+            except KeyError:
+                res.append(0)
+                address += 1
+                count -= 1
+            else:
+                res.extend(val.encode())
+                address += val.len
+                count -= val.len
+        if count < 0:
+            # well, this shouldn't happen but …
+            res = res[:count]
+        return res
+
+    def setValues(self, address:int, values:List[int]):
+        """Set the variables starting at @address to @values"""
+        while values:
+            try:
+                val = self[address]
+            except KeyError:
+                address += 1
+                values.pop(0)
+            else:
+                val.decode(values[:val.len])
+                address += val.len
+                values = values[val.len:]
+
+        self.changed.set()
+        self.changed = anyio.Event()
+
+    def delete(self, address, count=1):
+        while count:
+            self.pop(address, None)
+            address += 1
+            count -= 1
+
+
+class ValueIterator:
+    """
+    Helper class for iterating over `BaseValue` changes.
+
+    Posts a notification when values get skipped.
+    """
+    def __init__(self, val):
+        self.val = val
+        self.gen = max(0, val.gen-1)
+
+    async def __anext__(self):
+        """
+        Iterate over values / value changes.
+        
+        If the value is initially unknown, wait.
+        if it's been cleared, raises `StopAsyncIteration`.
+        """
+        if val.gen > 0 and val.value is None:
+            raise StopAsyncIteration
+        if self.gen == val.gen:
+            await self.changed.wait()
+        if self.value is None:
+            raise StopAsyncIteration
+        if self.gen+1 != val.gen:
+            logger.notice("%r: skipped %d",val.gen-self.gen-1)
+        self.gen = val.gen
+        return self.value
+
+
