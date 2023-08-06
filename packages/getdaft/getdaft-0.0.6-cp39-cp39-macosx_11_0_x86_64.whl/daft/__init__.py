365efa4298d881645fd7b3f19f22b82a5e3701f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:39 +0000

    Add package getdaft/0.0.6/getdaft-0.0.6-cp39-cp39-macosx_11_0_x86_64.whl

---
+++ packages/getdaft/getdaft-0.0.6-cp39-cp39-macosx_11_0_x86_64.whl/daft/__init__.py
@@ -0,0 +1,11 @@
+from daft.dataclasses import dataclass
+from daft.dataframe import DataFrame
+from daft.expressions import col
+from daft.logging import setup_logger
+from daft.udf import udf
+
+__all__ = ["DataFrame", "col", "udf"]
+
+setup_logger()
+
+__version__ = "0.0.6"
