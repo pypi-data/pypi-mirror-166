c891971822704b0480375a01ac09fb2533605b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:35 +0000

    Add package cfn-guard-rs-hook/0.1.1/cfn_guard_rs_hook-0.1.1.tar.gz

---
+++ packages/cfn-guard-rs-hook/cfn_guard_rs_hook-0.1.1.tar.gz/cfn_guard_rs_hook-0.1.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cfn_guard_rs_hook']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Jinja2>=3.0.0,<4.0.0',
+ 'cfn-guard-rs>=0.1.0,<0.2.0',
+ 'cloudformation-cli-python-lib>=2.1.12,<3.0.0']
+
+setup_kwargs = {
+    'name': 'cfn-guard-rs-hook',
+    'version': '0.1.1',
+    'description': 'Works with cloudformation-cli-python-lib to remove duplicate code when creating a CloudFormation registry hook that leverages the library cfn_guard_rs',
+    'long_description': None,
+    'author': 'Kevin DeJong',
+    'author_email': 'kddejong@amazon.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.2,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
