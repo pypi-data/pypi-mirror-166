779b84e04748eb3c41b59109de55259796b0fac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:40 +0000

    Add package shulkr/0.7.0/shulkr-0.7.0.tar.gz

---
+++ packages/shulkr/shulkr-0.7.0.tar.gz/shulkr-0.7.0/setup.py
@@ -0,0 +1,53 @@
+from glob import glob
+import os.path
+from setuptools import find_packages, setup
+from typing import List
+
+
+def package_files(directory: str) -> List[str]:
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(script_dir, 'README.md'), 'r') as f:
+	README = f.read()
+
+classifiers = [
+	'Development Status :: 2 - Pre-Alpha',
+	'Environment :: Console',
+	'Intended Audience :: Developers',
+	'Intended Audience :: Science/Research',
+	'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+	'Operating System :: OS Independent',
+	'Programming Language :: Python',
+	'Programming Language :: Python :: 3',
+	'Topic :: Games/Entertainment',
+	'Topic :: Scientific/Engineering',
+	'Topic :: Software Development :: Version Control :: Git',
+	'Topic :: Utilities'
+]
+
+setup(
+	name='shulkr',
+	version='0.7.0',
+	description='Decompile multiple versions of Minecraft with a single command (for research)',
+	long_description=README,
+	long_description_content_type='text/markdown',
+	url='https://github.com/clabe45/shulkr',
+	author='Caleb Sacks',
+	license='GPLv3',
+	classifiers=classifiers,
+    keywords=['minecraft', 'git', 'decompile', 'game'],
+    packages=find_packages(exclude=['tests']),
+    py_modules=['colorama', 'java', 'minecraft', 'mint', 'shulkr'],
+	# include_package_data=True,
+	# package_data={'': package_files('shulkr/DecompilerMC')},
+	install_requires=['gitpython', 'javalang', 'unidiff', 'requests', 'toml', 'click'],
+	entry_points={
+        'console_scripts': ['shulkr=shulkr.__main__:main']
+    }
+)
---
+++ packages/shulkr/shulkr-0.7.0.tar.gz/shulkr-0.7.0/tests/mint/smoke/test_command.py
@@ -0,0 +1,22 @@
+from mint.command import GitCommand
+import pytest
+
+
+@pytest.fixture
+def git(repo):
+	return GitCommand(repo.path)
+
+
+class TestCommand:
+	def test_getattr_returns_git_output(self, git):
+		# Make one commit so git knows which branch is checked out
+		git.commit(message='dummy commit', allow_empty=True)
+
+		# Now, make sure the result of 'git status' is correct
+		branch = git.rev_parse('HEAD', abbrev_ref=True)
+		assert git.status() == f'On branch {branch}\nnothing to commit, working tree clean'
+
+	def test_commit_messages_with_spaces_are_not_wrapped_with_quotes(self, git):
+		git.commit(message='dummy commit', allow_empty=True)
+
+		assert git.log('--format=%B') == 'dummy commit'
