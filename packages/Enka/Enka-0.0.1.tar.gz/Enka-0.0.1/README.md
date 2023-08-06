3944e88a70b00a61e32dcf93b8b0a16865e32267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:25 +0000

    Add package Enka/0.0.1/Enka-0.0.1.tar.gz

---
+++ packages/Enka/Enka-0.0.1.tar.gz/Enka-0.0.1/README.md
@@ -0,0 +1,3 @@
+Enka - Easiest way to view your Genshin Characters.
+
+This package is currently empty. I'll make a release soon.
---
+++ packages/Enka/Enka-0.0.1.tar.gz/Enka-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="Enka",
+    version="0.0.1",
+    description="An asynchronous wrapper around enka.network's API.",
+    author="VarMonke",
+    author_email="var.monke@gmail.com"
+)
