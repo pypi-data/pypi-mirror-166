097b18adc05c4c11bb2b5da22703a94e71c15ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:52 +0000

    Add package batspp/2.1.4/batspp-2.1.4-py3-none-any.whl

---
+++ packages/batspp/batspp-2.1.4-py3-none-any.whl/batspp/_exceptions.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+#
+# Exceptions module
+#
+
+
+"""
+Exceptions module
+"""
+
+
+# Standard packages
+## NOTE: this is empty for now
+
+
+# Installed packages
+## NOTE: this is empty for now
+
+
+# Local packages
+## NOTE: this is empty for now
+
+
+def error(
+        message:str='',
+        text_line:str='',
+        line:int=None,
+        column:str=None,
+        ) -> None:
+    """Raise exception"""
+    ## TODO: limit text_line if it is very long
+
+    output = ''
+    output += f'{message}' if message else ''
+    output += f'\nline {line}: {text_line}' if text_line and line else ''
+    output += f'\n             {" " * column}^' if column else ''
+
+    raise Exception(output)
+
+
+def assert_type(var: any, expected_type: any) -> None:
+    """Assert variable type"""
+    assert isinstance(var, expected_type), f'expected type {expected_type} but founded {type(var)}'
