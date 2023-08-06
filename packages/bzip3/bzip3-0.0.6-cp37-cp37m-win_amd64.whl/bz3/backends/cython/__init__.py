a20074dc9cf98fc43e9f3bdadec8ce8931f112d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:49 +0000

    Add package bzip3/0.0.6/bzip3-0.0.6-cp37-cp37m-win_amd64.whl

---
+++ packages/bzip3/bzip3-0.0.6-cp37-cp37m-win_amd64.whl/bz3/backends/cython/__init__.py
@@ -0,0 +1,10 @@
+"""
+Copyright (c) 2008-2021 synodriver <synodriver@gmail.com>
+"""
+from bz3.backends.cython._bz3_cy import (
+    BZ3Compressor,
+    BZ3Decompressor,
+    compress_file,
+    decompress_file,
+    test_file,
+)
