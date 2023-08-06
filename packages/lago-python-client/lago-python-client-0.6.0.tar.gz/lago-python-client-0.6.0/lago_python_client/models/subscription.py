762f8a051faf5636739a6d529dd25f64c29e3856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:49 +0000

    Add package lago-python-client/0.6.0/lago-python-client-0.6.0.tar.gz

---
+++ packages/lago-python-client/lago-python-client-0.6.0.tar.gz/lago-python-client-0.6.0/lago_python_client/models/subscription.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+class Subscription(BaseModel):
+    plan_code: Optional[str]
+    external_customer_id: Optional[str]
+    name: Optional[str]
+    external_id: Optional[str]
+    billing_time: Optional[str]
+
+
+class SubscriptionResponse(BaseModel):
+    lago_id: str
+    lago_customer_id: Optional[str]
+    external_customer_id: Optional[str]
+    canceled_at: Optional[str]
+    created_at: Optional[str]
+    plan_code: Optional[str]
+    started_at: Optional[str]
+    status: Optional[str]
+    name: Optional[str]
+    external_id: Optional[str]
+    billing_time: Optional[str]
+    terminated_at: Optional[str]
+    subscription_date: Optional[str]
