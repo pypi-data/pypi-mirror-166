36609150e9011fb097a0fe23ea1576a6610c3b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:10 +0000

    Add package deephaven-ipywidgets/0.1.0/deephaven_ipywidgets-0.1.0.tar.gz

---
+++ packages/deephaven-ipywidgets/deephaven_ipywidgets-0.1.0.tar.gz/deephaven_ipywidgets-0.1.0/deephaven_ipywidgets/labextension/static/lib_index_js.76f317d1c623ac10ea09.js
@@ -0,0 +1,31 @@
+"use strict";
+(self["webpackChunk_deephaven_ipywidgets"] = self["webpackChunk_deephaven_ipywidgets"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+// Copyright (c) Deephaven Data Labs LLC
+// Distributed under the terms of the Modified BSD License.
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+__exportStar(__webpack_require__(/*! ./version */ "./lib/version.js"), exports);
+__exportStar(__webpack_require__(/*! ./widget */ "./lib/widget.js"), exports);
+//# sourceMappingURL=index.js.map
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.76f317d1c623ac10ea09.js.map
