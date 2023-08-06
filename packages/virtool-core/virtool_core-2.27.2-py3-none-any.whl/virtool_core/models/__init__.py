14296304dc1dcba0169dca6eb7396800b60cb4b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:15 +0000

    Add package virtool-core/2.27.2/virtool_core-2.27.2-py3-none-any.whl

---
+++ packages/virtool-core/virtool_core-2.27.2-py3-none-any.whl/virtool_core/models/__init__.py
@@ -0,0 +1,11 @@
+from virtool_core.models import user
+from virtool_core.models import group
+from virtool_core.models import samples
+from virtool_core.models import subtraction
+
+
+group.Group.update_forward_refs(UserMinimal=user.UserMinimal)
+samples.Sample.update_forward_refs(
+    SubtractionNested=subtraction.SubtractionNested
+)
+subtraction.Subtraction.update_forward_refs(SampleNested=samples.SampleNested)
