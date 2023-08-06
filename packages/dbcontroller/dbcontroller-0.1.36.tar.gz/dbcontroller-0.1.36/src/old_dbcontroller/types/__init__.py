e7fd0972db89e0aa3bd3bceedeed38abcdf166e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:38 +0000

    Add package dbcontroller/0.1.36/dbcontroller-0.1.36.tar.gz

---
+++ packages/dbcontroller/dbcontroller-0.1.36.tar.gz/dbcontroller-0.1.36/src/old_dbcontroller/types/__init__.py
@@ -0,0 +1,8 @@
+"""
+    Fastberry Types
+"""
+from .controller import create_controllers
+from .dbmanager import Database
+from .forms import Form
+from .model import Model
+from .singleton import Admin
