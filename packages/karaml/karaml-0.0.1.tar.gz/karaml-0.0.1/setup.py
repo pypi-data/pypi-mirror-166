89c271d56d118401de02358c773202ea84464d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:29 +0000

    Add package karaml/0.0.1/karaml-0.0.1.tar.gz

---
+++ packages/karaml/karaml-0.0.1.tar.gz/karaml-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='karaml',
+    version='0.0.1',
+    description='KARaML Tools wrapper package',
+    long_description='',
+    url='https://www.example.com',
+    author='brandonb',
+    author_email='B.Bennett@leeds.ac.uk',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
