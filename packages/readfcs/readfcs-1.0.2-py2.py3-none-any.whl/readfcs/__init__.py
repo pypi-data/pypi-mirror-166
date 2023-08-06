0770a4f74e0e5b0dcd7df4f3170a1e888350c48d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:18:59 +0000

    Add package readfcs/1.0.2/readfcs-1.0.2-py2.py3-none-any.whl

---
+++ packages/readfcs/readfcs-1.0.2-py2.py3-none-any.whl/readfcs/__init__.py
@@ -0,0 +1,20 @@
+"""Parse fcs files into AnnData.
+
+Import the package::
+
+   import readfcs
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   read
+   ReadFCS
+   datasets
+"""
+
+__version__ = "1.0.2"
+
+from . import datasets
+from ._core import ReadFCS, read
