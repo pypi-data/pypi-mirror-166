47f0398b7fe82e739c92412067b20da6c209b677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:40 +0000

    Add package objectiv-modelhub/0.0.22/objectiv-modelhub-0.0.22.tar.gz

---
+++ packages/objectiv-modelhub/objectiv-modelhub-0.0.22.tar.gz/objectiv-modelhub-0.0.22/modelhub/pipelines/__init__.py
@@ -0,0 +1,6 @@
+"""
+Copyright 2021 Objectiv B.V.
+"""
+from modelhub.pipelines.extracted_contexts import ExtractedContextsPipeline
+from modelhub.pipelines.sessionized_data import SessionizedDataPipeline
+from modelhub.pipelines.identity_resolution import IdentityResolutionPipeline
