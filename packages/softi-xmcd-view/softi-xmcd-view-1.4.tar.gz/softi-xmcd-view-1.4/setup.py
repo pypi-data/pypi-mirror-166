d0dd4d1658f354bbdaee26cbf72a6ed272a24281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:14 +0000

    Add package softi-xmcd-view/1.4/softi-xmcd-view-1.4.tar.gz

---
+++ packages/softi-xmcd-view/softi-xmcd-view-1.4.tar.gz/softi-xmcd-view-1.4/setup.py
@@ -0,0 +1,21 @@
+from importlib.metadata import entry_points
+from setuptools import setup, find_packages
+
+setup(
+    name="softi-xmcd-view",
+    version=1.4,
+    packages=find_packages(include=['xmcd_gui_logic_2', 'xmcd_gui', 'resourc', 'command_line', 'main_1']),
+    
+    install_requires = [
+        'PyQt5',
+        'scikit-image',
+        'matplotlib',
+        'pandas',
+        'numpy',
+        'scipy'
+    ],
+    entry_points={
+        'console_scripts': ['softi-xmcd-view = command_line:main'],
+    },
+)
+
