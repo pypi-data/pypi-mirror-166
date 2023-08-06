cfdcc722644c12340ef10671e72153b1821523c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:13 +0000

    Add package whocan/0.3.0/whocan-0.3.0-py3-none-any.whl

---
+++ packages/whocan/whocan-0.3.0-py3-none-any.whl/whocan/__init__.py
@@ -0,0 +1,22 @@
+from whocan._errors import BaseError
+from whocan._errors import PolicyEvaluationError
+from whocan._errors import PolicyYamlInvalidError
+from whocan._policies import Policy
+from whocan._policies import Statement
+from whocan._policy_sets import PolicySet
+from whocan._policy_sets import Operation
+
+UNION = Operation.UNION
+INTERSECT = Operation.INTERSECT
+
+
+__all__ = (
+    'BaseError',
+    'Policy',
+    'PolicyEvaluationError',
+    'PolicySet',
+    'PolicyYamlInvalidError',
+    'Statement',
+    'UNION',
+    'INTERSECT',
+)
