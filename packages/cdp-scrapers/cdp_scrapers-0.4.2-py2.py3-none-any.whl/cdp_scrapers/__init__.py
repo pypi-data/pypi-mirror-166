3ba23d971ac868deb792d08e60192fbfee118445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:59 +0000

    Add package cdp-scrapers/0.4.2/cdp_scrapers-0.4.2-py2.py3-none-any.whl

---
+++ packages/cdp-scrapers/cdp_scrapers-0.4.2-py2.py3-none-any.whl/cdp_scrapers/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for cdp-scrapers."""
+
+__author__ = "Jackson Maxfield Brown"
+__email__ = "jmaxfieldbrown@gmail.com"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.4.2"
+
+
+def get_module_version():
+    return __version__
