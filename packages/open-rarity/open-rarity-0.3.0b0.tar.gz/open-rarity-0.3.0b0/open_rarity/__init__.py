b7d29704bd4ac58a653397c5e27695155635fa6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:03 +0000

    Add package open-rarity/0.3.0b0/open-rarity-0.3.0b0.tar.gz

---
+++ packages/open-rarity/open-rarity-0.3.0b0.tar.gz/open-rarity-0.3.0b0/open_rarity/__init__.py
@@ -0,0 +1,10 @@
+from .models import (
+    Collection,
+    EVMContractTokenIdentifier,
+    StringAttribute,
+    Token,
+    TokenMetadata,
+    TokenStandard,
+)
+from .scoring import Scorer as OpenRarityScorer
+from .rarity_ranker import RarityRanker
