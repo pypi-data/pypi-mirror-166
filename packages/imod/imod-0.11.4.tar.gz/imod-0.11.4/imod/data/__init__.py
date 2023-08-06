e084bcd433ba075685af67476bd315fde64c91b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:07 +0000

    Add package imod/0.11.4/imod-0.11.4.tar.gz

---
+++ packages/imod/imod-0.11.4.tar.gz/imod-0.11.4/imod/data/__init__.py
@@ -0,0 +1,13 @@
+# from .synthetic import
+from .sample_data import (
+    ahn,
+    fluxes,
+    head_observations,
+    hondsrug_drainage,
+    hondsrug_initial,
+    hondsrug_layermodel,
+    hondsrug_meteorology,
+    hondsrug_river,
+    lakes_shp,
+    twri_output,
+)
