f2793bd46b9dd800d5a8ce68fade690e9eaf3695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:57 +0000

    Add package opal/0.22.2/opal-0.22.2.tar.gz

---
+++ packages/opal/opal-0.22.2.tar.gz/opal-0.22.2/opal/core/search/static/js/search/services/patient_summary.js
@@ -0,0 +1,33 @@
+//
+// This is the main PatientSummary class for OPAL.
+//
+angular.module('opal.services').factory('PatientSummary', function() {
+        var PatientSummary = function(jsonResponse){
+            var startYear, endYear;
+
+            if(jsonResponse.start_date){
+                startYear= moment(jsonResponse.start_date, 'DD/MM/YYYY').format("YYYY");
+            }
+
+            if(jsonResponse.end_date){
+                endYear = moment(jsonResponse.end_date, 'DD/MM/YYYY').format("YYYY");
+            }
+
+            if(startYear && endYear && startYear !== endYear){
+              this.years = startYear + "-" + endYear;
+            }
+            else if(startYear){
+                this.years = startYear;
+            }
+            this.first_name = jsonResponse.first_name;
+            this.surname = jsonResponse.surname;
+            this.patientId = jsonResponse.patient_id;
+            this.count = jsonResponse.count;
+            this.dateOfBirth = moment(jsonResponse.date_of_birth, 'DD/MM/YYYY');
+            this.categories = jsonResponse.categories.join(", ");
+            this.link = "/#/patient/" + jsonResponse.patient_id;
+            this.hospitalNumber = jsonResponse.hospital_number;
+        };
+
+        return PatientSummary;
+    });
