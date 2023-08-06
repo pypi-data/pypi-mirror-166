fcf928ced947a9feefe974c1aad1aa37f2719589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:49 +0000

    Add package abork-logger/1.0.0/abork_logger-1.0.0.tar.gz

---
+++ packages/abork-logger/abork_logger-1.0.0.tar.gz/abork_logger-1.0.0/py_logger/__init__.py
@@ -0,0 +1,1 @@
+from .module import MessageTypes, StdOutTarget, LogFileTarget, Logger
---
+++ packages/abork-logger/abork_logger-1.0.0.tar.gz/abork_logger-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='abork_logger',
+    version='1.0.0',
+    author='Alex Bork',
+    packages=['py_logger'],
+    description='Logging Library'
+    )
