30869cf831919bf2931d1ddcf9c20675554bd4a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:22 +0000

    Add package mage-ai/0.4.3/mage_ai-0.4.3-py3-none-any.whl

---
+++ packages/mage-ai/mage_ai-0.4.3-py3-none-any.whl/mage_ai/shared/logger.py
@@ -0,0 +1,111 @@
+from contextlib import contextmanager
+from typing import Callable
+
+import logging
+import time
+
+logger = logging.getLogger(__name__)
+
+
+class timer(object):
+    """
+    with timer('metric.metric', tags={ 'key': 'value' }):
+        function()
+    """
+
+    def __init__(self, metric, tags={}, verbose=True):
+        self.metric = metric
+        self.start = None
+        self.tags = tags
+        self.verbose = verbose
+
+    def __enter__(self):
+        self.start = time.time()
+
+    def __exit__(self, type, value, traceback):
+        # Must convert to milliseconds, see details in
+        # https://statsd.readthedocs.io/en/v3.1/timing.html
+        dt = int((time.time() - self.start) * 1000)
+        if self.verbose:
+            logger.debug(f'[time] metric: {self.metric}, value: {dt}ms, tags: {self.tags}')
+
+
+class VerboseFunctionExec:
+    def __init__(
+        self,
+        message: str,
+        log_func: Callable[[str], None] = None,
+        prefix: str = '',
+        verbose: bool = True,
+    ):
+        self.message = message
+        self.verbose = verbose
+        self.prefix = prefix
+        self.log_func = log_func
+
+    def __enter__(self):
+        if self.verbose:
+            enter_message = f'{self.prefix} {self.message}...'
+            if self.log_func is None:
+                print_kwargs=dict()
+                if self.prefix is None or len(self.prefix) == 0:
+                    print_kwargs['end'] = ''
+                print(enter_message, **print_kwargs)
+            else:
+                self.log_func(enter_message)
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        if self.verbose:
+            log_func = self.log_func or print
+            if exc_type is None:
+                log_func(f'{self.prefix} DONE')
+            else:
+                log_func(f'{self.prefix} FAILED')
+
+
+class VerbosePrintHandler:
+    def __init__(self, start_msg, verbose=False):
+        self.verbose = verbose
+        if verbose:
+            print(start_msg)
+        self.exists_previous_message = False
+
+    @contextmanager
+    def print_msg(self, msg):
+        if self.verbose:
+            if self.exists_previous_message:
+                print(f'\r├─ ')
+            print(f'└─ {msg}...', end='')
+        yield msg
+        if self.verbose:
+            print('DONE', end='')
+            self.exists_previous_message = True
+
+
+class BlockFunctionExec:
+    def __init__(
+        self,
+        block_uuid: str,
+        message: str,
+        log_func: Callable[..., None],
+    ):
+        self.message = message
+        self.log_func = log_func
+        self.block_uuid = block_uuid
+        self.prefix = f'[{block_uuid}]'
+
+    def __enter__(self):
+        if self.log_func is not None:
+            self.log_func(
+                f'{self.prefix} {self.message}',
+                execution_state='busy',
+                block_uuid=self.block_uuid,
+            )
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        if self.log_func is not None:
+            self.log_func(
+                f'{self.prefix} DONE',
+                execution_state='idle',
+                block_uuid=self.block_uuid,
+            )
