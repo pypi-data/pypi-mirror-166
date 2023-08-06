d4aab69c539e790df3887ff53a5f5a6d7bb235a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:31 +0000

    Add package labrador-sdk/0.0.2/labrador_sdk-0.0.2-py3-none-any.whl

---
+++ packages/labrador-sdk/labrador_sdk-0.0.2-py3-none-any.whl/tests/test_pwm.py
@@ -0,0 +1,9 @@
+from labrador_sdk.gpio import GPIO
+from labrador_sdk.pwm import PWM
+from labrador_sdk.main import Labrador
+
+
+def test_pwm():
+    labrador = Labrador("64", kernel_version=">=4.19.98")
+    labrador.gpio3.enable_pwm(alias="motor1", freq=1, duty_cycle=0.5)
+    print("Oi\n")
