2f2e837378410917db32d89340adec69052a2d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:03 +0000

    Add package elice-scenario-based-tester/1.0.11/elice_scenario_based_tester-1.0.11-py3-none-any.whl

---
+++ packages/elice-scenario-based-tester/elice_scenario_based_tester-1.0.11-py3-none-any.whl/esbt/constants.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+WAIT_FOR_SCENARIO_TIMEOUT = 10
+
+FINISHED_SUFFIX: str = 'is_finished'
+REPEAT_CNT_SUFFIX: str = 'repeat_cnt'
+
+RESPONSE_VALIDATE_FIELD: str = '_result..status'
+
+FORM_DATA_CONTENT_TYPE = 'multipart/form-data;'
+
+RFC_KEYS: dict[str, str] = {
+    'runner': 'NoeQFL7JwYDpVqbhc9pMY3W9jDtGJDVq',
+    'worker': 'U2gH6kLRasdfPOYEaJyAOtFBbCCTX3C2',
+    'payment': '33DNHe9dkf9vUdooApWnTh0YWUNM0NAk',
+}
