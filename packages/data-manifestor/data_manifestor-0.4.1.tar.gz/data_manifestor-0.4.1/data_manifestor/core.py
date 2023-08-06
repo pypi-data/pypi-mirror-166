a5758d28055eddce3ffd12d2da10491622e7f030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:30 +0000

    Add package data-manifestor/0.4.1/data_manifestor-0.4.1.tar.gz

---
+++ packages/data-manifestor/data_manifestor-0.4.1.tar.gz/data_manifestor-0.4.1/data_manifestor/core.py
@@ -0,0 +1,43 @@
+import os
+from glob import glob
+
+from .dataclasses import LocalComparisonResult, Manifest
+from .exceptions import ManifestError
+from .paths import generate_path_pattern, resolve_paths
+from .transformers import alt_dict_to_template, dict_to_template, is_alt_format
+
+
+def generate_manifest(template_dict: dict, *args: str) -> Manifest:
+    try:
+        if is_alt_format(template_dict):
+            template = alt_dict_to_template(template_dict)
+        else:
+            template = dict_to_template(template_dict)
+
+        path_patterns = [
+            generate_path_pattern(template.path_prefix, path_pattern, *args)
+            for path_pattern in template.path_patterns
+        ]
+    except Exception as e:
+        raise ManifestError("Error parsing manifest") from e
+
+    return Manifest(
+        name=template.name,
+        template=template,
+        args=args,
+        path_patterns=path_patterns,
+    )
+
+
+def compare_manifest_to_local(
+    template_dict: dict, local_dir: str, *args: str
+) -> LocalComparisonResult:
+    manifest = generate_manifest(template_dict, *args)
+    return LocalComparisonResult(
+        local_dir=local_dir,
+        manifest=manifest,
+        resolved_paths=[
+            resolve_paths(local_dir, path_pattern)
+            for path_pattern in manifest.path_patterns
+        ],
+    )
---
+++ packages/data-manifestor/data_manifestor-0.4.1.tar.gz/data_manifestor-0.4.1/data_manifestor/dataclasses.py
@@ -0,0 +1,28 @@
+from typing import List, Tuple
+
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Template:
+
+    name: str
+    path_prefix: str = "{}-{}-{}"
+    path_patterns: list[str] = field(default_factory=list)
+
+
+@dataclass
+class Manifest:
+
+    name: str
+    template: Template
+    args: tuple[str] = field(default_factory=tuple)
+    path_patterns: list[str] = field(default_factory=list)
+
+
+@dataclass
+class LocalComparisonResult:
+
+    manifest: Manifest
+    local_dir: str
+    resolved_paths: list[tuple[str, list[str]]] = field(default_factory=list)
