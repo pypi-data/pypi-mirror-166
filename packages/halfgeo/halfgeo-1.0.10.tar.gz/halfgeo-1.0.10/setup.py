7c78e1c279bca641dec33420231a800ffaffdd47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:13 +0000

    Add package halfgeo/1.0.10/halfgeo-1.0.10.tar.gz

---
+++ packages/halfgeo/halfgeo-1.0.10.tar.gz/halfgeo-1.0.10/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+  name='halfgeo',
+  version='1.0.10',
+  license='MIT',
+  author="Izz Hafeez",
+  author_email='izzhafeez@gmail.com',
+  packages=find_packages('src'),
+  package_dir={'': 'src'},
+  url='https://github.com/mynameizzhafeez/geo',
+  keywords='geospatial singapore',
+  long_description=open('README.md').read(),
+  long_description_content_type='text/markdown',
+  install_requires=[
+      'pandas',
+      'geopandas',
+      'numpy',
+      'shapely',
+      'gsheets',
+      'Pillow'
+    ],
+
+)
