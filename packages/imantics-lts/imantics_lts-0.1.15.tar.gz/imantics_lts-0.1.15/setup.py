61e41f0ee2dbd8a34eb675ca8d618782a0cdb65f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:39 +0000

    Add package imantics-lts/0.1.15/imantics_lts-0.1.15.tar.gz

---
+++ packages/imantics-lts/imantics_lts-0.1.15.tar.gz/imantics_lts-0.1.15/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='imantics_lts',
+    version='0.1.15',
+    description='Python package for managing image annotations',
+    url='https://github.com/zhaoqyu/Image-Semantics-Tools',
+    author='zhaoqyu',
+    author_email='zhaoqyu@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    install_requires=['numpy', 'opencv-python>=3', 'lxml', 'xmljson'],
+    packages=['imantics_lts'],
+    python_requires='>=3.5',
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Sound/Audio :: Capture/Recording',
+        'Topic :: Multimedia :: Graphics :: Capture',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
