e65cddf8da7aabf26fc219058f8034f0c4aa32f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:13 +0000

    Add package pypetl-canary/0.0.0b5/pypetl_canary-0.0.0b5-py3-none-any.whl

---
+++ packages/pypetl-canary/pypetl_canary-0.0.0b5-py3-none-any.whl/pypetl/core/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+from pypetl.core import aws
+from pypetl.core import file
+from pypetl.core import tunnel
+from pypetl.core import db
+from pypetl.core import log
