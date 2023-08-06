fd7d0ba74d63376b783c233c4e5c89a1d75ea551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:46 +0000

    Add package sirius-jz/0.0.22/sirius_jz-0.0.22.tar.gz

---
+++ packages/sirius-jz/sirius_jz-0.0.22.tar.gz/sirius_jz-0.0.22/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from pathlib import Path
+
+curd = Path(__file__).parent
+
+page_description = (curd / "README.md").read_text()
+setup(
+    name='sirius_jz',
+    version='0.0.22',
+    author='LUIZ GUILHERME C S PADUA',
+    author_email='jzguipadua@gmail.com',
+    packages=['sirius_jz'],
+    description='Sirius Studio for development',
+    long_description=page_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/',
+    project_urls={
+        'Código fonte': 'https://github.com/',
+        'Download': 'https://github.com/'
+    },
+    license='MIT',
+    keywords='JackZafira Sirius Studio si Bifrost',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Internationalization'
+    ]
+)
