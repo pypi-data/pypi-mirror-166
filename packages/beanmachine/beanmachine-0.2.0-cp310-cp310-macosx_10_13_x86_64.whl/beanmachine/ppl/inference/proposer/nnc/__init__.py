f3aebe6b3e18860660afd679429d4fe8874aa57c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:31 +0000

    Add package beanmachine/0.2.0/beanmachine-0.2.0-cp310-cp310-macosx_10_13_x86_64.whl

---
+++ packages/beanmachine/beanmachine-0.2.0-cp310-cp310-macosx_10_13_x86_64.whl/beanmachine/ppl/inference/proposer/nnc/__init__.py
@@ -0,0 +1,39 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import logging
+from typing import Callable, Optional, Tuple, TypeVar
+
+from typing_extensions import ParamSpec
+
+logger = logging.getLogger(__name__)
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+def nnc_jit(
+    f: Callable[P, R], static_argnums: Optional[Tuple[int]] = None
+) -> Callable[P, R]:
+    """
+    A helper function that lazily imports the NNC utils, which initialize the compiler
+    and displaying a experimental warning, then invoke the underlying nnc_jit on
+    the function f.
+    """
+    try:
+        # The setup code in `nnc.utils` will only be executed once in a Python session
+        from beanmachine.ppl.inference.proposer.nnc.utils import nnc_jit as raw_nnc_jit
+    except Exception as e:
+        logger.warning(
+            f"Fails to initialize NNC due to the following error: {str(e)}\n"
+            "Falling back to default inference engine."
+        )
+        # return original function without change
+        return f
+
+    return raw_nnc_jit(f, static_argnums)
+
+
+__all__ = ["nnc_jit"]
