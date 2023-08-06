3960d76b7097719e04dffedf1ea2fae696600e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:42 +0000

    Add package laygo2/0.1.4/laygo2-0.1.4.tar.gz

---
+++ packages/laygo2/laygo2-0.1.4.tar.gz/laygo2-0.1.4/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='laygo2',
+    version='0.1.4',
+    author='Jaeduk Han',
+    description='LAYout with Gridded Object 2 - A Python Package for Scripted IC Layout Generation Flow',
+    long_description=readme,
+    url='https://github.com/niftylab/laygo2', # project address
+    license=license,
+    packages=find_packages(exclude=('test', 'docs')),
+    python_requires='>=3.0',
+)
