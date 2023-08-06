9edcdb6a35305ea4995e788d3d73ffe2083eac8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:57 +0000

    Add package hspylib/0.11.96/hspylib-0.11.96-py3-none-any.whl

---
+++ packages/hspylib/hspylib-0.11.96-py3-none-any.whl/hspylib/addons/widman/widgets/__init__.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v0.11.96
+#
+# Package: main.hspylib.addons.widman.widgets
+"""Package initialization."""
+
+__all__ = [
+  'widget_free', 
+  'widget_send_msg', 
+  'widget_time_calc'
+]
+__version__ = '0.11.96'
---
+++ packages/hspylib/hspylib-0.11.96-py3-none-any.whl/hspylib/core/tools/__init__.py
@@ -0,0 +1,17 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v0.11.96
+#
+# Package: main.hspylib.core.tools
+"""Package initialization."""
+
+__all__ = [
+  'collection_filter', 
+  'commons', 
+  'constants', 
+  'git_tools', 
+  'preconditions', 
+  'text_tools', 
+  'validator'
+]
+__version__ = '0.11.96'
