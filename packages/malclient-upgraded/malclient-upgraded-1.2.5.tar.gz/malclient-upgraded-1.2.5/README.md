2cd89956ad7b2aa8ee3ad4242629562899cc7689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:22 +0000

    Add package malclient-upgraded/1.2.5/malclient-upgraded-1.2.5.tar.gz

---
+++ packages/malclient-upgraded/malclient-upgraded-1.2.5.tar.gz/malclient-upgraded-1.2.5/README.md
@@ -0,0 +1,84 @@
+# MAL-API-Client-Upgraded
+A third party object-oriented python3 client library for MyAnimeList's official REST API.
+Originally created by [@JFryy](https://github.com/JFryy/MAL-API-Client), dropped around 2 years ago, picked up and rewritten by ModerNews to fit more modern standards and new REST API functions.
+
+## Documentation
+There is first version of docs available, [check it out](https://mal-api-client-upgraded.readthedocs.io)
+
+## Installation
+**Python 3.9 or newer required**, this is due to changes in type hinting guidelines, for more info regarding this issue read [PEP 585](https://peps.python.org/pep-0585/)  
+Install the latest stable version from [PyPI](https://pypi.org/project/malclient-upgraded/)  
+`pip install malclient-upgraded`  
+
+Or current unstable version directly from GitHub:  
+`pip install git+https://github.com/ModerNews/MAL-API-Client-Upgraded`
+
+
+## Authentication
+Client library uses OAuth2 authorization, all you need to do is register your app [here](https://myanimelist.net/apiconfig), and generate access token.  
+You can do it old-fashioned way using [this tutorial](https://myanimelist.net/blog.php?eid=835707)  
+Or you can use function implemented in API
+```python
+import malclient
+
+print(malclient.generate_token("<YOUR_CLIENT_ID>", "<YOUR_CLIENT_SECRET>"))
+```
+Although remember to call it only once and, then use the token generated this way, optionally with `Client.refresh_bearer_token` method  
+Once an access token is retrieved, you can simply authenticate with this api with the following:
+```python
+import malclient
+
+client = malclient.Client(access_token="<YOUR_ACCESS_TOKEN>")
+
+```
+
+As mentioned previously, if your scared that your token will time out you can also utilize `Client.refresh_bearer_token` function
+```python
+client.refresh_bearer_token(
+          client_id="<your-client-id>",
+          client_secret="<your-client-secret>",
+          refresh_token="<your-refresh-token>")
+```
+
+For any other issues regarding authentication, [please refer to the following guide](https://myanimelist.net/blog.php?eid=835707).
+
+## Quick Start Examples
+I contained some examples of usage of this wrapper, note that all responses are converted to python objects using pydantic
+
+```python
+import malclient
+
+# nsfw filter is enabled by default, although it's recommended to disable it if your results are missing titles, 
+#  you can also enable/disable it for every query individually 
+client = malclient.Client(access_token="<your-access-token>", nsfw=True)
+
+# search anime, returns list
+anime = client.search_anime("cowboy", limit=20)
+for item in anime:
+    # prints only titles
+    print(item)
+    # prints all attributes of object
+    print(repr(item))
+    
+# search anime, returns list
+manga = client.search_manga("Monogatari", limit=20)
+for item in manga:
+    # prints only titles
+    print(manga)
+    # prints all attributes of object
+    print(repr(manga))
+
+# Get individual anime by ID
+anime = client.get_anime_details(1)
+print(anime)
+print(repr(anime))
+
+# Update anime List based off of search results
+anime = client.search_anime("Monogatari", limit=1)
+```
+### Most Imoprtant To-Do's
+- [ ] Rewrite boards (currently disabled)
+- [ ] Rewrite my list updaters
+
+## P.S. 
+If anything bugs you, you can always reach me out at discord Gruzin#0911 as well
---
+++ packages/malclient-upgraded/malclient-upgraded-1.2.5.tar.gz/malclient-upgraded-1.2.5/malclient/Datamodels/models.py
@@ -0,0 +1,300 @@
+from __future__ import annotations
+import datetime
+from typing import Union, Optional
+
+from pydantic import BaseModel, HttpUrl
+
+from .enums import *
+
+
+__all__ = ['Asset', 'Node', 'AnimeSeason', 'Genre', 'Studio', 'Broadcast', 'Statistics', 'Relation', 'Recommendation',
+           'MyMangaListStatus', 'MyAnimeListStatus', 'AnimeObject', 'MangaObject', 'UserAnimeStatistics', "User"]
+
+
+class MALBaseModel(BaseModel):
+    """
+    Helper model class used to generate all models used by this API
+    """
+    class Config:
+        arbitrary_types_allowed = True
+
+
+class Asset(MALBaseModel):
+    """
+
+    Asset object, commonly representing an image with two resolutions
+
+    """
+    large: Optional[HttpUrl]
+    medium: HttpUrl
+
+    def __str__(self):
+        return self.medium
+
+
+class Node(MALBaseModel):
+    """
+
+    Object representing simplified anime/manga info
+
+    """
+    id: int
+    title: str
+    main_picture: Optional[Asset]
+
+    def __str__(self):
+        return self.title
+
+    def __eq__(self, other):
+        return id == other.id
+
+
+class AnimeSeason(MALBaseModel):
+    """
+
+    Representation of anime season (f.e. winter 2022)
+
+    """
+    year: int
+    season: str
+
+    def __str__(self):
+        return f'{self.season} {self.year}'
+
+    def __eq__(self, other):
+        return self.year == other.year and self.season == other.season
+
+
+class Genre(BaseModel):
+    """
+
+    Anime or Manga Genre
+
+    """
+    id: int
+    name: str
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        return id == other.id
+
+
+class Studio(MALBaseModel):
+    """
+
+    Representation of anime studio
+
+    """
+    id: int
+    name: str
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+
+class Broadcast(MALBaseModel):
+    """
+
+    Object representing date of episode broadcast (in JST)
+
+    """
+    day_of_the_week: str
+    start_time: Optional[datetime.time]
+
+
+class Statistics(MALBaseModel):
+    """
+
+    Object representing list statistics of anime on MAL
+    """
+    num_list_users: int
+    watching: int
+    completed: int
+    on_hold: int
+    dropped: int
+    plan_to_watch: int
+
+    def __init__(self, **data):
+        if 'status' in data.keys():
+            data = data['status'] | {'num_list_users': data['num_list_users']}
+        super().__init__(**data)
+
+
+class Relation(MALBaseModel):
+    """
+
+    Object representing relations between anime and/or manga
+
+    """
+    node: Node
+    relation_type: str
+    relation_type_formatted: str
+
+
+class Recommendation(MALBaseModel):
+    """
+
+    Representation of anime or manga recommendation
+
+    """
+    num_recommendations: int
+    node: Node
+
+
+class MyMangaListStatus(MALBaseModel):
+    """
+
+    Status from users manga list
+
+    """
+    score: Optional[int]
+    status: Optional[str]
+    is_rereading: Optional[bool]
+    updated_at: Union[datetime.datetime, str, None]
+    num_chapters_read: Optional[int]
+    num_volumes_read: Optional[int]
+    start_date: Union[datetime.date, str, None]
+    finish_date: Union[datetime.date, str, None]
+    priority: Optional[int]
+    num_times_reread: Optional[int]
+    reread_value: Optional[int]
+    tags: Optional[list[str]]
+    comments: Optional[str]
+
+
+class MyAnimeListStatus(MALBaseModel):
+    """
+
+    Status from users anime list
+
+    """
+    score: int
+    status: str
+    is_rewatching: bool
+    updated_at: Union[datetime.datetime, str]
+    num_episodes_watched: int
+    start_date: Union[datetime.date, str, None]
+    finish_date: Union[datetime.date, str, None]
+
+
+class AnimeObject(MALBaseModel):
+    """
+
+    Model of anime fetched from myanimelist
+
+    """
+    id: Optional[int]
+    title: Optional[str]
+    main_picture: Optional[Asset]
+    alternative_titles: Optional[dict]
+    start_date: Union[datetime.date, str, None]
+    end_date: Union[datetime.date, str, None]
+    synopsis: Optional[str]
+    mean: Optional[float]
+    rank: Optional[int]
+    popularity: Optional[int]
+    num_list_users: Optional[int]
+    num_scoring_users: Optional[int]
+    nsfw: Optional[Nsfw]
+    genres: Union[list[Genre], None]  # There are series that have no genres on database, like this: 'Katsudou Shashin'
+    created_at: Union[datetime.datetime, str, None]
+    updated_at: Union[datetime.datetime, str, None]
+    media_type: Union[AnimeType, str, None]
+    status: Optional[AnimeStatus]
+    my_list_status: Optional[MyAnimeListStatus]
+    num_episodes: Optional[int]
+    start_season: Optional[AnimeSeason]
+    broadcast: Optional[Broadcast]
+    source: Optional[Union[AnimeSource, str]]
+    average_episode_duration: Optional[int]
+    rating: Optional[Rating]
+    studios: Optional[list[Studio]]
+    pictures: Optional[list[Asset]]
+    background: Optional[str]
+    related_anime: Optional[list[Relation]]
+    related_manga: Optional[list[Relation]]
+    recommendations: Optional[list[Recommendation]]
+    statistics: Optional[Statistics]
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __str__(self):
+        return self.title
+
+
+class MangaObject(MALBaseModel):
+    """
+
+    Model of manga fetched from myanimelist
+    Some fields may not be mentioned in official documentation
+
+    """
+    id: Optional[int]
+    title: Optional[str]
+    main_picture: Optional[Asset]
+    alternative_titles: Optional[dict]
+    start_date: Union[str, datetime.date, None]  # strptime(d['start_date'], '%Y-%m-%d').date()
+    end_date: Union[str, datetime.date, None]  # strptime(d['end_date'], '%Y-%m-%d').date()
+    synopsis: Optional[str]
+    mean: Optional[float]
+    rank: Optional[int]
+    popularity: Optional[int]
+    num_list_users: Optional[int]
+    num_scoring_users: Optional[int]
+    nsfw: Optional[Nsfw]
+    genres: Optional[list[Genre]]
+    created_at: Union[datetime.datetime, str, None]  # strptime(d['created_at'], '%Y-%m-%dT%H:%M:%S%z')
+    updated_at: Union[datetime.datetime, str, None]  # strptime(d['updated_at'], '%Y-%m-%dT%H:%M:%S%z')
+    media_type: Union[MangaType, str, None]
+    status: Union[MangaStatus, str, None]
+    my_list_status: Optional[MyMangaListStatus]
+    num_volumes: Optional[int]
+    num_chapters: Optional[int]
+    pictures: Optional[list[Asset]]
+    background: Optional[str]
+    related_anime: Optional[list[Relation]]
+    related_manga: Optional[list[Relation]]
+    recommendations: Optional[list[Recommendation]]
+
+    def __eq__(self, other):
+        return id == other.id
+
+    def __str__(self):
+        return self.title
+
+
+class UserAnimeStatistics(MALBaseModel):
+    num_items_watching: int
+    num_items_completed: int
+    num_items_on_hold: int
+    num_items_dropped: int
+    num_items_plan_to_watch: int
+    num_items: int
+    num_days_watched: float
+    num_days_watching: float
+    num_days_completed: float
+    num_days_on_hold: float
+    num_days_dropped: float
+    num_days: float
+    num_episodes: int
+    num_times_rewatched: int
+    mean_score: float
+
+
+class User(MALBaseModel):
+    id: int
+    name: str
+    picture: str
+    gender: Optional[str]
+    birthday: Union[datetime.date, str, None]
+    location: Optional[str]
+    joined_at: Union[datetime.datetime, str, None]
+    time_zone: Optional[str]
+    is_supporter: Optional[bool]
+    anime_statistics: Optional[UserAnimeStatistics]
